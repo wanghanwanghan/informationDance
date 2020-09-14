f7a3dee7939540953b0a97f7e63fd78957bdba2f
wanghan <minglongoc@me.com>
2020-09-14 10:26:03 +0800

    123

--- App/HttpController/Business/Api/TaoShu/TaoShuController.php
+++ App/HttpController/Business/Api/TaoShu/TaoShuController.php
@@ -3,6 +3,7 @@
 namespace App\HttpController\Business\Api\TaoShu;
 
 use App\HttpController\Service\TaoShu\TaoShuService;
+use wanghanwanghan\someUtils\control;
 
 class TaoShuController extends TaoShuBase
 {
@@ -17,6 +18,33 @@ class TaoShuController extends TaoShuBase
         parent::afterAction($actionName);
     }
 
+    //检验企查查返回值，并给客户计费
+    private function checkResponse($res)
+    {
+        if (isset($res['PAGEINFO']) && isset($res['PAGEINFO']['TOTAL_COUNT']) && isset($res['PAGEINFO']['TOTAL_PAGE']) && isset($res['PAGEINFO']['CURRENT_PAGE']))
+        {
+            $res['Paging']=[
+                'page'=>$res['PAGEINFO']['CURRENT_PAGE'],
+                'pageSize'=>null,
+                'total'=>$res['PAGEINFO']['TOTAL_COUNT'],
+                'totalPage'=>$res['PAGEINFO']['TOTAL_PAGE'],
+            ];
+
+        }else
+        {
+            $res['Paging']=null;
+        }
+
+        if (isset($res['coHttpErr'])) return $this->writeJson(500,$res['Paging'],[],'co请求错误');
+
+        $res['ISUSUAL'] == '1' ? $res['code'] = 200 : $res['code'] = 600;
+
+        //拿返回结果
+        isset($res['RESULTDATA']) ? $res['Result'] = $res['RESULTDATA'] : $res['Result'] = [];
+
+        return $this->writeJson($res['code'],$res['Paging'],$res['Result'],null);
+    }
+
     //企业基本信息
     function getRegisterInfo()
     {
@@ -26,16 +54,7 @@ class TaoShuController extends TaoShuBase
 
         $res=(new TaoShuService())->post($postData,__FUNCTION__);
 
-        var_export($res);
-
-        //return $this->checkResponse($res);
-
-
-
-
-
-
-
+        return $this->checkResponse($res);
     }
 
 
