7dc7bc5f4086fe4a87518f61356c633a13e5dd3f
wanghan <minglongoc@me.com>
2020-09-22 12:08:34 +0800

    123

--- App/Task/TaskList/CreateEasyReportTask.php
+++ App/Task/TaskList/CreateEasyReportTask.php
@@ -2,6 +2,7 @@
 
 namespace App\Task\TaskList;
 
+use App\Csp\Service\CspService;
 use App\HttpController\Service\TaoShu\TaoShuService;
 use App\Task\TaskBase;
 use Carbon\Carbon;
@@ -24,27 +25,20 @@ class CreateEasyReportTask extends TaskBase implements TaskInterface
     function run(int $taskId, int $workerIndex)
     {
         $tmp1 = new TemplateProcessor(REPORT_MODEL_PATH . 'EasyReportModel_1.docx');
-        $tmp2 = new TemplateProcessor(REPORT_MODEL_PATH . 'EasyReportModel_1.docx');
 
         $tmp1->setImageValue('Logo', ['path' => REPORT_IMAGE_PATH . 'logo.jpg', 'width' => 200, 'height' => 40]);
-        $tmp2->setImageValue('Logo', ['path' => REPORT_IMAGE_PATH . 'logo.jpg', 'width' => 200, 'height' => 40]);
 
         $tmp1->setValue('entName', $this->entName);
-        $tmp2->setValue('entName', $this->entName);
 
         $tmp1->setValue('reportNum', $this->reportNum);
-        $tmp2->setValue('reportNum', $this->reportNum);
 
         $tmp1->setValue('time', Carbon::now()->format('Y年m月d日'));
-        $tmp2->setValue('time', Carbon::now()->format('Y年m月d日'));
 
         $tmp1->saveAs(REPORT_PATH . $this->reportNum . '_123.docx');
-        $tmp2->saveAs(REPORT_PATH . $this->reportNum . '_321.docx');
 
-        //企业基本信息
-        $res = (new TaoShuService())->setCheckRespFlag(true)->post(['entName' => $this->entName], 'getRegisterInfo');
 
-        var_export($res);
+        $this->cspHandleData();
+
 
         return true;
     }
@@ -54,21 +48,23 @@ class CreateEasyReportTask extends TaskBase implements TaskInterface
 
     }
 
+    //并发请求数据
     private function cspHandleData()
     {
+        //创建csp对象
+        $csp = CspService::getInstance()->create();
 
+        $csp->add('getRegisterInfo', function () {
+            return (new TaoShuService())->setCheckRespFlag(true)->post(['entName' => $this->entName], 'getRegisterInfo');
+        });
+
+        $csp->add('123', function () {
+
+        });
+
+
+        var_dump(CspService::getInstance()->exec($csp));
     }
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
