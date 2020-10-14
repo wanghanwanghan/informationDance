11b22f3c9e348c95dcd809447e765548b861186d
wanghan <minglongoc@me.com>
2020-10-14 13:32:52 +0800

    2020-10-14_13:32:52

--- App/Crontab/CrontabList/RunSupervisor.php
+++ App/Crontab/CrontabList/RunSupervisor.php
@@ -6,6 +6,8 @@ use App\Crontab\CrontabBase;
 use App\HttpController\Models\Api\SupervisorEntNameInfo;
 use App\HttpController\Models\Api\SupervisorPhoneEntName;
 use App\HttpController\Service\Common\CommonService;
+use App\HttpController\Service\CreateConf;
+use App\HttpController\Service\FaHai\FaHaiService;
 use EasySwoole\EasySwoole\Crontab\AbstractCronTask;
 
 class RunSupervisor extends AbstractCronTask
@@ -34,27 +36,80 @@ class RunSupervisor extends AbstractCronTask
         //taskId是进程周期内第几个task任务
         //可以用task，也可以用process
 
-        CommonService::getInstance()->log4PHP('这里运行了1');
-
         if (!$this->crontabBase->withoutOverlapping(self::getTaskName())) return true;
 
-        //取出本次要监控的企业列表
+        //取出本次要监控的企业列表，如果列表是空会跳到onException
         $target = SupervisorPhoneEntName::create()
             ->where('status', 1)->where('expireTime', time(), '>')
             ->get()->toArray();
 
-        $this->crontabBase->removeOverlappingKey(self::getTaskName());
+        foreach ($target as $one)
+        {
+            $this->sf($one['entName']);
+            //$this->gs($one['entName']);
+            //$this->gl($one['entName']);
+            //$this->jy($one['entName']);
+        }
 
-        CommonService::getInstance()->log4PHP('这里运行了2');
+        $this->crontabBase->removeOverlappingKey(self::getTaskName());
 
         return true;
     }
 
+    //司法相关
+    private function sf($entName)
+    {
+        //失信信息=================================================================
+        $postData = [
+            'doc_type' => 'shixin',
+            'keyword' => $entName,
+            'pageno' => 1,
+            'range' => 10,
+        ];
+
+        $res = (new FaHaiService())->getList(CreateConf::getInstance()->getConf('fahai.listBaseUrl') . 'sifa', $postData);
+
+        CommonService::getInstance()->log4PHP($res);
+
+//        if ($res['Status']==200 && !empty($res['Result']))
+//        {
+//            foreach ($res['Result'] as $one)
+//            {
+//                $check=SupervisorEntNameInfo::where('keyNo',$one['Id'])->first();
+//
+//                if ($check) continue;
+//
+//                strlen($one['Liandate']) > 9 ? $time=$one['Liandate'] : $time='';
+//
+//                $content="<p>名称: {$one['Executestatus']}</p>";
+//                $content.="<p>组织类型: {$one['OrgTypeName']}</p>";
+//                $content.="<p>案号: {$one['Anno']}</p>";
+//
+//                SupervisorEntNameInfo::create([
+//                    'entName'=>$entName,
+//                    'type'=>1,
+//                    'typeDetail'=>1,
+//                    'timeRange'=>Carbon::parse($time)->timestamp,
+//                    'level'=>1,
+//                    'desc'=>$one['Executestatus'],
+//                    'content'=>$content,
+//                    'detailUrl'=>'',
+//                    'keyNo'=>$one['Id'],
+//                ]);
+//
+//                $this->addEntName($entName,'sf');
+//            }
+//        }
+
+
+
+
+
+    }
+
     function onException(\Throwable $throwable, int $taskId, int $workerIndex)
     {
-        CommonService::getInstance()->log4PHP('这里运行了3');
         $this->crontabBase->removeOverlappingKey(self::getTaskName());
-        CommonService::getInstance()->log4PHP('这里运行了4');
     }
 
 
