e7a4c4dec48ed308f7f45201266680fcfb559687
wanghan <minglongoc@me.com>
2020-10-14 14:26:56 +0800

    2020-10-14_14:26:56

--- App/Crontab/CrontabList/RunSupervisor.php
+++ App/Crontab/CrontabList/RunSupervisor.php
@@ -9,6 +9,7 @@ use App\HttpController\Service\Common\CommonService;
 use App\HttpController\Service\CreateConf;
 use App\HttpController\Service\FaHai\FaHaiService;
 use App\HttpController\Service\QiChaCha\QiChaChaService;
+use Carbon\Carbon;
 use EasySwoole\EasySwoole\Crontab\AbstractCronTask;
 
 class RunSupervisor extends AbstractCronTask
@@ -16,6 +17,8 @@ class RunSupervisor extends AbstractCronTask
     private $crontabBase;
     private $qccUrl;
     private $fahaiList;
+    //发短信用的
+    private $entNameArr = [];
 
     //每次执行任务都会执行构造函数
     function __construct()
@@ -64,6 +67,23 @@ class RunSupervisor extends AbstractCronTask
         return true;
     }
 
+    //记录公司名和风险个数
+    private function addEntName($entName,$type)
+    {
+        $name=array_keys($this->entNameArr);
+
+        if (in_array($entName,$name))
+        {
+            $this->entNameArr[$entName][$type]++;
+
+        }else
+        {
+            $this->entNameArr[$entName][$type]=1;
+        }
+
+        return true;
+    }
+
     //司法相关
     private function sf($entName)
     {
@@ -75,37 +95,138 @@ class RunSupervisor extends AbstractCronTask
 
         $res = (new QiChaChaService())->setCheckRespFlag(true)->get($this->qccUrl . 'CourtV4/SearchShiXin', $postData);
 
-        CommonService::getInstance()->log4PHP($res);
+        if ($res['code']==200 && !empty($res['result']))
+        {
+            foreach ($res['result'] as $one)
+            {
+                $check=SupervisorEntNameInfo::create()->where('keyNo',$one['Id'])->get();
 
-//        if ($res['Status']==200 && !empty($res['Result']))
-//        {
-//            foreach ($res['Result'] as $one)
-//            {
-//                $check=SupervisorEntNameInfo::where('keyNo',$one['Id'])->first();
-//
-//                if ($check) continue;
-//
-//                strlen($one['Liandate']) > 9 ? $time=$one['Liandate'] : $time='';
-//
-//                $content="<p>名称: {$one['Executestatus']}</p>";
-//                $content.="<p>组织类型: {$one['OrgTypeName']}</p>";
-//                $content.="<p>案号: {$one['Anno']}</p>";
-//
-//                SupervisorEntNameInfo::create([
-//                    'entName'=>$entName,
-//                    'type'=>1,
-//                    'typeDetail'=>1,
-//                    'timeRange'=>Carbon::parse($time)->timestamp,
-//                    'level'=>1,
-//                    'desc'=>$one['Executestatus'],
-//                    'content'=>$content,
-//                    'detailUrl'=>'',
-//                    'keyNo'=>$one['Id'],
-//                ]);
-//
-//                $this->addEntName($entName,'sf');
-//            }
-//        }
+                if ($check) continue;
+
+                strlen($one['Liandate']) > 9 ? $time=$one['Liandate'] : $time='';
+
+                $content="<p>名称: {$one['Executestatus']}</p>";
+                $content.="<p>组织类型: {$one['OrgTypeName']}</p>";
+                $content.="<p>案号: {$one['Anno']}</p>";
+
+                SupervisorEntNameInfo::create()->data([
+                    'entName'=>$entName,
+                    'type'=>1,
+                    'typeDetail'=>1,
+                    'timeRange'=>Carbon::parse($time)->timestamp,
+                    'level'=>1,
+                    'desc'=>$one['Executestatus'],
+                    'content'=>$content,
+                    'detailUrl'=>'',
+                    'keyNo'=>$one['Id'],
+                ])->save();
+
+                $this->addEntName($entName,'sf');
+            }
+        }
+
+        //被执行人=================================================================
+        $postData = [
+            'searchKey' => $entName,
+            'isExactlySame' => true,
+        ];
+
+        $res = (new QiChaChaService())->setCheckRespFlag(true)->get($this->qccUrl . 'CourtV4/SearchZhiXing', $postData);
+
+        if ($res['code']==200 && !empty($res['result']))
+        {
+            foreach ($res['result'] as $one)
+            {
+                $check=SupervisorEntNameInfo::create()->where('keyNo',$one['Id'])->get();
+
+                if ($check) continue;
+
+                strlen($one['Liandate']) > 9 ? $time=$one['Liandate'] : $time='';
+
+                $content="<p>名称: {$one['Name']}</p>";
+                $content.="<p>标的: {$one['Biaodi']}</p>";
+                $content.="<p>案号: {$one['Anno']}</p>";
+
+                SupervisorEntNameInfo::create()->data([
+                    'entName'=>$entName,
+                    'type'=>1,
+                    'typeDetail'=>2,
+                    'timeRange'=>Carbon::parse($time)->timestamp,
+                    'level'=>2,
+                    'desc'=>'被执行人信息',
+                    'content'=>$content,
+                    'detailUrl'=>'',
+                    'keyNo'=>$one['Id'],
+                ])->save();
+
+                $this->addEntName($entName,'sf');
+            }
+        }
+
+        //股权冻结=================================================================
+        $postData=[
+            'keyWord'=>$entName,
+        ];
+
+        $res = (new QiChaChaService())->setCheckRespFlag(true)->get($this->qccUrl . 'JudicialAssistance/GetJudicialAssistance', $postData);
+
+        if ($res['code']==200 && !empty($res['result']))
+        {
+            foreach ($res['result'] as $one)
+            {
+                $id=md5(json_encode($one));
+
+                $check=SupervisorEntNameInfo::create()->where('keyNo',$id)->get();
+
+                if ($check) continue;
+
+                //是冻结还是解除冻结
+                if (!empty($one['EquityFreezeDetail']))
+                {
+                    //是冻结
+                    strlen($one['EquityFreezeDetail']['FreezeStartDate']) > 9 ?
+                        $time=$one['EquityFreezeDetail']['FreezeStartDate'] :
+                        $time='';
+
+                    $content="<p>被执行人: {$one['ExecutedBy']}</p>";
+                    $content.="<p>股权数额: {$one['EquityAmount']}</p>";
+                    $content.="<p>执行通知书文号: {$one['ExecutionNoticeNum']}</p>";
+                    $level=2;
+                }else
+                {
+                    //是解除冻结
+                    strlen($one['EquityUnFreezeDetail']['UnFreezeDate']) > 9 ?
+                        $time=$one['EquityUnFreezeDetail']['UnFreezeDate'] :
+                        $time='';
+
+                    $content="<p>相关企业名称: {$one['ExecutedBy']}</p>";
+                    $content.="<p>股权数额: {$one['EquityAmount']}</p>";
+                    $content.="<p>执行通知书文号: {$one['ExecutionNoticeNum']}</p>";
+                    $level=5;
+                }
+
+                SupervisorEntNameInfo::create()->data([
+                    'entName'=>$entName,
+                    'type'=>1,
+                    'typeDetail'=>3,
+                    'timeRange'=>Carbon::parse($time)->timestamp,
+                    'level'=>$level,
+                    'desc'=>$one['Status'],
+                    'content'=>$content,
+                    'detailUrl'=>'',
+                    'keyNo'=>$id,
+                ])->save();
+
+                if ($level===2) $this->addEntName($entName,'sf');
+            }
+        }
+
+
+
+
+
+
+
 
 
     }
