b5b51632128a94f8f4ed1caeebbe815ce2705be9
wanghan <minglongoc@me.com>
2020-10-19 15:46:50 +0800

    2020-10-19_15:46:50

--- App/Crontab/CrontabList/RunSupervisor.php
+++ App/Crontab/CrontabList/RunSupervisor.php
@@ -36,7 +36,7 @@ class RunSupervisor extends AbstractCronTask
 
     static function getRule(): string
     {
-        return '* * * * *';
+        return '5 4 * * *';
     }
 
     static function getTaskName(): string
