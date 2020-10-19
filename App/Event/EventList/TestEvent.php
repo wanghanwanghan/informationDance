9ec4f86ee78f8321014bc8d254b412f3f3de7c59
wanghan <minglongoc@me.com>
2020-10-19 16:15:23 +0800

    2020-10-19_16:15:23

--- App/Event/EventList/TestEvent.php
+++ App/Event/EventList/TestEvent.php
@@ -9,16 +9,25 @@ class TestEvent extends EventBase
 {
     use Singleton;
 
+    private $eventList = [];
+
     //在框架的initialize事件中进行注册事件
     function set($key, $item)
     {
         parent::initSet();
 
         if (is_callable($item)) {
+            $this->eventList[] = $key;
             return parent::set($key, $item);
         } else {
             return false;
         }
+    }
+
+    //获取已经注册的事件列表
+    function getEventList(): array
+    {
+        return $this->eventList;
     }
 
     //全局调用，执行事件
