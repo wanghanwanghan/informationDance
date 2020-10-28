e4b2a0beb14837e44349e6f52c4c06e9afb82e6d
wanghan <minglongoc@me.com>
2020-10-28 10:41:22 +0800

    2020-10-28_10:41:22

--- App/HttpController/Business/Admin/User/UserController.php
+++ App/HttpController/Business/Admin/User/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\HttpController\Business\Admin\User;
 
+use App\HttpController\Models\Api\LngLat;
+use App\HttpController\Models\Api\User;
+
 class UserController extends UserBase
 {
     function onRequest(?string $action): ?bool
@@ -14,7 +17,39 @@ class UserController extends UserBase
         parent::afterAction($actionName);
     }
 
+    //用户列表
+    function userList()
+    {
+        try
+        {
+            $list = User::create()->all();
 
+        }catch (\Throwable $e)
+        {
+            return $this->writeErr($e,__FUNCTION__);
+        }
+
+        empty($list) ? $res = null : $res = obj2Arr($list);
+
+        return $this->writeJson(200,null,$res,null);
+    }
+
+    //用户位置
+    function userLocation()
+    {
+        try
+        {
+            $list = LngLat::create()->all();
+
+        }catch (\Throwable $e)
+        {
+            return $this->writeErr($e,__FUNCTION__);
+        }
+
+        empty($list) ? $res = null : $res = obj2Arr($list);
+
+        return $this->writeJson(200,null,$res,null);
+    }
 
 
 
