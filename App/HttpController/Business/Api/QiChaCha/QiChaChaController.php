35fb9078e578036870666d4ce800ca662a411e2a
wanghan <minglongoc@me.com>
2020-11-30 14:03:19 +0800

    2020-11-30_14:03:18

--- App/HttpController/Business/Api/QiChaCha/QiChaChaController.php
+++ App/HttpController/Business/Api/QiChaCha/QiChaChaController.php
@@ -908,8 +908,6 @@ class QiChaChaController extends QiChaChaBase
 
         $postData=[
             'keyWord' => $entName,
-            'pageIndex'=>$page,
-            'pageSize'=>$pageSize,
         ];
 
         $res=(new QiChaChaService())->get($this->baseUrl.'JudicialAssistance/GetJudicialAssistance',$postData);
