81ff94c2f8aa4f63dad1d0dcc6047f2a1998857b
wanghan <minglongoc@me.com>
2020-12-15 10:17:13 +0800

    2020-12-15_10:17:13

--- App/Task/TaskList/CreateDeepReportTask.php
+++ App/Task/TaskList/CreateDeepReportTask.php
@@ -1174,7 +1174,7 @@ class CreateDeepReportTask extends TaskBase implements TaskInterface
 
         $docObj->setImageValue('fpxx_zyspfx_img', [
             'path' => $imgPath,
-            'width' => 450,
+            'width' => 440,
             'height' => 300
         ]);
 
