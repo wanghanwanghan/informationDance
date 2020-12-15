11d22e9bc133685cde9e3df8464d94cb7f273879
wanghan <minglongoc@me.com>
2020-12-15 17:51:40 +0800

    2020-12-15_17:51:40

--- App/Crontab/CrontabList/tool/Invoice.php
+++ App/Crontab/CrontabList/tool/Invoice.php
@@ -2359,7 +2359,7 @@ class Invoice
                         if ($data['label'] == '')
                         {
                             $data['label']=$year.".$month";
-                        }elseif (strlen($data['label']) <= 14 && (substr($data['label'],0,4)!=$year || substr($data['label'],-4)!=$year))
+                        }elseif (strlen($data['label']) <= 14 && (substr($data['label'],0,4)!=$year || (substr($data['label'],0,4)!=$year && substr($data['label'],-4)!=$year)))
                         {
                             $data['label'].=" - $year";
                         }else
