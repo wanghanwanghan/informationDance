848211564815db6df514e7b97736263497a71773
wanghan <minglongoc@me.com>
2020-12-16 17:14:30 +0800

    2020-12-16_17:14:30

--- App/HttpController/Business/Provide/ProvideBase.php
+++ App/HttpController/Business/Provide/ProvideBase.php
@@ -3,6 +3,7 @@
 namespace App\HttpController\Business\Provide;
 
 use App\HttpController\Index;
+use App\HttpController\Service\Common\CommonService;
 use App\HttpController\Service\CreateConf;
 
 class ProvideBase extends Index
@@ -12,14 +13,14 @@ class ProvideBase extends Index
     function onRequest(?string $action): ?bool
     {
         parent::onRequest($action);
-
         $this->qccListUrl = CreateConf::getInstance()->getConf('qichacha.baseUrl');
-
+        CommonService::getInstance()->log4PHP('ProvideBase onRequest');
         return true;
     }
 
     function afterAction(?string $actionName): void
     {
         parent::afterAction($actionName);
+        CommonService::getInstance()->log4PHP('ProvideBase afterAction');
     }
 }
--- App/HttpController/Business/Provide/QiChaCha/QiChaChaController.php
+++ App/HttpController/Business/Provide/QiChaCha/QiChaChaController.php
@@ -3,20 +3,27 @@
 namespace App\HttpController\Business\Provide\QiChaCha;
 
 use App\HttpController\Business\Provide\ProvideBase;
+use App\HttpController\Service\Common\CommonService;
 
 class QiChaChaController extends ProvideBase
 {
     function onRequest(?string $action): ?bool
     {
-        return parent::onRequest($action);
+        parent::onRequest($action);
+        CommonService::getInstance()->log4PHP('qichacha onRequest');
+        return true;
     }
 
     function afterAction(?string $actionName): void
     {
         parent::afterAction($actionName);
+        CommonService::getInstance()->log4PHP('qichacha afterAction');
     }
 
-
+    function getTest()
+    {
+        CommonService::getInstance()->log4PHP(__FUNCTION__);
+    }
 
 
 
