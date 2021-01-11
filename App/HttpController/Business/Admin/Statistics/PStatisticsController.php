5be81c6df4cbd86e86b265b93b7802b145b559e3
wanghan <minglongoc@me.com>
2021-01-11 18:22:28 +0800

    2021-01-11_18:22:28

--- App/HttpController/Business/Admin/Statistics/PStatisticsController.php
+++ App/HttpController/Business/Admin/Statistics/PStatisticsController.php
@@ -3,6 +3,7 @@
 namespace App\HttpController\Business\Admin\Statistics;
 
 use App\HttpController\Models\Provide\RequestRecode;
+use Carbon\Carbon;
 use Ritaswc\ZxIPAddress\IPv4Tool;
 
 class PStatisticsController extends StatisticsBase
@@ -24,7 +25,9 @@ class PStatisticsController extends StatisticsBase
         $page = $this->getRequestData('page', 1);
         $pageSize = $this->getRequestData('pageSize', 20);
 
-        $data = RequestRecode::create()->addSuffix(2020)->alias('t1')
+        $year = Carbon::now()->year;
+
+        $data = RequestRecode::create()->addSuffix($year)->alias('t1')
             ->join('information_dance_request_user_info as t2', 't1.userId = t2.id', 'left')
             ->join('information_dance_request_api_info as t3', 't1.provideApiId = t3.id', 'left')
             ->field([
@@ -46,7 +49,7 @@ class PStatisticsController extends StatisticsBase
             ])->order('t1.created_at', 'desc')
             ->limit($this->exprOffset($page, $pageSize), $pageSize)->all();
 
-        $total = RequestRecode::create()->addSuffix(2020)->alias('t1')
+        $total = RequestRecode::create()->addSuffix($year)->alias('t1')
             ->join('information_dance_request_user_info as t2', 't1.userId = t2.id', 'left')
             ->join('information_dance_request_api_info as t3', 't1.provideApiId = t3.id', 'left')
             ->count('t1.id');
--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 
 include './vendor/autoload.php';
 
-$url = 'https://api.meirixindong.com/provide/v1/zw/getInvoiceOcr';
+$url = 'https://api.meirixindong.com/provide/v1/qcc/getIPOGuarantee';
 
 $appId = 'PHP_is_the_best_language_in_the_world';
 $appSecret = 'PHP_GO';
@@ -13,17 +13,20 @@ $data = [
     'appId' => $appId,
     'time' => $time,
     'sign' => $sign,
-//    'image' => new \CURLFile(realpath('./WechatIMG261-tuya.png'))
+    // 'image' => new \CURLFile(realpath('./WechatIMG261-tuya.png'))
+    'entName' => '北京京东世纪贸易有限公司',
+    'page' => 1,
+    'pageSize' => 10,
 ];
 
-//$curl=curl_init();//初始化
-//curl_setopt($curl,CURLOPT_URL,$url);//设置请求地址
-//curl_setopt($curl,CURLOPT_POST,true);//设置post方式请求
-//curl_setopt($curl,CURLOPT_CONNECTTIMEOUT,15);//几秒后没链接上就自动断开
-//curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,FALSE);
-//curl_setopt($curl,CURLOPT_POSTFIELDS,$data);//提交的数据
-//curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);//返回值不直接显示
-//$res=curl_exec($curl);//发送请求
+$curl=curl_init();//初始化
+curl_setopt($curl,CURLOPT_URL,$url);//设置请求地址
+curl_setopt($curl,CURLOPT_POST,true);//设置post方式请求
+curl_setopt($curl,CURLOPT_CONNECTTIMEOUT,15);//几秒后没链接上就自动断开
+curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,FALSE);
+curl_setopt($curl,CURLOPT_POSTFIELDS,$data);//提交的数据
+curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);//返回值不直接显示
+$res=curl_exec($curl);//发送请求
 
 
 //$curl = curl_init();
@@ -38,32 +41,6 @@ $data = [
 //dd(json_decode($result, 1));
 
 
-function sortArrByKey($array, $key = 'id', $rule = 'desc')
-{
-    strtolower($rule) === 'asc' ? $rule = 'SORT_ASC' : $rule = 'SORT_DESC';
-
-    $arrSort = [];
-
-    foreach ($array as $index => $one) {
-        foreach ($one as $key => $val) {
-            $arrSort[$key][$index] = $val;
-        }
-    }
-
-    array_multisort($arrSort[$key], constant($rule), $array);
-
-    return $array;
-}
-
-function sortArrByKeyNew($array, $key = 'id', $rule = 'desc')
-{
-    strtolower($rule) === 'asc' ? $rule = 'SORT_ASC' : $rule = 'SORT_DESC';
-
-    $array_column = array_column($array, $key);
-    array_multisort($array_column, constant($rule), $array);
-
-    return $array;
-}
 
 
 
