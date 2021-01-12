60d1ab9480c65ad65759f6c647bca89f70b31c7b
wanghan <minglongoc@me.com>
2021-01-12 12:08:53 +0800

    2021-01-12_12:08:53

--- App/HttpController/Business/Admin/Api/PApiController.php
+++ App/HttpController/Business/Admin/Api/PApiController.php
@@ -38,7 +38,7 @@ class PApiController extends ApiBase
         $desc = $this->getRequestData('desc');
         $source = $this->getRequestData('source');
         $price = $this->getRequestData('price');//成本价
-        $apiDoc = $this->getRequestData('apiDoc');//成本价
+        $apiDoc = $this->getRequestData('apiDoc');
 
         if (empty($path) || empty($name)) return $this->writeJson(201);
         if (empty($source) || empty($price)) return $this->writeJson(201);
