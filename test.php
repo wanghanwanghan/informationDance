b8d60a9fff7b564d9d55bc088bba4ba85a3ff8ed
wanghan <minglongoc@me.com>
2021-01-12 09:30:46 +0800

    2021-01-12_09:30:46

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 
 include './vendor/autoload.php';
 
-$url = 'https://api.meirixindong.com/provide/v1/qcc/getIPOGuarantee';
+$url = 'https://api.meirixindong.com/provide/v1/qq/getThreeYearsData';
 
 $appId = 'PHP_is_the_best_language_in_the_world';
 $appSecret = 'PHP_GO';
