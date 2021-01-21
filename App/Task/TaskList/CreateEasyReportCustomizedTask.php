02460a8f2075bc8de16165c09db7db67e10a7893
wanghan <minglongoc@me.com>
2021-01-21 16:49:06 +0800

    2021-01-21_16:49:06

--- App/Task/TaskList/CreateEasyReportCustomizedTask.php
+++ App/Task/TaskList/CreateEasyReportCustomizedTask.php
@@ -107,7 +107,7 @@ class CreateEasyReportCustomizedTask extends TaskBase implements TaskInterface
     {
         $pdf->AddPage();
         $tbl = <<<EOD
-<table border="1" cellpadding="0" cellspacing="0" style="border-collapse: collapse">
+<table border="1" cellspacing="0" style="border-collapse: collapse">
 <thead>
  <tr style="background-color:#FFFF00;color:#0000FF;">
   <td width="30" align="center"><b>A</b></td>
