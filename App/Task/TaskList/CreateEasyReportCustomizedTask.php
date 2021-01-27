668d66ba51b54b105a0dac4cac597590d06d07a7
wanghan <minglongoc@me.com>
2021-01-27 11:35:16 +0800

    2021-01-27_11:35:16

--- App/Task/TaskList/CreateEasyReportCustomizedTask.php
+++ App/Task/TaskList/CreateEasyReportCustomizedTask.php
@@ -453,10 +453,10 @@ TEMP;
     </tr>
     <tr>
         <td width="7%">序号</td>
-        <td width="11%">列入日期</td>
+        <td width="12%">列入日期</td>
         <td width="35%">列入原因</td>
         <td width="12%">移出日期</td>
-        <td width="35%">移出原因</td>
+        <td width="34%">移出原因</td>
     </tr>
     {$insert}
 </table>
@@ -464,6 +464,48 @@ TEMP;
             $pdf->writeHTML($html, true, false, false, false, '');
         }
 
+        CommonService::getInstance()->log4PHP($cspData);
+
+        //基本信息 实际控制人
+        if (array_key_exists('Beneficiary',$cspData))
+        {
+            $insert = '';
+
+            if (!empty($cspData['Beneficiary']))
+            {
+                $i = 1;
+
+                foreach ($cspData['Beneficiary'] as $one)
+                {
+                    $temp = '<tr>';
+                    $temp .= "<td>{$i}</td>";
+                    $temp .= "<td>{$this->formatDate($one['AddDate'])}</td>";
+                    $temp .= "<td>{$one['AddReason']}</td>";
+                    $temp .= "<td>{$this->formatDate($one['RemoveDate'])}</td>";
+                    $temp .= "<td>{$one['RomoveReason']}</td>";
+                    $temp .= '</tr>';
+                    $insert .= $temp;
+                    $i++;
+                }
+            }
+
+            $html = <<<TEMP
+<table border="1" cellpadding="5" style="border-collapse: collapse;width: 100%;text-align: center">
+    <tr>
+        <td colspan="5" style="text-align: center;background-color: #d3d3d3">经营异常</td>
+    </tr>
+    <tr>
+        <td width="7%">序号</td>
+        <td width="12%">列入日期</td>
+        <td width="35%">列入原因</td>
+        <td width="12%">移出日期</td>
+        <td width="34%">移出原因</td>
+    </tr>
+    {$insert}
+</table>
+TEMP;
+            $pdf->writeHTML($html, true, false, false, false, '');
+        }
 
 
 
