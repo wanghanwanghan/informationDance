86a5a6105cc8e6fa89c60b398a988fda41f70db2
wanghan <minglongoc@me.com>
2021-01-28 13:19:43 +0800

    2021-01-28_13:19:43

--- App/Task/TaskList/CreateEasyReportCustomizedTask.php
+++ App/Task/TaskList/CreateEasyReportCustomizedTask.php
@@ -2803,12 +2803,12 @@ TEMP;
         <td colspan="6">证监会许可信息 {$num} 项，报告中提供最新的 20 条记录</td>
     </tr>
     <tr>
-        <td>序号</td>
-        <td>标题</td>
-        <td>文书号</td>
-        <td>许可事项</td>
-        <td>管理机关</td>
-        <td>许可时间</td>
+        <td width="7%">序号</td>
+        <td width="13%">标题</td>
+        <td width="13%">文书号</td>
+        <td width="41%">许可事项</td>
+        <td width="13%">管理机关</td>
+        <td width="13%">许可时间</td>
     </tr>
     {$insert}
 </table>
@@ -2817,6 +2817,58 @@ TEMP;
         }
     }
 
+    //一行两会信息 外汇局处罚
+    private function safe_chufa(Tcpdf $pdf, $cspData)
+    {
+        if (array_key_exists(__FUNCTION__,$cspData))
+        {
+            $insert = $num = '';
+
+            if (!empty($cspData[__FUNCTION__]))
+            {
+                $i = 1;
+
+                $num = $cspData[__FUNCTION__]['total'];
+
+                foreach ($cspData[__FUNCTION__]['list'] as $one)
+                {
+                    $temp = '<tr>';
+                    $temp .= "<td>{$i}</td>";
+                    $temp .= "<td>{$one['detail']['caseNo']}</td>";
+                    $temp .= "<td>{$one['detail']['caseCause']}</td>";
+                    $temp .= "<td>{$one['detail']['eventResult']}</td>";
+                    $temp .= "<td>{$one['detail']['money']}</td>";
+                    $temp .= "<td>{$one['detail']['authority']}</td>";
+                    $temp .= "<td>{$one['sortTimeString']}</td>";
+                    $temp .= '</tr>';
+                    $insert .= $temp;
+                    $i++;
+                }
+            }
+
+            $html = <<<TEMP
+<table border="1" cellpadding="5" style="border-collapse: collapse;width: 100%;text-align: center">
+    <tr>
+        <td colspan="6" style="text-align: center;background-color: #d3d3d3">外汇局处罚</td>
+    </tr>
+    <tr>
+        <td colspan="6">外汇局处罚 {$num} 项，报告中提供最新的 20 条记录</td>
+    </tr>
+    <tr>
+        <td width="7%">序号</td>
+        <td width="13%">文书号</td>
+        <td width="18%">违规行为</td>
+        <td width="18%">罚款结果</td>
+        <td width="18%">罚款金额</td>
+        <td width="13%">执行机关</td>
+        <td width="13%">处罚时间</td>
+    </tr>
+    {$insert}
+</table>
+TEMP;
+            $pdf->writeHTML($html, true, false, false, false, '');
+        }
+    }
 
 
 
