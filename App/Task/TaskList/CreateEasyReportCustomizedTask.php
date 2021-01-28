6ad46a4b840612c33b0ea38daf6630d11221e1ae
wanghan <minglongoc@me.com>
2021-01-28 12:24:58 +0800

    2021-01-28_12:24:58

--- App/Task/TaskList/CreateEasyReportCustomizedTask.php
+++ App/Task/TaskList/CreateEasyReportCustomizedTask.php
@@ -2558,7 +2558,54 @@ TEMP;
         }
     }
 
+    //海关信息 海关处罚
+    private function custom_punish(Tcpdf $pdf, $cspData)
+    {
+        if (array_key_exists(__FUNCTION__,$cspData))
+        {
+            $insert = $num = '';
 
+            if (!empty($cspData[__FUNCTION__]))
+            {
+                $i = 1;
+
+                $num = $cspData[__FUNCTION__]['total'];
+
+                foreach ($cspData[__FUNCTION__]['list'] as $one)
+                {
+                    $temp = '<tr>';
+                    $temp .= "<td>{$i}</td>";
+                    $temp .= "<td>{$one['detail']['ggType']}</td>";
+                    $temp .= "<td>{$one['detail']['eventType']}</td>";
+                    $temp .= "<td>{$one['detail']['yj']}</td>";
+                    $temp .= "<td>{$one['sortTimeString']}</td>";
+                    $temp .= '</tr>';
+                    $insert .= $temp;
+                    $i++;
+                }
+            }
+
+            $html = <<<TEMP
+<table border="1" cellpadding="5" style="border-collapse: collapse;width: 100%;text-align: center">
+    <tr>
+        <td colspan="5" style="text-align: center;background-color: #d3d3d3">海关处罚</td>
+    </tr>
+    <tr>
+        <td colspan="5">海关处罚 {$num} 项，报告中提供最新的 20 条记录</td>
+    </tr>
+    <tr>
+        <td>序号</td>
+        <td>公告类型</td>
+        <td>处罚类别/案件性质</td>
+        <td>依据</td>
+        <td>处罚日期</td>
+    </tr>
+    {$insert}
+</table>
+TEMP;
+            $pdf->writeHTML($html, true, false, false, false, '');
+        }
+    }
 
 
 
