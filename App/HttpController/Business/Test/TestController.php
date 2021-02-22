cd9c9334a23d3a533aab75e82bf57896822160d6
wanghan <minglongoc@me.com>
2021-02-22 18:39:09 +0800

    2021-02-22_18:39:09

--- App/HttpController/Business/Test/TestController.php
+++ App/HttpController/Business/Test/TestController.php
@@ -27,6 +27,8 @@ class TestController extends BusinessBase
 
         if (empty($entList)) return $this->writeJson(201, null, null, '公司名称不能是空');
 
+        $temp = [];
+
         foreach ($entList as $entName) {
 
             $entName = trim($entName);
@@ -35,10 +37,61 @@ class TestController extends BusinessBase
                 ->setCheckRespFlag(true)
                 ->getThreeYears(['entName' => $entName]);
 
-            CommonService::getInstance()->log4PHP($res);
+            if ($res['code'] === 200 && !empty($res['result'])) {
+
+                //ASSGRO_REL 资产总额
+                //LIAGRO_REL 负债总额
+                //VENDINC_REL 营业总收入
+                //MAIBUSINC_REL 主营业务收入
+                //PROGRO_REL 利润总额
+                //NETINC_REL 净利润
+                //RATGRO_REL 纳税总额
+                //TOTEQU_REL 所有者权益
+                //SOCNUM 社保人数
+
+                foreach ($res['result'] as $key => $val) {
+                    if (empty($val)) {
+                        $temp[] = [$entName, $key, '无数据', '无数据'];
+                    } else {
+                        $temp[] = [$entName, $key, 'ASSGRO_REL 资产总额', $val['ASSGRO_REL']];
+                        $temp[] = [$entName, $key, 'LIAGRO_REL 负债总额', $val['LIAGRO_REL']];
+                        $temp[] = [$entName, $key, 'VENDINC_REL 营业总收入', $val['VENDINC_REL']];
+                        $temp[] = [$entName, $key, 'MAIBUSINC_REL 主营业务收入', $val['MAIBUSINC_REL']];
+                        $temp[] = [$entName, $key, 'PROGRO_REL 利润总额', $val['PROGRO_REL']];
+                        $temp[] = [$entName, $key, 'NETINC_REL 净利润', $val['NETINC_REL']];
+                        $temp[] = [$entName, $key, 'RATGRO_REL 纳税总额', $val['RATGRO_REL']];
+                        $temp[] = [$entName, $key, 'TOTEQU_REL 所有者权益', $val['TOTEQU_REL']];
+                        $temp[] = [$entName, $key, 'SOCNUM 社保人数', $val['SOCNUM']];
+                    }
+                }
+            }
         }
 
-        return $this->writeJson();
+        if (!empty($temp)) {
+
+            $config = [
+                'path' => ROOT_PATH . DIRECTORY_SEPARATOR,
+            ];
+
+            $fileName = 'tutorial01.xlsx';
+
+            $xlsxObject = new \Vtiful\Kernel\Excel($config);
+
+            $filePath = $xlsxObject->fileName('tutorial01.xlsx', 'sheet1')
+                ->header(['企业名称', '年', '字段', '数值'])->data($temp)->output();
+
+            $this->response()
+                ->withHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            $this->response()->withHeader('Content-Disposition', 'attachment;filename="' . $fileName . '"');
+            $this->response()->withHeader('Content-Length', filesize($filePath));
+            $this->response()->withHeader('Content-Transfer-Encoding', 'binary');
+            $this->response()->withHeader('Cache-Control', 'must-revalidate');
+            $this->response()->withHeader('Cache-Control', 'max-age=0');
+            $this->response()->withHeader('Pragma', 'public');
+
+        } else {
+            return $this->writeJson(200, null, $temp, 'ok');
+        }
     }
 
 }
