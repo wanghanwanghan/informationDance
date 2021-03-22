d1b42684911c97a2c29ca145498fee582308acff
wanghan <minglongoc@me.com>
2021-03-22 14:54:59 +0800

    2021-03-22_14:54:59

--- App/HttpController/Service/XinDong/Score/xds.php
+++ App/HttpController/Service/XinDong/Score/xds.php
@@ -8,15 +8,18 @@ class xds
 {
     function cwScore($entName): ?array
     {
-        $arr = (new LongXinService())->getFinanceData([
+        $arr = (new LongXinService())->setCheckRespFlag(true)->getFinanceData([
             'entName' => $entName,
             'code' => '',
             'beginYear' => date('Y') - 1,
             'dataCount' => 4,
         ], false);
 
+//        if (!isset($arr['code']) || $arr['code'] !== 200 || empty($res['data']))
+//            return null;
 
-        return empty($arr) ? null : $arr;
+
+        return $arr;
     }
 
 
