01084c081101d22588ec2e68210371c2940a6acd
wanghan <minglongoc@me.com>
2021-03-22 17:25:48 +0800

    2021-03-22_17:25:48

--- App/HttpController/Business/Api/ZhongWang/ZhongWangController.php
+++ App/HttpController/Business/Api/ZhongWang/ZhongWangController.php
@@ -126,6 +126,8 @@ class ZhongWangController extends ZhongWangBase
 
         !(isset($res['code']) && $res['code'] == 0) ?: $res['code'] = 200;
 
+        // $res['message'] = (new xds())->cwScore($entName);
+
         return $this->writeJson($res['code'], null, $res['data'], $res['message']);
     }
 
