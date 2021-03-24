b8f9d7f2f4a36e084f7d341014c983f12a4ae15b
wanghan <minglongoc@me.com>
2021-03-24 19:30:05 +0800

    2021-03-24_19:30:05

--- App/Command/CommandList/TestCommand.php
+++ App/Command/CommandList/TestCommand.php
@@ -3,9 +3,6 @@
 namespace App\Command\CommandList;
 
 use App\Command\CommandBase;
-use App\HttpController\Service\Common\CommonService;
-use App\HttpController\Service\CreateConf;
-use EasySwoole\Pool\Manager;
 
 class TestCommand extends CommandBase
 {
@@ -15,77 +12,10 @@ class TestCommand extends CommandBase
     }
 
     //php easyswoole test
+    //只能执行initialize里的
     function exec(array $args): ?string
     {
         parent::commendInit();
-
-        try {
-            $mysqlObj = Manager::getInstance()
-                ->get(CreateConf::getInstance()->getConf('env.mysqlDatabaseMZJD'))
-                ->getObj();
-            $mysqlObj->rawQuery('truncate table qyxx_copy1');
-            while (true) {
-                $limit = 0;
-                $sql = <<<EOF
-SELECT
-	ORG_CODE,
-	XZQH_NAME,
-	count( 1 ) AS num 
-FROM
-	qyxx 
-WHERE
-	XZQH_NAME IS NOT NULL 
-	AND XZQH_NAME <> "" 
-GROUP BY
-	ORG_CODE,
-	XZQH_NAME 
-	LIMIT {$limit},
-	500
-EOF;
-                $list = $mysqlObj->rawQuery($sql);
-                $list = obj2Arr($list);
-
-                if (empty($list)) break;
-
-                foreach ($list as $index => $val) {
-                    if (strpos($val['XZQH_NAME'], '-') !== false) {
-                        $val['XZQH_NAME'] = str_replace('--', '-', $val['XZQH_NAME']);
-                        $placeArr = explode('-', $val['XZQH_NAME']);
-                        $placeArr = array_filter($placeArr);
-                        $sql = <<<EOF
-SELECT
-	* 
-FROM
-	qyxx_copy1 
-WHERE
-	XZQH_NAME = {$placeArr[0]}
-EOF;
-                        $check = $mysqlObj->rawQuery($sql);
-                        if (empty($check)) {
-                            $sql = <<<EOF
-INSERT INTO qyxx_copy1
-VALUES
-	( {$placeArr[0]}, {$val['num']} )
-EOF;
-                        } else {
-                            $sql = <<<EOF
-UPDATE qyxx_copy1 
-SET num = num + {$val['num']} 
-WHERE
-	XZQH_NAME = {$placeArr[0]}
-EOF;
-                        }
-                        $mysqlObj->rawQuery($sql);
-                    }
-                }
-            }
-        } catch (\Throwable $e) {
-            CommonService::getInstance()->log4PHP($e->getMessage());
-        } finally {
-            Manager::getInstance()
-                ->get(CreateConf::getInstance()->getConf('env.mysqlDatabaseMZJD'))
-                ->recycleObj($mysqlObj);
-        }
 
         return 'this is exec' . PHP_EOL;
     }
--- App/Process/ProcessList/TestProcess.php
+++ App/Process/ProcessList/TestProcess.php
@@ -3,7 +3,9 @@
 namespace App\Process\ProcessList;
 
 use App\HttpController\Service\Common\CommonService;
+use App\HttpController\Service\CreateConf;
 use App\Process\ProcessBase;
+use EasySwoole\Pool\Manager;
 use Swoole\Process;
 
 class TestProcess extends ProcessBase
@@ -22,6 +24,74 @@ class TestProcess extends ProcessBase
 
         //接收数据 string
         $data = jsonDecode($process->read());
+
+        try {
+            $mysqlObj = Manager::getInstance()
+                ->get(CreateConf::getInstance()->getConf('env.mysqlDatabaseMZJD'))
+                ->getObj();
+            $mysqlObj->rawQuery('truncate table qyxx_copy1');
+            while (true) {
+                $limit = 0;
+                $sql = <<<EOF
+SELECT
+	ORG_CODE,
+	XZQH_NAME,
+	count( 1 ) AS num 
+FROM
+	qyxx 
+WHERE
+	XZQH_NAME IS NOT NULL 
+	AND XZQH_NAME <> "" 
+GROUP BY
+	ORG_CODE,
+	XZQH_NAME 
+	LIMIT {$limit},
+	500
+EOF;
+                $list = $mysqlObj->rawQuery($sql);
+                $list = obj2Arr($list);
+
+                if (empty($list)) break;
+
+                foreach ($list as $index => $val) {
+                    if (strpos($val['XZQH_NAME'], '-') !== false) {
+                        $val['XZQH_NAME'] = str_replace('--', '-', $val['XZQH_NAME']);
+                        $placeArr = explode('-', $val['XZQH_NAME']);
+                        $placeArr = array_filter($placeArr);
+                        $sql = <<<EOF
+SELECT
+	* 
+FROM
+	qyxx_copy1 
+WHERE
+	XZQH_NAME = {$placeArr[0]}
+EOF;
+                        $check = $mysqlObj->rawQuery($sql);
+                        if (empty($check)) {
+                            $sql = <<<EOF
+INSERT INTO qyxx_copy1
+VALUES
+	( {$placeArr[0]}, {$val['num']} )
+EOF;
+                        } else {
+                            $sql = <<<EOF
+UPDATE qyxx_copy1 
+SET num = num + {$val['num']} 
+WHERE
+	XZQH_NAME = {$placeArr[0]}
+EOF;
+                        }
+                        $mysqlObj->rawQuery($sql);
+                    }
+                }
+            }
+        } catch (\Throwable $e) {
+            CommonService::getInstance()->log4PHP($e->getMessage());
+        } finally {
+            Manager::getInstance()
+                ->get(CreateConf::getInstance()->getConf('env.mysqlDatabaseMZJD'))
+                ->recycleObj($mysqlObj);
+        }
 
         return true;
     }
