29ae225fc548a96ad90cb4f160b788bc5be683c4
wanghan <minglongoc@me.com>
2021-03-29 15:18:06 +0800

    2021-03-29_15:18:06

--- App/HttpController/Service/LongXin/LongXinService.php
+++ App/HttpController/Service/LongXin/LongXinService.php
@@ -309,8 +309,6 @@ class LongXinService extends ServiceBase
 
         TaskService::getInstance()->create(new insertFinance($postData['entName'], $temp, $social['AnnualSocial']));
 
-        CommonService::getInstance()->log4PHP($temp);
-
         //原值计算
         if ($postData['dataCount'] > 1) {
             $temp = $this->exprHandle($temp);
