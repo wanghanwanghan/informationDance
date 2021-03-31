989d1471d1e15610312e27536a8fe37dc8d861f6
wanghan <minglongoc@me.com>
2021-03-31 14:12:11 +0800

    2021-03-31_14:12:11

--- App/HttpController/Business/Api/QiChaCha/QiChaChaController.php
+++ App/HttpController/Business/Api/QiChaCha/QiChaChaController.php
@@ -152,25 +152,20 @@ class QiChaChaController extends QiChaChaBase
 
         $res = (new QiChaChaService())->get($this->baseUrl . 'Beneficiary/GetBeneficiary', $postData);
 
-        CommonService::getInstance()->log4PHP($res);
-
         $tmp = [];
 
         if (count($res['Result']['BreakThroughList']) > 0) {
             $total = current($res['Result']['BreakThroughList']);
             $total = substr($total['TotalStockPercent'], 0, -1);
-
             if ($total >= 50) {
                 //如果第一个人就是大股东了，就直接返回
                 $tmp = $res['Result']['BreakThroughList'][0];
-
             } else {
                 //把返回的所有人加起来和100做减法，求出坑
                 $hole = 100;
                 foreach ($res['Result']['BreakThroughList'] as $key => $val) {
                     $hole -= substr($val['TotalStockPercent'], 0, -1);
                 }
-
                 //求出坑的比例，如果比第一个人大，那就是特殊机构，如果没第一个人大，那第一个人就是控制人
                 if ($total > $hole) $tmp = $res['Result']['BreakThroughList'][0];
             }
