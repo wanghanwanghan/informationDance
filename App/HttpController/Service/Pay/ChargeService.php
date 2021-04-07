a2706c61696003acf632be9162dfaf5b269e7162
wanghan <minglongoc@me.com>
2021-04-07 13:39:02 +0800

    2021-04-07_13:39:02

--- App/HttpController/Service/Pay/ChargeService.php
+++ App/HttpController/Service/Pay/ChargeService.php
@@ -91,7 +91,7 @@ class ChargeService extends ServiceBase
                 ->where('phone', $phone)
                 ->where('entName', $entName)
                 ->where('moduleId', $moduleNum)
-                ->where('created_at', time() - 30, '>')//首先要看这个人在30秒之前有没有真的消费并扣钱
+                ->where('created_at', time() - 5, '>')//首先要看这个人在5秒之前有没有真的消费并扣钱
                 ->where('price', 0, '<')//是否有被扣费
                 ->get();
 
