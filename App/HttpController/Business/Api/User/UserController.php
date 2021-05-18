9f5bd22de197456c9c31d8d837f7658e3036d4ef
wanghan <minglongoc@me.com>
2021-05-18 12:22:38 +0800

    2021-05-18_12:22:38

--- App/HttpController/Business/Api/User/UserController.php
+++ App/HttpController/Business/Api/User/UserController.php
@@ -683,7 +683,11 @@ class UserController extends UserBase
         $fileHandle = $fileObject->getHandle();
         $format = new \Vtiful\Kernel\Format($fileHandle);
         $boldStyle = $format->bold()->toResource();
-        $fileObject->header($header)->data($data)->setRow('A1', 20, $boldStyle)->output();
+        $fileObject->header($header)
+            ->data($data)
+            ->setRow('A1', 20, $boldStyle)
+            ->setColumn('A:A', 200, $boldStyle)
+            ->output();
 
         return $this->writeJson(200, null, 'Static/Temp/' . $filename);
     }
