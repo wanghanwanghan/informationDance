234e0c56cfcb6bb2a27364daa41ff8f327932bc8
wanghan <minglongoc@me.com>
2021-05-24 11:08:39 +0800

    2021-05-24_11:08:39

--- App/Process/ProcessList/WanBaoChuiProcess.php
+++ App/Process/ProcessList/WanBaoChuiProcess.php
@@ -2,6 +2,8 @@
 
 namespace App\Process\ProcessList;
 
+use App\HttpController\Service\Common\CommonService;
+use App\HttpController\Service\HttpClient\CoHttpClient;
 use App\Process\ProcessBase;
 use Swoole\Process;
 
@@ -13,14 +15,20 @@ class WanBaoChuiProcess extends ProcessBase
     {
         //可以用来初始化
         parent::run($arg);
+
+        $res = (new CoHttpClient())->useCache(false)
+            ->send('http://wbcapi.shuhuiguoyou.com/auction/0/?page=10', [], [
+                'token' => 'df0b8ce22bb092a98a533f66705f50b7',
+            ], [], 'post');
+
+        CommonService::getInstance()->log4PHP($res);
+
+
     }
 
     protected function onPipeReadable(Process $process)
     {
         parent::onPipeReadable($process);
-
-        //接收数据 string
-        $data = jsonDecode($process->read());
 
         return true;
     }
