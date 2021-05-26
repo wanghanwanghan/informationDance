b6d95231e3738639a8a326dac2d5d4290b885d02
wanghan <minglongoc@me.com>
2021-05-26 16:13:10 +0800

    2021-05-26_16:13:10

--- App/HttpController/Business/Api/User/UserController.php
+++ App/HttpController/Business/Api/User/UserController.php
@@ -175,16 +175,12 @@ class UserController extends UserBase
         $pwd_saki_confirm = $this->request()->getRequestParam('pwd_saki_confirm') ?? '';
 
         try {
-            $user_info = User::create()->where([
-                'phone' => $phone,
-            ])->get();
+            $user_info = User::create()->where('phone', $phone)->get();
             if (empty($user_info)) return $this->writeJson(201, null, null, '用户不存在');
             if (mb_strlen($pwd_saki) > 10) return $this->writeJson(201, null, null, '新密码最多10位');
             if (mb_strlen($pwd_saki) < 6) return $this->writeJson(201, null, null, '新密码最少6位');
             if ($pwd_saki !== $pwd_saki_confirm) return $this->writeJson(201, null, null, '新密码输入不一致');
-            $user_info->update([
-                'password' => $pwd_saki,
-            ]);
+            $user_info->update(['password' => $pwd_saki]);
         } catch (\Throwable $e) {
             return $this->writeErr($e, __FUNCTION__);
         }
