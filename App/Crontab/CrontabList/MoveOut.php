75e2f179d218ff7f952ac4546d01bd10afaf2a2b
wanghan <minglongoc@me.com>
2021-06-02 18:01:19 +0800

    2021-06-02_18:01:19

--- App/Crontab/CrontabList/MoveOut.php
+++ App/Crontab/CrontabList/MoveOut.php
@@ -22,8 +22,8 @@ class MoveOut extends AbstractCronTask
     static function getRule(): string
     {
         //每天的凌晨5点
-        return '0 5 * * *';
-        //return '* * * * *';
+        //return '0 5 * * *';
+        return '* * * * *';
     }
 
     static function getTaskName(): string
