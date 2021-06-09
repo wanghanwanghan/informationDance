d21c72aaac30a6b5bf3f41db549cbcbd0ee6b555
wanghan <minglongoc@me.com>
2021-06-09 15:50:33 +0800

    2021-06-09_15:50:33

--- App/HttpController/Service/GuoPiao/GuoPiaoService.php
+++ App/HttpController/Service/GuoPiao/GuoPiaoService.php
@@ -314,6 +314,8 @@ class GuoPiaoService extends ServiceBase
 
         $res = $this->readyToSend($api_path, $body);
 
+        CommonService::getInstance()->log4PHP($res);
+
         return $this->checkRespFlag ? $this->checkResp($res, __FUNCTION__) : $res;
     }
 
