3179827ed869b3a4e16051c3750e6b58c38953b4
wanghan <minglongoc@me.com>
2021-06-11 17:14:19 +0800

    2021-06-11_17:14:19

--- App/HttpController/Service/MoveOut/MoveOutService.php
+++ App/HttpController/Service/MoveOut/MoveOutService.php
@@ -2,9 +2,11 @@
 
 namespace App\HttpController\Service\MoveOut;
 
+use App\HttpController\Models\Api\MoveOutEntNameRel;
 use App\HttpController\Models\Api\MoveOutPhoneEntName;
+use App\HttpController\Models\EntDb\EntDbBasic;
+use App\HttpController\Models\EntDb\EntDbInv;
 use App\HttpController\Service\Common\CommonService;
-use App\HttpController\Service\CreateTable\CreateTableService;
 use App\HttpController\Service\ServiceBase;
 use wanghanwanghan\someUtils\traits\Singleton;
 
@@ -15,18 +17,6 @@ class MoveOutService extends ServiceBase
     //更新所有监控中的企业
     function updateDatabase(): ?array
     {
-        //分公司:将园内企业名单作比对目标
-        //提取信动T+1新企数据中的所有***分公司
-        //将与园内企业主体名匹配的列为预警推荐目标
-
-        //子公司（含股比66%以上）将园内企业名单作比对目标
-        //提取信动T+1新企数据中的所有股东名称与股比信息
-        //占比66%以上的股东名称且与园内企业主体名匹配的列为预警推荐目标
-
-        //园内企业名单调用信动接口调出所有下属公司（含分公司、子公司、占比66%以上公司）
-        //提取信动T+2变更数据中的所有变更地址项的公司名称
-        //与园内企业下属公司名称匹配的将开始接口反推的园内企业列为预警推荐目标
-
         $list = MoveOutPhoneEntName::create()
             ->where('status', 1)
             ->where('expireTime', time(), '>')
@@ -37,17 +27,63 @@ class MoveOutService extends ServiceBase
             return null;
         }
 
+        foreach ($list as $oneEnt) {
+            //分公司:将园内企业名单作比对目标
+            //提取信动T+1新企数据中的所有***分公司
+            //将与园内企业主体名匹配的列为预警推荐目标
+            $basicInfo = EntDbBasic::create()->where('ENTNAME', "{$oneEnt->entName}%", 'LIKE')->all();
+            if (!empty($basicInfo)) {
+                foreach ($basicInfo as $oneTarget) {
+                    $relInfo = MoveOutEntNameRel::create()->where([
+                        'pid' => $oneEnt->id,
+                        'bid' => $oneTarget->id,
+                        'type' => 'basic',
+                    ])->get();
+                    if (empty($relInfo)) {
+                        MoveOutEntNameRel::create()->data([
+                            'pid' => $oneEnt->id,
+                            'bid' => $oneTarget->id,
+                            'entName' => $oneTarget->ENTNAME,
+                            'oldName' => $oneTarget->OLDNAME,
+                            'code' => $oneTarget->SHXYDM,
+                            'type' => 'basic',
+                        ])->save();
+                    } else {
+                        $relInfo->update([
+                            'entName' => $oneTarget->ENTNAME,
+                            'oldName' => $oneTarget->OLDNAME,
+                        ]);
+                    }
+                }
+            }
 
-
-
-
-
-
-
-
-
-
-
+            //子公司（含股比66%以上）将园内企业名单作比对目标
+            //提取信动T+1新企数据中的所有股东名称与股比信息
+            //占比66%以上的股东名称且与园内企业主体名匹配的列为预警推荐目标
+            $invInfo = EntDbInv::create()->where('INV', $oneEnt->entName)->where('CONRATIO', '66', '>=')->all();
+            if (!empty($invInfo)) {
+                foreach ($invInfo as $oneTarget) {
+                    $relInfo = MoveOutEntNameRel::create()->where([
+                        'pid' => $oneEnt->id,
+                        'iid' => $oneTarget->id,
+                        'type' => 'inv',
+                    ])->get();
+                    if (empty($relInfo)) {
+                        MoveOutEntNameRel::create()->data([
+                            'pid' => $oneEnt->id,
+                            'iid' => $oneTarget->id,
+                            'entName' => $oneTarget->ENTNAME,
+                            'code' => $oneTarget->SHXYDM,
+                            'type' => 'inv',
+                        ])->save();
+                    } else {
+                        $relInfo->update([
+                            'entName' => $oneTarget->ENTNAME,
+                        ]);
+                    }
+                }
+            }
+        }
 
         return null;
     }
