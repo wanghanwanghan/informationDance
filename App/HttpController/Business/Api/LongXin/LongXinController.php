4f152f3e8c9c868d10f588bdffbd54b6ba30b6e7
wanghan <minglongoc@me.com>
2021-06-16 14:51:21 +0800

    2021-06-16_14:51:21

--- App/HttpController/Business/Api/LongXin/LongXinController.php
+++ App/HttpController/Business/Api/LongXin/LongXinController.php
@@ -344,6 +344,7 @@ class LongXinController extends LongXinBase
                 $res = (new LongXinService())->getFinanceBaseMergeData($postData, false);
             } else {
                 $res = (new LongXinService())->getFinanceData($postData, false);
+                CommonService::getInstance()->log4PHP($res);
             }
 
             $charge = ChargeService::getInstance()->LongXin($this->request(), 51, $entName[$i]);
