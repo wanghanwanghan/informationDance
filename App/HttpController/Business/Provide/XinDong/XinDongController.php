476b8e29908743b8cc4fac4db105094ff1e49208
wanghan <minglongoc@me.com>
2021-07-02 14:21:24 +0800

    2021-07-02_14:21:24

--- App/HttpController/Business/Provide/XinDong/XinDongController.php
+++ App/HttpController/Business/Provide/XinDong/XinDongController.php
@@ -271,6 +271,7 @@ class XinDongController extends ProvideBase
                 }
                 if ($finance['code'] === 200 && !empty($finance['result'])) {
                     foreach ($finance['result'] as $year => $val) {
+                        if ($year - 0 === 2020) continue;
                         if (!empty($val)) {
                             $data['result']['VENDINC'] = is_numeric($val['VENDINC']) ? $val['VENDINC'] - 0 : null;
                             $data['result']['ASSGRO'] = is_numeric($val['ASSGRO']) ? $val['ASSGRO'] - 0 : null;
