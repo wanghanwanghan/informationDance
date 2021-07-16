daa72a4e343389151de020aafdaa8f2e19342a40
wanghan <minglongoc@me.com>
2021-07-16 09:39:44 +0800

    2021-07-16_09:39:44

---
+++ handleZip.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\HttpController\Models\EntDb\EntDbBasic;
+use App\HttpController\Models\EntDb\EntDbInv;
+use App\HttpController\Models\EntDb\EntDbModify;
+use App\HttpController\Service\CreateConf;
+use App\HttpController\Service\CreateMysqlOrm;
+use App\HttpController\Service\CreateMysqlPoolForEntDb;
+use App\HttpController\Service\CreateMysqlPoolForMinZuJiDiDb;
+use App\HttpController\Service\CreateMysqlPoolForProjectDb;
+use App\HttpController\Service\Zip\ZipService;
+use \EasySwoole\EasySwoole\Core;
+use App\HttpController\Service\CreateDefine;
+use App\HttpController\Service\Common\CommonService;
+use \EasySwoole\Component\Process\Config;
+use \EasySwoole\Component\Process\AbstractProcess;
+use Swoole\Coroutine;
+
+require_once './vendor/autoload.php';
+
+Core::getInstance()->initialize();
+
+class P extends AbstractProcess
+{
+    protected function run($arg)
+    {
+
+    }
+
+}
+
+CreateDefine::getInstance()->createDefine(__DIR__);
+CreateConf::getInstance()->create(__DIR__);
+CreateMysqlPoolForProjectDb::getInstance()->createMysql();
+CreateMysqlPoolForEntDb::getInstance()->createMysql();
+CreateMysqlPoolForMinZuJiDiDb::getInstance()->createMysql();
+CreateMysqlOrm::getInstance()->createMysqlOrm();
+CreateMysqlOrm::getInstance()->createEntDbOrm();
+
+$conf = new Config();
+
+$conf->setEnableCoroutine(true);
+
+$process = new P($conf);
+
+$process->getProcess()->start();
+
+while (Swoole\Process::wait(true)) {
+    var_dump('exit eee');
+    die();
+}
