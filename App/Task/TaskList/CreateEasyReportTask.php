d48d8ece59b6a1ba011ac8e661e91dd9c8993496
wanghan <minglongoc@me.com>
2021-07-29 13:07:54 +0800

    2021-07-29_13:07:54

--- App/Task/TaskList/CreateEasyReportTask.php
+++ App/Task/TaskList/CreateEasyReportTask.php
@@ -563,6 +563,9 @@ class CreateEasyReportTask extends TaskBase implements TaskInterface
         } else {
             if (is_numeric($data[0]['PROGRO_yoy'])) {
                 $val = round($data[0]['PROGRO_yoy'] * 100);
+                CommonService::getInstance()->log4PHP([
+                    '利润同比' => $val
+                ]);
                 if ($val <= -50) {
                     $score = 4;
                 } elseif ($val >= -50 && $val <= -21) {
