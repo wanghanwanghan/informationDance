b5a95032667aef6e0a71e72620803fa61a10b450
wanghan <minglongoc@me.com>
2021-08-03 18:30:34 +0800

    2021-08-03_18:30:34

--- App/HttpController/Business/Admin/GroceryStore/GroceryStoreController.php
+++ App/HttpController/Business/Admin/GroceryStore/GroceryStoreController.php
@@ -87,6 +87,19 @@ class GroceryStoreController extends GroceryStoreBase
             ['id' => 48, 'pid' => 39, 'label' => '其他水上运输辅助活动'],
         ];
 
+        $menu = [
+            ['id' => 1, 'pid' => 0, 'label' => '您要查询哪个行业？'],
+            ['id' => 2, 'pid' => 1, 'label' => '物流'],
+            ['id' => 3, 'pid' => 2, 'label' => '企业名称中含有？'],
+            ['id' => 4, 'pid' => 3, 'label' => '物流'],
+            ['id' => 5, 'pid' => 3, 'label' => '货运'],
+            ['id' => 6, 'pid' => 3, 'label' => '普通货运'],
+            ['id' => 7, 'pid' => 2, 'label' => '经营范围属于？'],
+            ['id' => 8, 'pid' => 7, 'label' => '普通货运'],
+            ['id' => 9, 'pid' => 7, 'label' => '不普通货运'],
+            ['id' => 10, 'pid' => 2, 'label' => '行业类型是？'],
+        ];
+
         $res = [];
 
         $this->traverseMenu($menu, $res, 0);
