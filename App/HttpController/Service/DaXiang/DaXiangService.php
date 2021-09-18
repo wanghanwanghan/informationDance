4a814d4b57bcf210fcc1d1864a1f2c50eb81cac0
wanghan <minglongoc@me.com>
2021-09-18 11:09:21 +0800

    2021-09-18_11:09:21

--- App/HttpController/Service/DaXiang/DaXiangService.php
+++ App/HttpController/Service/DaXiang/DaXiangService.php
@@ -141,6 +141,12 @@ class DaXiangService extends ServiceBase
         //14通行费电子票
         //15二手车销售统一发票
 
+        CommonService::getInstance()->log4PHP([
+            'url' => $url,
+            'access_token' => $token,
+            'arr' => $arr,
+        ]);
+
         return (new CoHttpClient())
             ->useCache(false)
             ->send($url . "?access_token={$token}", $arr, [], [], 'postjson');
