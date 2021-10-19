37b80c9742f7f736105eaf778fd8d5a94b1915c7
wanghan <minglongoc@me.com>
2021-10-19 13:29:51 +0800

    2021-10-19_13:29:51

--- App/HttpController/Business/Test/TestController.php
+++ App/HttpController/Business/Test/TestController.php
@@ -44,6 +44,7 @@ class TestController extends BusinessBase
         $stream = ssh2_exec($conn, 'ls');
 
 
+        stream_set_blocking($stream, true);
         $stream = stream_get_contents($stream);
 
 
