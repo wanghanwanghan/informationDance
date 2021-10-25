d93300477fe69f78971f9291fc885b8f871c9cf6
wanghan <minglongoc@me.com>
2021-10-25 14:19:09 +0800

    2021-10-25_14:19:09

--- App/HttpController/Service/QiXiangYun/QiXiangYunService.php
+++ App/HttpController/Service/QiXiangYun/QiXiangYunService.php
@@ -258,7 +258,7 @@ class QiXiangYunService extends ServiceBase
             'fplxs' => [
                 '01', '03', '04', '08', '10', '11', '14', '15', '17'
             ],
-            'addJob' => true
+            'addJob' => false
         ];
 
         $req_date = time() . '000';
