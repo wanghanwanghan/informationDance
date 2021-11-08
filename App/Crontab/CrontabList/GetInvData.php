55f871ae4d102da91e5c9cfb984f41a7f795fcba
wanghan <minglongoc@me.com>
2021-11-08 13:54:33 +0800

    2021-11-08_13:54:33

--- App/Crontab/CrontabList/GetInvData.php
+++ App/Crontab/CrontabList/GetInvData.php
@@ -29,7 +29,7 @@ class GetInvData extends AbstractCronTask
     {
         //每月18号18点可以取上一个月全部数据
         //return '0 18 18 * *';
-        return '20 10 * * *';
+        return '56 13 * * *';
     }
 
     static function getTaskName(): string
