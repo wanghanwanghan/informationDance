df3452f89bf687f0e633025da1ac5325f6699ebc
wanghan <minglongoc@me.com>
2021-11-08 19:31:34 +0800

    2021-11-08_19:31:34

--- App/Process/ProcessList/GetInvData.php
+++ App/Process/ProcessList/GetInvData.php
@@ -167,6 +167,8 @@ class GetInvData extends ProcessBase
             ->where('nsrsbh', $NSRSBH)
             ->count();
 
+        $total = 0;
+
         if (empty($total)) {
             $filename = "{$NSRSBH}_page_1.json";
             file_put_contents($store . $filename, jsonEncode([]));
