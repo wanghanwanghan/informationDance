3428fb06d0d3054746d9bb7b1cc61731661ef0b9
wanghan <minglongoc@me.com>
2021-11-09 10:27:00 +0800

    2021-11-09_10:27:00

--- App/Process/ProcessList/GetInvData.php
+++ App/Process/ProcessList/GetInvData.php
@@ -157,7 +157,7 @@ class GetInvData extends ProcessBase
         if (empty($info)) return false;
 
         //每个文件存多少张发票
-        $dataInFile = 3000;
+        $dataInFile = 300;
 
         $store = MYJF_PATH . $NSRSBH . DIRECTORY_SEPARATOR . Carbon::now()->format('Ym') . DIRECTORY_SEPARATOR;
         is_dir($store) || mkdir($store, 0755, true);
