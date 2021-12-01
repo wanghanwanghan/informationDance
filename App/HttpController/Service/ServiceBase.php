53134196815809061f84aa5305324afb0563842a
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2021-12-01 15:26:22 +0800

    2021-12-01_15:26:22

--- App/HttpController/Service/ServiceBase.php
+++ App/HttpController/Service/ServiceBase.php
@@ -156,7 +156,7 @@ class ServiceBase
                 ->get();
             if (empty($info)) {
                 $ext['requestId'] = $this->requestId;
-                RequestSourceRecode::create()->data($ext)->save();
+                RequestSourceRecode::create()->addSuffix(date('Y'))->data($ext)->save();
             } else {
                 $info->update($ext);
             }
