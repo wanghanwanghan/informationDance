4bfebe81519e424d4c9763a499f1e9e76dce52a3
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2021-12-03 18:06:30 +0800

    2021-12-03_18:06:30

---
+++ App/HttpController/Business/Provide/YiZhangTong/YiZhangTongController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\HttpController\Business\Provide\YiZhangTong;
+
+use App\Csp\Service\CspService;
+use App\HttpController\Business\Provide\ProvideBase;
+use App\HttpController\Service\BaiXiang\BaiXiangService;
+use App\HttpController\Service\YiZhangTong\YiZhangTongService;
+
+class YiZhangTongController extends ProvideBase
+{
+    function onRequest(?string $action): ?bool
+    {
+        return parent::onRequest($action);
+    }
+
+    function afterAction(?string $actionName): void
+    {
+        parent::afterAction($actionName);
+    }
+
+    function checkResponse($res): bool
+    {
+        if (empty($res[$this->cspKey])) {
+            $this->responseCode = 500;
+            $this->responsePaging = null;
+            $this->responseData = $res[$this->cspKey];
+            $this->spendMoney = 0;
+            $this->responseMsg = '请求超时';
+        } else {
+            $this->responseCode = $res[$this->cspKey]['code'];
+            $this->responsePaging = $res[$this->cspKey]['paging'];
+            $this->responseData = $res[$this->cspKey]['result'];
+            $this->responseMsg = $res[$this->cspKey]['msg'];
+
+            $res[$this->cspKey]['code'] === 200 ?: $this->spendMoney = 0;
+        }
+
+        return true;
+    }
+
+    //产品列表接口
+    function getProductList(): bool
+    {
+        $this->csp->add($this->cspKey, function () {
+            return (new YiZhangTongService())
+                ->setCheckRespFlag(true)
+                ->getProductList();
+        });
+
+        $res = CspService::getInstance()->exec($this->csp, $this->cspTimeout);
+
+        return $this->checkResponse($res);
+    }
+
+
+}
+
+
+
--- App/HttpController/Service/YiZhangTong/YiZhangTongService.php
+++ App/HttpController/Service/YiZhangTong/YiZhangTongService.php
@@ -5,15 +5,22 @@ namespace App\HttpController\Service\YiZhangTong;
 use App\HttpController\Service\Common\CommonService;
 use App\HttpController\Service\HttpClient\CoHttpClient;
 use App\HttpController\Service\ServiceBase;
+use wanghanwanghan\someUtils\control;
 
 class YiZhangTongService extends ServiceBase
 {
     private $test_url;
     private $test_app_id;
     private $test_channel;
-    private $test_service_id;
     private $test_app_secret;
     private $test_rsa_pub;
+
+    private $header = [
+        'Content-Type' => 'application/json;charset=UTF-8',
+    ];
+    private $time;
+    private $ak;
+    private $send_ak;
 
     function __construct()
     {
@@ -21,7 +28,6 @@ class YiZhangTongService extends ServiceBase
         $this->test_url = 'https://smelp-wg-web-stg1.ocft.com/smelp-wg/smelp_wg';
         $this->test_app_id = 'APP_1400';
         $this->test_channel = '1009';
-        $this->test_service_id = 'serviceId字典值';
         $this->test_app_secret = 'dHKq/fx1/7F9lUWkHhv1Dw==';
         $this->test_rsa_pub = <<<str
 -----BEGIN PUBLIC KEY-----
@@ -31,39 +37,64 @@ MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCZKs+rCLRSPeS0ioV9lLDztKF3
 tv6KR7pT0nAqXHIyXQIDAQAB
 -----END PUBLIC KEY-----
 str;
+        $this->time = time() . mt_rand(100, 999);
+
+        $this->ak = control::getUuid(16);
+        $pkey = openssl_pkey_get_public($this->test_rsa_pub);
+        openssl_public_encrypt($this->ak, $send_ak, $pkey);
+        $this->send_ak = base64_encode($send_ak);
     }
 
     private function checkResp($res): array
     {
-        $code = 200;
-
+        $res['responseCode'] - 0 === 0 ? $code = 200 : $code = $res['responseCode'];
 
         $paging = null;
 
+        $result = $res['responseData'];
 
-        $result = null;
-
-
-        $msg = null;
+        $msg = $res['responseMessage'];
 
         return $this->createReturn($code, $paging, $result, $msg);
     }
 
-    function getDptEntDetail($entname = '', $creditcode = '', $licreccode = '')
+    private function createMsg(array $data): string
     {
+        return base64_encode(
+            openssl_encrypt(
+                jsonEncode($data, false), 'AES-128-CBC', $this->ak, OPENSSL_RAW_DATA, 'hanasian12345678'
+            )
+        );
+    }
+
+    private function createSign(string $msg, string $ser_id): string
+    {
+        return hash('sha256', $msg . $this->test_app_secret . $this->time . $ser_id . $this->test_channel);
+    }
+
+    //产品列表接口
+    function getProductList()
+    {
+        $ser_id = '1001100057';
+
+        $msg = $this->createMsg([
+            'channelCode' => 'XWD',
+        ]);
+
         $post_data = [
-            'entname' => trim($entname),
-            'creditcode' => trim($creditcode),
-            'licreccode' => trim($licreccode),
+            'serviceId' => $ser_id,
+            'appId' => $this->test_app_id,
+            'requestId' => control::getUuid(),
+            'timestamp' => $this->time,
+            'channel' => $this->test_channel,
+            'signture' => $this->createSign($msg, $ser_id),
+            'ak' => $this->send_ak,
+            'message' => $msg,
         ];
 
-        $post_data = array_filter($post_data);
-
-        $url = $this->test_url . '/dpt/ent/detail';
-
-        $resp = (new CoHttpClient())->send($url, $post_data, [], [], 'get');
-
-        CommonService::getInstance()->log4PHP($resp);
+        $resp = (new CoHttpClient())
+            ->useCache(false)
+            ->send($this->test_url, $post_data, $this->header, [], 'postjson');
 
         return $this->checkRespFlag ? $this->checkResp($resp) : $resp;
     }
