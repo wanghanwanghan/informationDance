7e6d02732e611936bde74628b0bc5fda2eed5ad0
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2021-12-03 18:43:07 +0800

    2021-12-03_18:43:07

--- App/HttpController/Business/Provide/YiZhangTong/YiZhangTongController.php
+++ App/HttpController/Business/Provide/YiZhangTong/YiZhangTongController.php
@@ -62,7 +62,6 @@ class YiZhangTongController extends ProvideBase
             'productCode' => $this->getRequestData('productCode'),
             'UMCode' => $this->getRequestData('UMCode'),
             'marketPersonnelCode' => $this->getRequestData('marketPersonnelCode'),
-            'bz' => '',
         ];
 
         $this->csp->add($this->cspKey, function () use ($post_data) {
