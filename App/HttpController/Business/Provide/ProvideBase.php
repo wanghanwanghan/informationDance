cac56c61319827fd5b6d27b9296d24f29663fad6
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2021-12-07 16:49:58 +0800

    2021-12-07_16:49:58

--- App/HttpController/Business/Provide/ProvideBase.php
+++ App/HttpController/Business/Provide/ProvideBase.php
@@ -183,12 +183,25 @@ class ProvideBase extends Index
                 $rsa_pri_name = $info->getAttr('rsaPri');
                 //私钥解密
                 $stream = file_get_contents(RSA_KEY_PATH . $rsa_pri_name);
+
+                CommonService::getInstance()->log4PHP($stream);
+
                 $aes_key = control::rsaDecrypt($requestData['encrypt'], $stream, 'pri');
+
+                CommonService::getInstance()->log4PHP($aes_key);
+
                 //aes解密
                 $content = control::aesDecode($requestData['content'], $aes_key, 128, $requestData['encodeMode']);
+
+                CommonService::getInstance()->log4PHP($content);
+
                 $content_arr = jsonDecode($content);
+
+                CommonService::getInstance()->log4PHP($content_arr);
+
                 if (is_array($content_arr)) {
                     $requestData = array_merge($requestData, $content_arr);
+                    CommonService::getInstance()->log4PHP($requestData);
                 }
             }
         }
@@ -200,8 +213,6 @@ class ProvideBase extends Index
         }
 
         $this->requestData = $requestData;
-
-        CommonService::getInstance()->log4PHP($this->requestData);
 
         return (isset($requestData[$key])) ? $requestData[$key] : $default;
     }
