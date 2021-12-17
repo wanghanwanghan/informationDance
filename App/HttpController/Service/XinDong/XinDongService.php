e9abb87fa8391aa9f1db49d914ebd5761f95b5a3
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2021-12-17 16:31:26 +0800

    2021-12-17_16:31:26

--- App/HttpController/Service/XinDong/XinDongService.php
+++ App/HttpController/Service/XinDong/XinDongService.php
@@ -793,45 +793,56 @@ class XinDongService extends ServiceBase
         switch ($label_num) {
             case $label_num <= 2:
                 $after_change_num = 1;
+                $desc='微型，一般指规模在100万以下';
                 break;
             case $label_num <= 4:
                 $after_change_num = 2;
+                $desc='小型C类，一般指规模在100万以上，500万以下';
                 break;
             case $label_num <= 5:
                 $after_change_num = 3;
+                $desc='小型B类，一般指规模在500万以上，1000万以下';
                 break;
             case $label_num <= 7:
                 $after_change_num = 4;
+                $desc='小型A类，一般指规模在1000万以上，3000万以下';
                 break;
             case $label_num <= 9:
                 $after_change_num = 5;
+                $desc='中型C类，一般指规模在3000万以上，5000万以下';
                 break;
             case $label_num <= 12:
                 $after_change_num = 6;
+                $desc='中型B类，一般指规模在5000万以上，8000万以下';
                 break;
             case $label_num <= 14:
                 $after_change_num = 7;
+                $desc='中型A类，一般指规模在8000万以上，1亿以下';
                 break;
             case $label_num <= 18:
                 $after_change_num = 8;
+                $desc='大型C类，一般指规模在1亿以上，5亿以下';
                 break;
             case $label_num <= 23:
                 $after_change_num = 9;
+                $desc='大型B类，一般指规模在5亿以上，10亿以下';
                 break;
             case $label_num <= 27:
                 $after_change_num = 10;
+                $desc='大型A类，一般指规模在10亿以上，50亿以下';
                 break;
             case $label_num <= 32:
                 $after_change_num = 11;
+                $desc='特大型C类，一般指规模在50亿以上，100亿以下';
                 break;
             case $label_num <= 36:
                 $after_change_num = 12;
+                $desc='特大型B类，一般指规模在100亿以上，500亿以下';
                 break;
             default:
                 $after_change_num = 13;
+                $desc='特大型A类，一般指规模在500亿以上';
         }
-
-        $desc = control::getUuid(8);
 
         return ['A' . $after_change_num, $desc];
     }
