83878471926c686d0fe98339c4faf4ffe9633d40
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2021-12-27 14:37:29 +0800

    2021-12-27_14:37:29

--- App/HttpController/Business/Provide/TaoShu/TaoShuController.php
+++ App/HttpController/Business/Provide/TaoShu/TaoShuController.php
@@ -821,8 +821,6 @@ class TaoShuController extends ProvideBase
             'pageSize' => $pageSize . '',
         ];
 
-        CommonService::getInstance()->log4PHP($postData);
-
         $this->csp->add($this->cspKey, function () use ($postData) {
             return (new TaoShuService())
                 ->setCheckRespFlag(true)
