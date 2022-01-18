59317657e4a3fe682c4331b7ed83801a2f22c473
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2022-01-18 16:02:34 +0800

    2022-01-18_16:02:34

--- updateNacaoJGLX.php
+++ updateNacaoJGLX.php
@@ -5,13 +5,12 @@ use App\HttpController\Service\CreateMysqlOrm;
 use App\HttpController\Service\CreateMysqlPoolForEntDb;
 use App\HttpController\Service\CreateMysqlPoolForMinZuJiDiDb;
 use App\HttpController\Service\CreateMysqlPoolForProjectDb;
+use App\HttpController\Service\CreateRedisPool;
 use \EasySwoole\EasySwoole\Core;
 use App\HttpController\Service\CreateDefine;
 use \EasySwoole\Component\Process\Config;
 use \EasySwoole\Component\Process\AbstractProcess;
 use Swoole\Coroutine;
-use EasySwoole\ORM\Db\Connection;
-use EasySwoole\ORM\DbManager;
 
 require_once './vendor/autoload.php';
 
@@ -21,10 +20,67 @@ class P extends AbstractProcess
 {
     protected function run($arg)
     {
-        while (true) {
-            echo '123' . PHP_EOL;
-            sleep(1);
+        $redis = \EasySwoole\RedisPool\Redis::defer('redis');
+        $redis->select(13);
+
+        $fp = fopen('/mnt/tiaoma.log', 'r');
+
+        $i = 1;
+
+        // 0、1、2，其中 0 是提取关键字，1 是切字分词， 2 是获取词性标注。
+
+        while (feof($fp) === false) {
+
+            $str = trim(fgets($fp));
+
+            $arr = explode('|||', $str);
+
+            if (count($arr) !== 3) {
+                continue;
+            }
+
+            $entname = trim($arr[1]);
+            $terms = trim($arr[2]);
+
+            if (empty($entname) || empty($terms)) {
+                continue;
+            }
+
+            $jieba = jieba($terms, 0);
+
+            if (!empty($jieba)) {
+                $jieba = array_map(function ($row) {
+                    if (preg_match('/^[\x{4e00}-\x{9fa5}]+$/u', $row) > 0) {
+                        return $row;
+                    }
+                    return null;
+                }, $jieba);
+            } else {
+                continue;
+            }
+
+            $jieba = array_values(array_filter($jieba));
+
+            if (empty($jieba)) {
+                continue;
+            }
+
+            foreach ($jieba as $one) {
+                //ZINCRBY '每日信动（北京）有限公司' 1 '衬衫'
+                $redis->zInCrBy($entname, 1, $one);
+            }
+
+            $i++;
+
+            if ($i % 100000 === 0) {
+                $o_o = date('Y-m-d H:i:s', time()) . " 已经处理到了第 {$i} 行";
+                file_put_contents(
+                    '/home/wwwroot/informationDance/Static/Log/jieba.log', $o_o . PHP_EOL, FILE_APPEND
+                );
+            }
+
         }
+
     }
 
     protected function onShutDown()
@@ -38,34 +94,17 @@ class P extends AbstractProcess
     }
 }
 
+//mysql
 CreateDefine::getInstance()->createDefine(__DIR__);
 CreateConf::getInstance()->create(__DIR__);
-CreateMysqlPoolForProjectDb::getInstance()->createMysql();
-CreateMysqlPoolForEntDb::getInstance()->createMysql();
-CreateMysqlPoolForMinZuJiDiDb::getInstance()->createMysql();
-CreateMysqlOrm::getInstance()->createMysqlOrm();
-CreateMysqlOrm::getInstance()->createEntDbOrm();
+//CreateMysqlPoolForProjectDb::getInstance()->createMysql();
+//CreateMysqlPoolForEntDb::getInstance()->createMysql();
+//CreateMysqlPoolForMinZuJiDiDb::getInstance()->createMysql();
+//CreateMysqlOrm::getInstance()->createMysqlOrm();
+//CreateMysqlOrm::getInstance()->createEntDbOrm();
 
-//orm
-$config = new \EasySwoole\ORM\Db\Config();
-
-//数据库配置
-$config->setHost('rm-2ze5r17pbzd3l7rakyo.mysql.rds.aliyuncs.com');
-$config->setPort(3306);
-$config->setUser('mrxd');
-$config->setPassword('zbxlbj@2018*()');
-$config->setDatabase('shang_pin_tiao_ma');
-$config->setCharset('utf8mb4');
-
-//链接池配置
-$config->setGetObjectTimeout(3.0); //设置获取连接池对象超时时间
-$config->setIntervalCheckTime(30 * 1000); //设置检测连接存活执行回收和创建的周期
-$config->setMaxIdleTime(15); //连接池对象最大闲置时间(秒)
-$config->setMaxObjectNum(50); //设置最大连接池存在连接对象数量
-$config->setMinObjectNum(5); //设置最小连接池存在连接对象数量
-$config->setAutoPing(5); //设置自动ping客户端链接的间隔
-
-DbManager::getInstance()->addConnection(new Connection($config), 'SPTM');
+//redis pool
+CreateRedisPool::getInstance()->createRedis();
 
 $conf = new Config();
 
