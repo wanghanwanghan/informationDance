7f85e3304a8843882b1465ed13107d11debbf847
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2022-02-11 13:47:30 +0800

    2022-02-11_13:47:30

--- send_to_mayi.php
+++ send_to_mayi.php
@@ -30,25 +30,72 @@ Core::getInstance()->initialize();
 
 class send_to_mayi extends AbstractProcess
 {
+    public $baseUrl = 'http://api.qixiangyun.com/v1/';
+    public $appkey = '10002009';
+    public $secret = 'OkdvOUZ3fxb3mNxtko69nedNPgkjY2E8gBP7x7opkoY5tSyO';
+    public $nsrsbh = '91110108MA01KPGK0L';
+
+    function createToken(): string
+    {
+        $url = $this->baseUrl . 'AGG/oauth2/login';
+
+        $data = [
+            'grant_type' => 'client_credentials',
+            'client_appkey' => $this->appkey,
+            'client_secret' => md5($this->secret),
+        ];
+
+        $header = [
+            'content-type' => 'application/json;charset=UTF-8'
+        ];
+
+        $res = (new CoHttpClient())
+            ->useCache(false)->setEx(0.3)
+            ->needJsonDecode(true)
+            ->send($url, $data, $header, [], 'postjson');
+
+        return $res['value']['access_token'];
+    }
+
     protected function run($arg)
     {
         for ($i = 1; $i <= 24; $i++) {
 
             $kpyf = Carbon::now()->subMonths($i)->format('Ym');
 
+            $url = $this->baseUrl . 'FP/getFpxzStatus';
 
+            $data = [
+                'nsrsbh' => $this->nsrsbh,
+                'kpyf' => $kpyf - 0,//Ym
+                'jxxbzs' => ['jx', 'xx'],
+                'fplxs' => ['01', '03', '04', '08', '10', '11', '14', '15', '17'],
+                'addJob' => false
+            ];
 
+            $req_date = time() . mt_rand(100, 999);
 
+            $token = $this->createToken();
 
+            $sign = base64_encode(
+                md5('POST_' . md5(jsonEncode($data, false)) . '_' . $req_date . '_' . $token . '_' . $this->secret)
+            );
 
+            $req_sign = "API-SV1:{$this->appkey}:" . $sign;
 
+            $header = [
+                'content-type' => 'application/json;charset=UTF-8',
+                'access_token' => $token,
+                'req_date' => $req_date,
+                'req_sign' => $req_sign,
+            ];
 
+            $res = (new CoHttpClient())
+                ->useCache(false)
+                ->needJsonDecode(false)
+                ->send($url, $data, $header, [], 'postjson');
 
-
-
-
-
-
+            var_dump($res);
         }
     }
 
