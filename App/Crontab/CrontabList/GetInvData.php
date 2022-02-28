a88321463905ddf205c7389a082b261761038e53
lemon li <lemonli@lemondeMacBook-Pro.local>
2022-02-28 15:47:28 +0800

    update crontab time

--- App/Crontab/CrontabList/GetInvData.php
+++ App/Crontab/CrontabList/GetInvData.php
@@ -30,7 +30,7 @@ class GetInvData extends AbstractCronTask
     {
         //每月19号凌晨4点可以取上一个月全部数据
         //return '0 4 19 * *';
-        return '03 15 28 * * ';//'39 21 14 * *';
+        return '52 15 28 * * ';//'39 21 14 * *';
     }
 
     static function getTaskName(): string
