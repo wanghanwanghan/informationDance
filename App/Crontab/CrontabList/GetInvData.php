882b45852c53761de23d72d1edc16f388dab0d10
lemon li <lemonli@lemondeMacBook-Pro.local>
2022-03-02 16:04:13 +0800

    add code

--- App/Crontab/CrontabList/GetInvData.php
+++ App/Crontab/CrontabList/GetInvData.php
@@ -30,7 +30,7 @@ class GetInvData extends AbstractCronTask
     {
         //每月19号凌晨4点可以取上一个月全部数据
         //return '0 4 19 * *';
-        return '03 16 02 * * ';//'39 21 14 * *';
+        return '06 16 02 * * ';//'39 21 14 * *';
     }
 
     static function getTaskName(): string
