a834dc672af9a3a1b28b7d397fd326a6059e152c
lemon li <lemonli@lemondeMacBook-Pro.local>
2022-03-10 18:25:16 +0800

    add code

--- App/Crontab/CrontabList/GetAuthBook.php
+++ App/Crontab/CrontabList/GetAuthBook.php
@@ -35,7 +35,7 @@ class GetAuthBook extends AbstractCronTask
     static function getRule(): string
     {
         //每分钟执行一次
-        return '25 18 10 * *';
+        return '28 18 10 * *';
 
     }
 
