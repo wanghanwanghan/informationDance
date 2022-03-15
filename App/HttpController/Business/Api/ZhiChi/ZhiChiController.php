ae787e288ba6291658ed0fa5c32c6c1d9f45b9a8
lemon li <lemonli@lemondeMacBook-Pro.local>
2022-03-15 16:58:30 +0800

    add code

--- App/HttpController/Business/Api/ZhiChi/ZhiChiController.php
+++ App/HttpController/Business/Api/ZhiChi/ZhiChiController.php
@@ -20,25 +20,6 @@ class ZhiChiController extends ChuangLanBase
 
     function directUrl(){
         $res = (new ZhiChiService())->directUrl();
-        return $this->writeJson($res['code'], null, $res['data'], $res['message']);
-    }
-    function checkResponse($res)
-    {
-        if (empty($res[$this->cspKey])) {
-            $this->responseCode = 500;
-            $this->responsePaging = null;
-            $this->responseData = $res[$this->cspKey];
-            $this->spendMoney = 0;
-            $this->responseMsg = '请求超时';
-        } else {
-            $this->responseCode = $res[$this->cspKey]['code'];
-            $this->responsePaging = $res[$this->cspKey]['paging'];
-            $this->responseData = $res[$this->cspKey]['data'];
-            $this->responseMsg = $res[$this->cspKey]['message'];
-
-            $res[$this->cspKey]['code'] === 200 ?: $this->spendMoney = 0;
-        }
-
-        return true;
+        return $this->writeJson($res['ret_code'], null, $res['item'], $res['ret_msg']);
     }
 }
