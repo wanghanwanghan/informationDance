6f4df9f7a819b7a5f7de37bc405a81931415ad07
lemon li <lemonli@lemondeMacBook-Pro.local>
2022-03-17 16:30:51 +0800

    add code

--- App/HttpController/Business/AdminRoles/User/UserController.php
+++ App/HttpController/Business/AdminRoles/User/UserController.php
@@ -8,6 +8,8 @@ use App\HttpController\Models\Api\PurchaseInfo;
 use App\HttpController\Models\Api\PurchaseList;
 use App\HttpController\Models\Api\User;
 use App\HttpController\Models\Api\Wallet;
+use App\HttpController\Models\Provide\RequestApiInfo;
+use App\HttpController\Models\Provide\RequestUserApiRelationship;
 use App\HttpController\Models\Provide\RequestUserInfo;
 use App\HttpController\Service\CreateConf;
 use App\HttpController\Service\Pay\ali\aliPayService;
@@ -67,6 +69,37 @@ class UserController extends UserBase
             'roles'=>$info->roles,
             'id'=>$info->id
         ];
+        return $this->writeJson(200, '',$data, '成功');
+    }
+
+    /**
+     * 根据用户获取用户的接口明细
+     */
+    function getApiListByUser(){
+        $appId = $this->getRequestData('username') ?? '';
+        $token = $this->getRequestData('token') ?? '';
+        if (empty($token) || empty($appId)) return $this->writeJson(201, null, null, '参数不可以为空');
+        $info = RequestUserInfo::create()->where("token = '{$token}' and appId = '{$appId}'")->get();
+        if(empty($info)){
+            return $this->writeJson(201, null, null, '用户未登录');
+        }
+        $shipList = RequestUserApiRelationship::create()->where(" userId = {$info->id}")->all();
+        $data = [];
+        foreach ($shipList as $item) {
+            $apiId = $item->getAttr('apiId');
+            $apiInfo = RequestApiInfo::create()->where("id={$apiId}")->get();
+            $data[$apiId] = [
+                'path' => $apiInfo->path,
+                'name' => $apiInfo->name,
+                'desc' => $apiInfo->desc,
+                'source' => $apiInfo->source,
+                'price' => $apiInfo->price,
+                'status' => $apiInfo->status,
+                'apiDoc' => $apiInfo->apiDoc,
+                'created_at' => $apiInfo->created_at,
+                'updated_at' => $apiInfo->updated_at,
+            ];
+        }
         return $this->writeJson(200, '',$data, '成功');
     }
 }
