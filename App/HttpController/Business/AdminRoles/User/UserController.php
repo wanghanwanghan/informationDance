c402d037e6ff7f955aaeed7161827bf68ac45939
lemon li <lemonli@lemondeMacBook-Pro.local>
2022-03-22 14:51:43 +0800

    add code

--- App/HttpController/Business/AdminRoles/User/UserController.php
+++ App/HttpController/Business/AdminRoles/User/UserController.php
@@ -462,8 +462,8 @@ class UserController extends UserBase
         foreach ($emptyTypes as $emptyType) {
             $emptyType = explode('-',$emptyType);
             $fun = BarchChargingLog::$type_map[$emptyType['0']][$emptyType['1']];
-//            dingAlarm('导出日志-方法名',['$fun'=>$fun,'$emptyType'=>json_encode($emptyType)]);
-            list($filePath,$data) = $this->taoshuRegisterInfo($nameArr);
+            dingAlarm('导出日志-方法名',['$fun'=>$fun,'$emptyType'=>json_encode($emptyType)]);
+            list($filePath,$data) = $this->{$fun}($nameArr);
             $fileArr[$emptyType] = $filePath;
             $this->inseartChargingLog($info->id,$batchNum,$emptyType,$data,$filePath);
         }
