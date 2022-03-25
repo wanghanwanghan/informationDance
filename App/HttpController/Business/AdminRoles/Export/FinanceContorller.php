e10723371c814c8e918ab113807d388c0054337e
lemon li <lemonli@lemondeMacBook-Pro.local>
2022-03-25 14:30:39 +0800

    add code

--- App/HttpController/Business/AdminRoles/Export/FinanceContorller.php
+++ App/HttpController/Business/AdminRoles/Export/FinanceContorller.php
@@ -8,29 +8,32 @@ use App\HttpController\Service\LongXin\LongXinService;
 class FinanceContorller  extends UserController
 {
     public $kidTpye = [
-        'ASSGRO' => ['key'=>'ASSGRO_yoy','value'=>'资产总额'],
-        'LIAGRO' => ['key'=>'LIAGRO_yoy','value'=>'负债总额'],
-        'VENDINC' => ['key'=>'VENDINC_yoy','value'=>'营业总收入'],
-        'MAIBUSINC' => ['key'=>'MAIBUSINC_yoy','value'=>'主营业务收入'],
-        'PROGRO' => ['key'=>'PROGRO_yoy','value'=>'利润总额'],
-        'NETINC' => ['key'=>'NETINC_yoy','value'=>'净利润'],
-        'RATGRO' => ['key'=>'RATGRO_yoy','value'=>'纳税总额'],
-        'TOTEQU' => ['key'=>'TOTEQU_yoy','value'=>'所有者权益合计'],
-        'SOCNUM' => ['key'=>'','value'=>'社保人数'],
+        'ASSGRO' => '资产总额',
+        'LIAGRO' => '负债总额',
+        'VENDINC' => '营业总收入',
+        'MAIBUSINC' => '主营业务收入',
+        'PROGRO' => '利润总额',
+        'NETINC' => '净利润',
+        'RATGRO' => '纳税总额',
+        'TOTEQU' => '所有者权益合计',
+        'SOCNUM' => '社保人数',
     ];
     /**
      * 西南年报
      */
     public function xinanGetFinanceNotAuth($entNames,$kidTypes)
     {
-        $kidTypeList = explode('-',$kidTypes);
+        $kidTypes = explode('/',$kidTypes);
+        $kidTypeList = explode('-',$kidTypes['0']);
+        $kidTypesKeyArr = explode(',',$kidTypes['1']);
         $fileName = date('YmdHis', time()) . '年报.csv';
         $file = TEMP_FILE_PATH . $fileName;
-        $insertData = [
-            '公司名称', '年', '资产总额', '负债总额', '营业总收入', '主营业务收入', '利润总额', '净利润', '纳税总额','所有者权益合计','社保人数'
-        ];
+        $insertData = ['公司名称', '年'];
+        foreach ($kidTypesKeyArr as $item) {
+            $insertData[] = $this->kidTpye[$item]['value'];
+        }
         file_put_contents($file, implode(',', $this->replace($insertData)) . PHP_EOL, FILE_APPEND);
-        $data = [];
+        $resData = [];
         foreach ($entNames as $ent) {
             $postData = [
                 'entName' => $ent['entName'],
@@ -39,10 +42,23 @@ class FinanceContorller  extends UserController
                 'dataCount' => $kidTypeList['1'],//取最近几年的
             ];
             $res = (new LongXinService())->getFinanceData($postData, false);
+            if(empty($res['data'])){
+                continue;
+            }
+            foreach ($res['data'] as $year=>$datum) {
+                $insertData = [
+                    $ent['entName'],
+                    $year,
+                ];
+                foreach ($kidTypesKeyArr as $key=>$item) {
+                    $insertData[] = $datum[$key];
+                }
+                $resData[] = $insertData;
+                file_put_contents($file, implode(',', $this->replace($insertData)) . PHP_EOL, FILE_APPEND);
+            }
             dingAlarm('年报',['$entName'=>$ent['entName'],'$data'=>json_encode($res)]);
-
         }
-
+        return [$fileName, $resData];
     }
 
 }
