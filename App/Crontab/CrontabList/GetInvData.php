8f7051c15ba9fd67380c818177ac2dc0bde2012c
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2022-03-30 16:47:38 +0800

    2022-03-30_16:47:38

--- App/Crontab/CrontabList/GetInvData.php
+++ App/Crontab/CrontabList/GetInvData.php
@@ -31,7 +31,7 @@ class GetInvData extends AbstractCronTask
     {
         //每月19号凌晨4点可以取上一个月全部数据
         //return '0 4 19 * *';
-        return '40 14 28 * * ';
+        return '52 16 30 * * ';
     }
 
     static function getTaskName(): string
@@ -189,7 +189,7 @@ class GetInvData extends AbstractCronTask
                 ];
 
                 //生产环境先不通知
-                if ($oneReadyToSend->belong - 0 === 36) {
+                if ($oneReadyToSend->belong - 0 === 41) {
                     CommonService::getInstance()->log4PHP([$body], 'info', 'notify_fp');
 
                     $ret = (new CoHttpClient())
