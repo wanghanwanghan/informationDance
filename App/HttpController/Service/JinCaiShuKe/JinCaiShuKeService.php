ebeb17da3f79258b16999bbe56f4c6b7c7b93667
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2022-04-08 15:05:39 +0800

    2022-04-08_15:05:39

--- App/HttpController/Service/JinCaiShuKe/JinCaiShuKeService.php
+++ App/HttpController/Service/JinCaiShuKe/JinCaiShuKeService.php
@@ -44,10 +44,8 @@ class JinCaiShuKeService extends ServiceBase
                 'serviceid' => $serviceid,
             ]);
 
-        CommonService::getInstance()->log4PHP(['签名体' => $str]);
-
         return $signType === '0' ?
-            hash_hmac('sha256', $str, $this->appSecret) :
+            base64_encode(hash_hmac('sha256', $str, $this->appSecret)) :
             strtoupper(md5(
                 $this->appKey .
                 $this->appSecret .
