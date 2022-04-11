2e64b234ba55b65cdbaacd8f6669d9c2e91ebe2f
lemon li <lemonli@lemondeMacBook-Pro.local>
2022-04-11 15:59:33 +0800

    add

--- App/HttpController/Business/AdminRoles/Export/IntellectualPropertyContorller.php
+++ App/HttpController/Business/AdminRoles/Export/IntellectualPropertyContorller.php
@@ -9,7 +9,39 @@ use App\HttpController\Service\LongDun\LongDunService;
 class IntellectualPropertyContorller extends UserController
 {
     /**
-     * 企查查软件著作权
+     * 企查查--软件著作权
+     */
+    public function qccGetSearchSoftwareCr($entNames){
+//        dingAlarm('涉税处罚公示',['fhGetSatpartyChufa'=>json_encode($entNames)]);
+//        $fileName = date('YmdHis', time()) . '涉税处罚公示.csv';
+//        $file = TEMP_FILE_PATH . $fileName;
+//        $header = [
+//
+//        ];
+//        file_put_contents($file, implode(',', $header) . PHP_EOL, FILE_APPEND);
+        $resData = [];
+        foreach ($entNames as $ent) {
+            $data = $this->getSearchSoftwareCr($ent['entName'], 1);
+            dingAlarm('软件著作权',['$datum'=>json_encode($data),'entName'=>$ent['entName']]);
+//            if (empty($data['satparty_chufaList'])) {
+//                continue;
+//            }
+//            if (isset($data['totalPageNum']) && $data['totalPageNum'] > 1) {
+//                for ($i = 2; $i <= $data['totalPageNum']; $i++) {
+//                    $data2 = $this->getSearchSoftwareCr($ent['entName'], 1);
+//                    $data['satparty_chufaList'] = array_merge($data['satparty_chufaList'], $data2['satparty_chufaList']);
+//                }
+//            }
+//            foreach ($data['satparty_chufaList'] as $datum) {
+////                dingAlarm('涉税处罚公示',['$datum'=>json_encode($datum),'entName'=>$ent['entName']]);
+////                $resData[] = $this->fhGetSatpartyChufaDetail($datum['entryId'], $file, $ent['entName']);
+//            }
+        }
+//        return [$fileName, $resData];
+    }
+
+    /**
+     * 企查查--软件著作权
      */
     public function getSearchSoftwareCr($entName,$page){
         $postData = [
@@ -17,12 +49,19 @@ class IntellectualPropertyContorller extends UserController
             'pageIndex' => $page,
             'pageSize' => 10,
         ];
-
         return (new LongDunService())->get(CreateConf::getInstance()->getConf('longdun.baseUrl') . 'CopyRight/SearchSoftwareCr', $postData);
     }
+    /**
+     * 企查查--商标详情
+     */
+    public function getTmSearchDetail($id)
+    {
+        $postData = ['id' => $id];
+        $res = (new LongDunService())->get(CreateConf::getInstance()->getConf('longdun.baseUrl') . 'tm/GetDetails', $postData);
+    }
 
-    /*
-     * 作品著作权
+    /**
+     * 企查查--作品著作权
      */
     public function getSearchCopyRight($entName,$page){
         $postData = [
@@ -34,8 +73,8 @@ class IntellectualPropertyContorller extends UserController
         return (new LongDunService())->get(CreateConf::getInstance()->getConf('longdun.baseUrl') . 'CopyRight/SearchCopyRight', $postData);
     }
 
-    /*
-     * 企业证书查询
+    /**
+     * 企查查--企业证书查询
      */
     public function getSearchCertification($entName,$page){
         $postData = [
@@ -46,9 +85,20 @@ class IntellectualPropertyContorller extends UserController
 
         return (new LongDunService())->get(CreateConf::getInstance()->getConf('longdun.baseUrl') . 'ECICertification/SearchCertification', $postData);
      }
+    /**
+     * 企查查--企业证书查询详情
+     */
+    public function getSearchCertificationDetail()
+    {
+        $id = $this->request()->getRequestParam('id');
 
-     /*
-      * 商标
+        $postData = ['certId' => $id];
+
+        $res = (new LongDunService())->get(CreateConf::getInstance()->getConf('longdun.baseUrl') . 'ECICertification/GetCertificationDetailById', $postData);
+
+    }
+     /**
+      * 企查查--商标
       */
     public function getTmSearch($entName,$page){
         $postData = [
@@ -59,8 +109,8 @@ class IntellectualPropertyContorller extends UserController
         return (new LongDunService())->get(CreateConf::getInstance()->getConf('longdun.baseUrl') . 'tm/Search', $postData);
     }
 
-    /*
-     * 专利
+    /**
+     * 企查查--专利
      */
     public function getPatentV4Search($entName,$page){
         $postData = [
@@ -71,4 +121,16 @@ class IntellectualPropertyContorller extends UserController
         return (new LongDunService())->get(CreateConf::getInstance()->getConf('longdun.baseUrl') . 'PatentV4/Search', $postData);
     }
 
+    /**
+     * 企查查--专利详情
+     */
+    public function getPatentV4SearchDetail()
+    {
+        $id = $this->request()->getRequestParam('id');
+
+        $postData = ['id' => $id];
+
+        $res = (new LongDunService())->get(CreateConf::getInstance()->getConf('longdun.baseUrl') . 'PatentV4/GetDetails', $postData);
+
+    }
 }
