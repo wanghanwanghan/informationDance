e4fc06a030e90e2fd80fd30be8d56c443a8bf607
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2022-04-12 14:14:20 +0800

    2022-04-12_14:14:20

--- App/HttpController/Service/LongXin/LongXinService.php
+++ App/HttpController/Service/LongXin/LongXinService.php
@@ -108,7 +108,7 @@ class LongXinService extends ServiceBase
     //公司名称换取entid
     private function getEntid($entName, string $includegeti = '0'): ?string
     {
-        $ctype = preg_match('/\d{5}/', $entName) ? 1 : 3;
+        $ctype = preg_match('/\d{5}/', $entName) ? '1' : '3';
 
         $arr = [
             'key' => $entName,
