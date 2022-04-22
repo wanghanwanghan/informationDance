80bb4a5b57e9e93b7a9bf42208fc3e3fd3bb71b7
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2022-04-22 11:44:55 +0800

    2022-04-22_11:44:55

--- App/Crontab/CrontabList/GetAuthBook.php
+++ App/Crontab/CrontabList/GetAuthBook.php
@@ -48,8 +48,8 @@ class GetAuthBook extends AbstractCronTask
         $url_arr = [
             36 => 'https://invoicecommercial.test.dl.alipaydev.com/api/wezTech/collectNotify',//dev
             //36 => 'http://invoicecommercial.dev.dl.alipaydev.com/api/wezTech/collectNotify',//test rsa和dev一样
-            41 => 'https://invoicecommercial-pre.antfin.com/api/wezTech/collectNotify',//pre
-            42 => 'https://invoicecommercial.antfin.com/api/wezTech/collectNotify',//pro
+            41 => 'https://invoicecommercial.antfin.com/api/wezTech/collectNotify',//pre 和 pro 交换了
+            42 => 'https://invoicecommercial-pre.antfin.com/api/wezTech/collectNotify',//pro 和 pre 交换了
         ];
 
         $ids = $this->getNeedSealID();//123123
--- App/Crontab/CrontabList/GetInvData.php
+++ App/Crontab/CrontabList/GetInvData.php
@@ -92,8 +92,8 @@ class GetInvData extends AbstractCronTask
         $url_arr = [
             36 => 'https://invoicecommercial.test.dl.alipaydev.com/api/wezTech/collectNotify',//dev
             //36 => 'http://invoicecommercial.dev.dl.alipaydev.com/api/wezTech/collectNotify',//test rsa和dev一样
-            41 => 'https://invoicecommercial-pre.antfin.com/api/wezTech/collectNotify',//pre
-            42 => 'https://invoicecommercial.antfin.com/api/wezTech/collectNotify',//pro
+            41 => 'https://invoicecommercial.antfin.com/api/wezTech/collectNotify',//pre 和 pro 交换了
+            42 => 'https://invoicecommercial-pre.antfin.com/api/wezTech/collectNotify',//pro 和 pre 交换了
         ];
 
         $total = AntAuthList::create()
