78e3816d59d5310a3dab646860d851a7781e9e5b
tian <1270331569@qq.com>
2022-04-27 19:02:29 +0800

    test if can get conf

--- App/HttpController/Business/Api/XinDong/XinDongController.php
+++ App/HttpController/Business/Api/XinDong/XinDongController.php
@@ -777,9 +777,13 @@ eof;
         if (!$companyId) {
             $this->writeJson(201, null, null, '参数缺失');
         }
-        $res = sqlRaw("select * form company limit 1");
+        // $res = sqlRaw("select * form company limit 1");
         // $res = Company::create()->where('id', 1)->get();
         // $res = (new XinDongService())->getCompanyBasicInfo();
+        $res = [
+            CreateConf::getInstance()->getConf('env.mysqlDatabaseRDS_3_prism1'),
+            CreateConf::getInstance()->getConf('env.mysqlDatabaseRDS_3_nic_code'),
+        ];
         return $this->writeJson(200, intval($responseArr['hits']['total'])/$postData['size'], $res, '成功', true, []);
     }
 }
