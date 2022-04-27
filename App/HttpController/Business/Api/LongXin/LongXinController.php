9e085261d40c11c361b2921c5c58896acf116625
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2022-04-27 18:35:43 +0800

    2022-04-27_18:35:43

--- App/HttpController/Business/Api/LongXin/LongXinController.php
+++ App/HttpController/Business/Api/LongXin/LongXinController.php
@@ -367,10 +367,11 @@ class LongXinController extends LongXinBase
             if ($mergeData === 1) {
                 $res = (new LongXinService())->getFinanceBaseMergeData($postData, false);
             } else {
-                $res = (new LongXinService())->setCal(true)->getFinanceData($postData, false);
+                $res = (new LongXinService())
+                    ->setCal(true)
+                    ->setCheckRespFlag(false)
+                    ->getFinanceData($postData, false);
             }
-
-            CommonService::getInstance()->log4PHP($res);
 
             $charge = ChargeService::getInstance()->LongXin($this->request(), 51, $entName[$i]);
 
