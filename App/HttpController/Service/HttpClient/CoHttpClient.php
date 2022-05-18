e52ddb11f2953c6289fb7d4c4b69013fe1edb758
lemon li <lemonli@lemondeMacBook-Pro.local>
2022-05-18 14:38:11 +0800

    add

--- App/HttpController/Service/HttpClient/CoHttpClient.php
+++ App/HttpController/Service/HttpClient/CoHttpClient.php
@@ -62,12 +62,10 @@ class CoHttpClient extends ServiceBase
 //            CommonService::getInstance()->log4PHP([$url,$postData],'info','http_return_data');
 
             if ($method === 'GET') $data = $request->get();
-            CommonService::getInstance()->log4PHP([$url,$postData,$GLOBALS['HTTP_RAW_POST_DATA']],'info','http_return_data');
 
             //整理结果
             $data = $data->getBody();
 //            dingAlarm('http返回',['$url'=>$url,'$data'=>json_encode($data),'$postData'=>json_encode($postData)]);
-          CommonService::getInstance()->log4PHP([$url,$postData,$data,$headers],'info','http_return_data');
         } catch (\Exception $e) {
             $this->writeErr($e, 'CoHttpClient');
             return ['coHttpErr' => 'error'];
