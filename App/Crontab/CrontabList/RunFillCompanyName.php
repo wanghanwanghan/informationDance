58b2ffea1f449ce8799bd3d147446e049c8d923f
tian <1270331569@qq.com>
2022-05-18 17:41:39 +0800

    test

--- App/Crontab/CrontabList/RunFillCompanyName.php
+++ App/Crontab/CrontabList/RunFillCompanyName.php
@@ -81,8 +81,8 @@ class RunFillCompanyName extends AbstractCronTask
     function run(int $taskId, int $workerIndex): bool
     {
         
-        for($i=1;$i<=200;$i++){
-            $size = 1000 ;
+        for($i=1;$i<=2;$i++){
+            $size = 10 ;
             $sql = " select id from  `company_name`  order by id  desc limit 1 ";
             $list = sqlRaw($sql, CreateConf::getInstance()->getConf('env.mysqlDatabase'));
             $minId = 0;
