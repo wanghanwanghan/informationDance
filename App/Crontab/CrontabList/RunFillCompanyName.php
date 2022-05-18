9b74b100fdfee8a1e1253c96104500783c61714e
tian <1270331569@qq.com>
2022-05-18 14:36:48 +0800

    test

--- App/Crontab/CrontabList/RunFillCompanyName.php
+++ App/Crontab/CrontabList/RunFillCompanyName.php
@@ -81,7 +81,7 @@ class RunFillCompanyName extends AbstractCronTask
     {
        
         $sql = "
-        select id from  `company`  order by id  desc limit 1
+        select id from  `admin_new_api`  order by id  desc limit 1
         ";
         $list = sqlRaw($sql, CreateConf::getInstance()->getConf('env.mysqlDatabase'));
         CommonService::getInstance()->log4PHP('RunFillCompanyName'.
