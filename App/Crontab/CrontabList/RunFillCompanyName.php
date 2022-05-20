aa828a64a015adde4db928e6b12b731298fcea92
tian <1270331569@qq.com>
2022-05-20 19:10:30 +0800

    test

--- App/Crontab/CrontabList/RunFillCompanyName.php
+++ App/Crontab/CrontabList/RunFillCompanyName.php
@@ -143,7 +143,9 @@ class RunFillCompanyName extends AbstractCronTask
         $from = $from + $size +1 ;
         $this->addById($from,$size,$configArr,$tableName);
 
-        
+        $from = $from + $size +1 ;
+        $this->addById($from,$size,$configArr,$tableName);
+
         return true ;  
     }
 
