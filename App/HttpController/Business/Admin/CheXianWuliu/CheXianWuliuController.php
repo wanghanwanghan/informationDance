ae90da7fea1fe0b7b7d831da923eeee999efb525
tian <1270331569@qq.com>
2022-05-26 21:12:25 +0800

    test

--- App/HttpController/Business/Admin/CheXianWuliu/CheXianWuliuController.php
+++ App/HttpController/Business/Admin/CheXianWuliu/CheXianWuliuController.php
@@ -67,7 +67,7 @@ class CheXianWuliuController extends CheXianWuliuBase
       
         $idsArr = explode(',',$idsStr);
         CommonService::getInstance()->log4PHP( 'setIsOk '.json_encode($idsArr));
-        return $this->writeJson(200, null, $res, '操作成功(生效'.$succeedNum.'个)');
+        return $this->writeJson(200, null, [], '操作成功(生效个)');
         $succeedNum = 0 ;
         foreach($idsArr as $id){ 
             if(
