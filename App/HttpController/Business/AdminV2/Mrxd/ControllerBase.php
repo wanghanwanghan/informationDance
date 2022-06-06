b857c3064b24c3dd1b77d59f10f487787c977a41
tian <1270331569@qq.com>
2022-06-06 16:30:26 +0800

    test

--- App/HttpController/Business/AdminV2/Mrxd/ControllerBase.php
+++ App/HttpController/Business/AdminV2/Mrxd/ControllerBase.php
@@ -124,27 +124,27 @@ class ControllerBase extends Index
 
         $raw = jsonDecode($string);
         $form = $this->request()->getRequestParam();
-        CommonService::getInstance()->log4PHP(
-            [
-                'getRequestData',
-                'string' => $string,
-                'raw' => $raw,
-                'form' => $form,
-            ]
-        );
+        // CommonService::getInstance()->log4PHP(
+        //     [
+        //         'getRequestData',
+        //         'string' => $string,
+        //         'raw' => $raw,
+        //         'form' => $form,
+        //     ]
+        // );
         !empty($raw) ?: $raw = [];
         !empty($form) ?: $form = [];
 
         $requestData = array_merge($raw, $form);
-        CommonService::getInstance()->log4PHP(
-            [
-                'getRequestData',
-                'string' => $string,
-                'raw' => $raw,
-                'form' => $form,
-                'requestData' => $requestData,
-            ]
-        );
+        // CommonService::getInstance()->log4PHP(
+        //     [
+        //         'getRequestData',
+        //         'string' => $string,
+        //         'raw' => $raw,
+        //         'form' => $form,
+        //         'requestData' => $requestData,
+        //     ]
+        // );
         if($key){
             return (isset($requestData[$key])) ? $requestData[$key] : $default;
         }
