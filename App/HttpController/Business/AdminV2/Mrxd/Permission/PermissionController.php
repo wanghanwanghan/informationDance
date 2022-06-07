d5ea60750e993aae5ca654d8a8ae729be78edfc6
liyunxian <908770505@qq.com>
2022-06-07 15:39:26 +0800

    add

--- App/HttpController/Business/AdminV2/Mrxd/Permission/PermissionController.php
+++ App/HttpController/Business/AdminV2/Mrxd/Permission/PermissionController.php
@@ -109,8 +109,8 @@ class PermissionController extends ControllerBase
             'class' => $requestData['class'] ? $requestData['class']: $info['class'],
             'parent_id' => $requestData['parent_id'] ? $requestData['parent_id']: $info['parent_id'],
             'remark' => $requestData['remark'] ? $requestData['remark']: $info['remark'],
-            'link' => $requestData['remark'] ? $requestData['remark']: $info['link'],
-            'icon' => $requestData['remark'] ? $requestData['remark']: $info['icon'],
+            'link' => $requestData['link'] ? $requestData['link']: $info['link'],
+            'icon' => $requestData['icon'] ? $requestData['icon']: $info['icon'],
         ]);
         return $this->writeJson();
     }
