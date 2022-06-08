f847e6490d00dc02e1f6654eb4b9c90f1e5a191d
tian <1270331569@qq.com>
2022-06-08 15:44:16 +0800

    路由改下

--- App/HttpController/AdminV2Router.php
+++ App/HttpController/AdminV2Router.php
@@ -76,7 +76,7 @@ class AdminV2Router
             $routeCollector->addRoute(['GET', 'POST'], '/updateConfigStatus', $prefix . 'updateConfigStatus');
             $routeCollector->addRoute(['GET', 'POST'], '/uploadeCompanyLists', $prefix . 'uploadeCompanyLists');
             $routeCollector->addRoute(['GET', 'POST'], '/getUploadLists', $prefix . 'getUploadLists'); 
-            $routeCollector->addRoute(['GET', 'POST'], '/getNeedsConfirmExportLists', $prefix . 'getNeedsConfirmExportLists'); exportFinanceData
+            $routeCollector->addRoute(['GET', 'POST'], '/getNeedsConfirmExportLists', $prefix . 'getNeedsConfirmExportLists'); 
             $routeCollector->addRoute(['GET', 'POST'], '/exportFinanceData', $prefix . 'exportFinanceData'); 
             $routeCollector->addRoute(['GET', 'POST'], '/ConfirmFinanceData', $prefix . 'ConfirmFinanceData'); 
         });
