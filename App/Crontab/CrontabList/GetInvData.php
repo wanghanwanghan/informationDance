ea8e4ad1bb2c49e83c6e792d037f867dc7789635
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2022-06-09 17:13:14 +0800

    2022-06-09_17:13:14

--- App/Crontab/CrontabList/GetInvData.php
+++ App/Crontab/CrontabList/GetInvData.php
@@ -88,12 +88,19 @@ class GetInvData extends AbstractCronTask
     //通知蚂蚁
     function sendToAnt(): bool
     {
+        //我想让你们把预发环境（appid: EC31D756AA8A86396F98CE08EDC9668B）通知我们数据的接收地址配置从
+        //https://invoicecommercial-pre.antfin.com/api/wezTech/collectNotify
+        //改为
+        //https://trustdata-pre.antgroup.com/api/wezTech/collectNotify
+
+
+
         //根据三个id，通知不同的url
         $url_arr = [
             36 => 'https://invoicecommercial.test.dl.alipaydev.com/api/wezTech/collectNotify',//dev
             //36 => 'http://invoicecommercial.dev.dl.alipaydev.com/api/wezTech/collectNotify',//test rsa和dev一样
             41 => 'https://invoicecommercial.antfin.com/api/wezTech/collectNotify',//pre 和 pro 交换了
-            42 => 'https://invoicecommercial-pre.antfin.com/api/wezTech/collectNotify',//pro 和 pre 交换了
+            42 => 'https://trustdata-pre.antgroup.com/api/wezTech/collectNotify',//pro 和 pre 交换了
         ];
 
         $total = AntAuthList::create()
