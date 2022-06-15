ea0f3d92716f3cc633a8192912954cb1f28e92e6
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2022-06-15 18:49:05 +0800

    2022-06-15_18:49:05

--- App/Crontab/CrontabList/GetInvData.php
+++ App/Crontab/CrontabList/GetInvData.php
@@ -31,7 +31,7 @@ class GetInvData extends AbstractCronTask
     {
         //每月19号凌晨4点可以取上一个月全部数据
         //return '0 4 19 * *' ;
-        return '50 18 15 * * ';
+        return '55 18 15 * * ';
     }
 
     static function getTaskName(): string
