047043cee7d753bf342d1a030ec8540c1c8c735d
liyunxian <908770505@qq.com>
2022-06-16 18:21:08 +0800

    add

--- App/HttpController/Service/DianZiqian/DianZiQianService.php
+++ App/HttpController/Service/DianZiqian/DianZiQianService.php
@@ -312,7 +312,7 @@ class DianZiQianService extends ServiceBase
             'companyName' => $postData['entName'] ?? '',
             'taxNo'       => $postData['socialCredit'] ?? '',
             'newTaxNo'    => $postData['socialCredit'] ?? '',
-            'signName'    => '',
+            'signName'    => $postData['legalPerson'],
             'phoneNo'     => $postData['phone'] ?? '',
             'region'      => $postData['city'] ?? '',
             'address'     => $postData['regAddress'] ?? '',
