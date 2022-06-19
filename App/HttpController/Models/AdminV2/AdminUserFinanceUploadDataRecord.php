fe516ab8b6b23c4b405ac334937d12221b131e64
tian <1270331569@qq.com>
2022-06-19 09:31:16 +0800

    测试

--- App/HttpController/Models/AdminV2/AdminUserFinanceUploadDataRecord.php
+++ App/HttpController/Models/AdminV2/AdminUserFinanceUploadDataRecord.php
@@ -313,6 +313,21 @@ class AdminUserFinanceUploadDataRecord extends ModelBase
     }
 
     public static function updatePriceType($info){
+        CommonService::getInstance()->log4PHP(
+            json_encode(
+                [
+                    'updatePriceType',
+                    'id' => $info['id'],
+                    'price' => $info['price'],
+                    'price_type' => $info['price_type'],
+                    'price_type_remark' => $info['price_type_remark'],
+                    'charge_year' => $info['charge_year'],
+                    'charge_year_start' => $info['charge_year_start'],
+                    'charge_year_end' => $info['charge_year_end'],
+                ]
+            )
+        );
+
         $info = AdminUserFinanceUploadDataRecord::create()
             ->where('id',$info['id'])
             ->get();
