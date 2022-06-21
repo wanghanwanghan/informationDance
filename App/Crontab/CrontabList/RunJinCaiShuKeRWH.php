2998fbd7fc057290bd39b2b57bd3d600d41de8d0
liyunxian <908770505@qq.com>
2022-06-21 10:36:44 +0800

    add

--- App/Crontab/CrontabList/RunJinCaiShuKeRWH.php
+++ App/Crontab/CrontabList/RunJinCaiShuKeRWH.php
@@ -21,7 +21,7 @@ class RunJinCaiShuKeRWH extends AbstractCronTask
     static function getRule(): string
     {
         //每分钟执行一次
-        return '* * * * *';
+        return '01 11 * * *';
     }
 
     static function getTaskName(): string
