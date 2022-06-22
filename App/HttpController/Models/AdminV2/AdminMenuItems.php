c69c8edacbb039b2bbe06223de2679a5a78e4bb5
tian <1270331569@qq.com>
2022-06-22 11:07:25 +0800

    测试

--- App/HttpController/Models/AdminV2/AdminMenuItems.php
+++ App/HttpController/Models/AdminV2/AdminMenuItems.php
@@ -16,7 +16,7 @@ class AdminMenuItems extends ModelBase
     protected $updateTime = 'updated_at'; 
 
     static function getMenusByParentId($parentId){
-        $sql = "SELECT * FROM  admin_menu_items WHERE parent_id = $parentId AND `status` = 1  ORDER BY  `order` desc  " ;
+        $sql = "SELECT * FROM  admin_menu_items WHERE parent_id = $parentId AND `status` = 1  ORDER BY  `order` asc  " ;
 
         $list = sqlRaw($sql, CreateConf::getInstance()->getConf('env.mysqlDatabase'));
         CommonService::getInstance()->log4PHP(
