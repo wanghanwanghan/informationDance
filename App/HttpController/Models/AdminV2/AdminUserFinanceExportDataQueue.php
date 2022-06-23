c76ddec7c375b425d5d31d7621c2635d3ad2ee6c
tian <1270331569@qq.com>
2022-06-23 16:37:58 +0800

    测试

--- App/HttpController/Models/AdminV2/AdminUserFinanceExportDataQueue.php
+++ App/HttpController/Models/AdminV2/AdminUserFinanceExportDataQueue.php
@@ -69,7 +69,7 @@ class AdminUserFinanceExportDataQueue extends ModelBase
                'path' => $requestData['path']?:'',
                'file_name' => $requestData['file_name']?:'',
                 'status' => $requestData['status']?:1,
-               'real_charge' => $requestData['real_charge']?:1,
+               'real_charge' => $requestData['real_charge']?:0,
                'created_at' => time(),
                'updated_at' => time(),
            ])->save();
