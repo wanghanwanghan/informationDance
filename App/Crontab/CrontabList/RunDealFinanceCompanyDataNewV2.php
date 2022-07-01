ce113f2533466f64396b3454ca93a1181f455989
tian <1270331569@qq.com>
2022-07-01 12:09:31 +0800

    测试下新的财务流程

--- App/Crontab/CrontabList/RunDealFinanceCompanyDataNewV2.php
+++ App/Crontab/CrontabList/RunDealFinanceCompanyDataNewV2.php
@@ -248,7 +248,19 @@ class RunDealFinanceCompanyDataNewV2 extends AbstractCronTask
             if(
                 !AdminUserFinanceConfig::checkIfNeedsConfirm($uploadRes['user_id'])
             ){
+                CommonService::getInstance()->log4PHP(
+                    json_encode([
+                        __CLASS__.__FUNCTION__ .' 不需要确认   '.$uploadRes['user_id'],
+                    ])
+                );
                 return AdminUserFinanceUploadRecord::changeStatus($uploadRes['id'],AdminUserFinanceUploadRecordV3::$stateAllSet);
+            }
+            else{
+                return CommonService::getInstance()->log4PHP(
+                    json_encode([
+                        __CLASS__.__FUNCTION__ .' 需要确认 '.$uploadRes['user_id'],
+                    ])
+                );
             }
 
             //需要确认的 先去拉取财务数据
