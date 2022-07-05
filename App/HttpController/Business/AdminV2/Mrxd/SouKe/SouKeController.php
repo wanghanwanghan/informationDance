fa875fc5cba15c8b7b69b45e14a56ac2dddb9d53
tian <1270331569@qq.com>
2022-07-05 19:28:47 +0800

    测试

--- App/HttpController/Business/AdminV2/Mrxd/SouKe/SouKeController.php
+++ App/HttpController/Business/AdminV2/Mrxd/SouKe/SouKeController.php
@@ -181,7 +181,18 @@ class SouKeController extends ControllerBase
         }
 
         $requestData =  $this->getRequestData();
+        if(substr($requestData['basic_nicid'], -1) == ','){
+            $requestData['basic_nicid'] = rtrim($requestData['basic_nicid'], ",");
+        }
 
+        if(substr($requestData['basic_regionid'], -1) == ','){
+            $requestData['basic_regionid'] = rtrim($requestData['basic_regionid'], ",");
+        }
+
+        if(substr($requestData['basic_jlxxcyid'], -1) == ','){
+            $requestData['basic_jlxxcyid'] = rtrim($requestData['basic_jlxxcyid'], ",");
+        }
+        
 
         $checkRes = DataModelExample::checkField(
             [
