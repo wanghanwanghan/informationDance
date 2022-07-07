72ff3bea035432409cc4b6b00037837e74935798
tian <1270331569@qq.com>
2022-07-07 11:22:50 +0800

    删除多余代码

--- App/HttpController/Models/AdminV2/AdminUserFinanceData.php
+++ App/HttpController/Models/AdminV2/AdminUserFinanceData.php
@@ -359,14 +359,14 @@ class AdminUserFinanceData extends ModelBase
                 $dbDataArr['year'] = $yearItem;
                 $dbDataArr['raw_return'] = @json_encode($resData);
                 $addRes = NewFinanceData::addRecordV2($dbDataArr);
-                CommonService::getInstance()->log4PHP(
-                    json_encode([
-                        __CLASS__.__FUNCTION__ ,
-                        'NewFinanceData::addRecordV2 '=>$dbDataArr,
-                        '$addRes' => $addRes,
-                        '$yearItem' => $yearItem,
-                    ])
-                );
+//                CommonService::getInstance()->log4PHP(
+//                    json_encode([
+//                        __CLASS__.__FUNCTION__ ,
+//                        'NewFinanceData::addRecordV2 '=>$dbDataArr,
+//                        '$addRes' => $addRes,
+//                        '$yearItem' => $yearItem,
+//                    ])
+//                );
                 if(!$addRes){
                     return CommonService::getInstance()->log4PHP(
                         json_encode([
