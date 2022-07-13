98c6ef59d6f6c5fa781edeb866cbc938305f5464
tian <1270331569@qq.com>
2022-07-13 18:01:49 +0800

    测试

--- App/ElasticSearch/Model/Company.php
+++ App/ElasticSearch/Model/Company.php
@@ -103,7 +103,7 @@ class Company extends ServiceBase
         $companyLocationEsModel
             //经营范围
             ->SetAreaQuery($areaArr)
-            ->addSize(500)
+            ->addSize(5000)
             ->addSort('companyid',"desc")
             ->searchFromEs();
         $xdIds = [0];
