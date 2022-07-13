ad81d29c99c12c1100c9cf9568b6c0294f7fc265
tian <1270331569@qq.com>
2022-07-13 16:00:10 +0800

    es加下排序

--- App/ElasticSearch/Model/Company.php
+++ App/ElasticSearch/Model/Company.php
@@ -104,6 +104,7 @@ class Company extends ServiceBase
             //经营范围
             ->SetAreaQuery($areaArr)
             ->addSize(500)
+            ->addSort('companyid',"desc")
             ->searchFromEs();
         $xdIds = [0];
         foreach($companyLocationEsModel->return_data['hits']['hits'] as $dataItem){
