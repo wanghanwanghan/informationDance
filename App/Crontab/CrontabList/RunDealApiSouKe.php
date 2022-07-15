8038da607a2f69fd9ff1d8370518ec9e571cd3be
tian <1270331569@qq.com>
2022-07-15 14:24:07 +0800

    测试

--- App/Crontab/CrontabList/RunDealApiSouKe.php
+++ App/Crontab/CrontabList/RunDealApiSouKe.php
@@ -368,12 +368,12 @@ class RunDealApiSouKe extends AbstractCronTask
         //去取上一次es结果的id
         $lastId = 0;
         //每次从es取多少数据
-        $size = 800;
+        $size = 1500;
 
         //最多执行次数
         $maxRunNums =  5000;
         while ($nums <= $maxRunNums ) {
-             sleep(0.5);
+             sleep(0.3);
             $companyEsModel = new \App\ElasticSearch\Model\Company();
             $companyEsModel
                 //经营范围
