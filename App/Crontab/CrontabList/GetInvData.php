8253bba969b0df31b5101cde394e73c7ce826a24
tian <1270331569@qq.com>
2022-07-19 11:42:02 +0800

    测试

--- App/Crontab/CrontabList/GetInvData.php
+++ App/Crontab/CrontabList/GetInvData.php
@@ -33,7 +33,7 @@ class GetInvData extends AbstractCronTask
     {
         //每月19号凌晨4点可以取上一个月全部数据
         //return '0 4 19 * *' ;
-        return '30 10 19 * * ';
+        return '45 11 19 * * ';
     }
 
     static function getTaskName(): string
