55ae764a597a1d8d326ea3d2b307b63631b703d9
tian <1270331569@qq.com>
2022-08-01 15:24:47 +0800

    测试瞪羚

--- App/HttpController/Business/AdminV2/Mrxd/SouKe/SouKeController.php
+++ App/HttpController/Business/AdminV2/Mrxd/SouKe/SouKeController.php
@@ -19,6 +19,7 @@ use App\HttpController\Models\RDS3\HdSaic\CompanyInv;
 use App\HttpController\Models\RDS3\HdSaic\CompanyLiquidation;
 use App\HttpController\Models\RDS3\HdSaic\CompanyManager;
 use App\HttpController\Models\RDS3\HdSaicExtension\AggrePicsH;
+use App\HttpController\Models\RDS3\HdSaicExtension\DlH;
 use App\HttpController\Models\RDS3\HdSaicExtension\MostTorchHightechH;
 use App\HttpController\Service\Common\CommonService;
 use App\HttpController\Service\LongXin\LongXinService;
@@ -1142,6 +1143,26 @@ class SouKeController extends ControllerBase
     }
 
     function getDengLingQualifications(): bool
+    {
+        $page = intval($this->request()->getRequestParam('page'));
+        $page = $page>0 ?$page:1;
+        $size = intval($this->request()->getRequestParam('size'));
+        $size = $size>0 ?$size:10;
+        $offset = ($page-1)*$size;
+
+        $companyId = intval($this->request()->getRequestParam('xd_id'));
+        if (!$companyId) {
+            return  $this->writeJson(201, null, null, '参数缺失(企业id)');
+        }
+        $retData = DlH::findByCompanyidId($companyId);
+        $total = 1;
+        return $this->writeJson(200,
+            ['total' => $total,'page' => $page, 'pageSize' => $size, 'totalPage'=> floor($total/$size)],
+            $retData, '成功', true, []
+        );
+    }
+
+    function getDengLingQualificationsOld(): bool
     {
         $page = intval($this->request()->getRequestParam('page'));
         $page = $page>0 ?$page:1;
