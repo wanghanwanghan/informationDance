6875a9fd64f97e551701b0205995698ea186fdbc
tian <1270331569@qq.com>
2022-08-03 13:51:26 +0800

    测试

--- App/HttpController/Business/OnlineGoods/Mrxd/UserController.php
+++ App/HttpController/Business/OnlineGoods/Mrxd/UserController.php
@@ -127,7 +127,7 @@ class UserController extends \App\HttpController\Business\OnlineGoods\Mrxd\Contr
         $digit = OnlineGoodsUser::createRandomDigit();
 
        //发短信
-        $res = (new AliSms())->sendByTempleteV3($phone, 'SMS_244025473',[
+        $res = (new AliSms())->sendByTempleteV2($phone, 'SMS_244025473',[
             'name' => $digit,
             'money' => $digit,
         ]);
