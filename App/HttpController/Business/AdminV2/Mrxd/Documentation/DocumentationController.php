e36ada30664a65003a672727842a2de15a04e736
tian <1270331569@qq.com>
2022-08-08 11:14:45 +0800

    在线文档 增加删除

--- App/HttpController/Business/AdminV2/Mrxd/Documentation/DocumentationController.php
+++ App/HttpController/Business/AdminV2/Mrxd/Documentation/DocumentationController.php
@@ -34,13 +34,35 @@ class DocumentationController extends ControllerBase
         return $this->writeJson();
     }
 
+    public function getOne(){
+        $requestData = $this->getRequestData();
+
+        $id = $requestData['id'] ;
+
+        if(!empty($id)){
+            return $this->writeJson(203,  [  ], [],'参数缺失');
+        }
+
+        $res = Documentation::findById(
+            $id
+        );
+
+        return $this->writeJson(200,  [],  $res,'成功');
+    }
+
     public function getAll(){
         $requestData = $this->getRequestData();
 
         $page = $requestData['page']??1;
         $createdAtStr = $this->getRequestData('created_at');
         $createdAtArr = explode('|||',$createdAtStr);
-        $whereArr = [];
+        $whereArr = [
+            [
+                'field' => 'status',
+                'value' => Documentation::$status_init,
+                'operate' => '=',
+            ]
+        ];
         if (
             !empty($createdAtArr) &&
             !empty($createdAtStr)
@@ -55,6 +77,11 @@ class DocumentationController extends ControllerBase
                     'field' => 'created_at',
                     'value' => strtotime($createdAtArr[1].' 23:59:59'),
                     'operate' => '<=',
+                ],
+                [
+                    'field' => 'status',
+                    'value' => Documentation::$status_init,
+                    'operate' => '=',
                 ]
             ];
         }
@@ -148,6 +175,35 @@ class DocumentationController extends ControllerBase
         return $this->writeJson(200,  [ ],  $res,'成功');
     }
 
+    //update
+    public function delDocumention(){
+        $requestData = $this->getRequestData();
+        $checkRes = DataModelExample::checkField(
+            [
+
+                'id' => [
+                    'not_empty' => 1,
+                    'field_name' => 'id',
+                    'err_msg' => 'id不能为空',
+                ],
+            ],
+            $requestData
+        );
+        if(
+            !$checkRes['res']
+        ){
+            return $this->writeJson(203,[ ] , [], $checkRes['msgs'], true, []);
+        }
+
+        $res = Documentation::updateById(
+            $requestData['id'],
+            [
+              'status'=> Documentation::$status_del
+            ]
+        );
+
+        return $this->writeJson(200,  [ ],  $res,'成功');
+    }
 
     public function downloadDocumention(){
         $requestData = $this->getRequestData();
