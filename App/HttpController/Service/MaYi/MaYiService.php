fc747da36efb4efb4156cc1d2a55669a38864d19
liyunxian <908770505@qq.com>
2022-08-11 09:55:34 +0800

    add

--- App/HttpController/Service/MaYi/MaYiService.php
+++ App/HttpController/Service/MaYi/MaYiService.php
@@ -5268,7 +5268,7 @@ class MaYiService extends ServiceBase
         //增加除授权书其他证书的表，并做关联
         if (!empty($data['fileData'])) {
             foreach ($data['fileData'] as $datum) {
-                $id='';
+//                $id='';
                 if($datum['isSeal'] === 'true' && $data['test']){
                     $gaizhangParam = [
                         'entName'      => $data['entName'],
