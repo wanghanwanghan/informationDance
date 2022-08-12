2166367815f616fa96785599a35dfafd2d477082
tian <1270331569@qq.com>
2022-08-12 14:32:06 +0800

    测试

--- App/HttpController/Models/AdminV2/CarInsuranceInstallment.php
+++ App/HttpController/Models/AdminV2/CarInsuranceInstallment.php
@@ -498,11 +498,11 @@ class CarInsuranceInstallment extends ModelBase
         $data = jsonDecode($res['data']);
         foreach ($data as $dataItem){
             if($dataItem['columnSequence'] == 16){
-                //$suoDeShui[] =  $dataItem;
-                $suoDeShui[] =  [
-                    'beginDate'=>$dataItem['beginDate'],
-                    'currentAmount'=>$dataItem['currentAmount'],
-                ];
+                $suoDeShui[] =  $dataItem;
+//                $suoDeShui[] =  [
+//                    'beginDate'=>$dataItem['beginDate'],
+//                    'currentAmount'=>$dataItem['currentAmount'],
+//                ];
             }
         }
         //按时间顺序排列
