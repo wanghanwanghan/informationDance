3f959f57abdc1be39e29a5a2a0d175030b40a635
tian <1270331569@qq.com>
2022-08-16 11:04:34 +0800

    删除多余log

--- App/HttpController/Models/AdminV2/CarInsuranceInstallment.php
+++ App/HttpController/Models/AdminV2/CarInsuranceInstallment.php
@@ -1015,12 +1015,12 @@ class CarInsuranceInstallment extends ModelBase
         $res = (new GuoPiaoService())->getIncometaxMonthlyDeclaration(
             $social_credit_code
         );
-          CommonService::getInstance()->log4PHP(
-              json_encode([
-                  __CLASS__.__FUNCTION__ .__LINE__,
-                  'getIncometaxMonthlyDeclaration$res' => $res
-              ])
-          );
+//          CommonService::getInstance()->log4PHP(
+//              json_encode([
+//                  __CLASS__.__FUNCTION__ .__LINE__,
+//                  'getIncometaxMonthlyDeclaration$res' => $res
+//              ])
+//          );
         $data = jsonDecode($res['data']);
         foreach ($data as $dataItem){
             if($dataItem['columnSequence'] == 16){
