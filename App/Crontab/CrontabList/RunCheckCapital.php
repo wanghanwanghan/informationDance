040c214064a1e08fd95102a0133e96e9a095e299
liyunxian <908770505@qq.com>
2022-08-30 15:06:48 +0800

    add

--- App/Crontab/CrontabList/RunCheckCapital.php
+++ App/Crontab/CrontabList/RunCheckCapital.php
@@ -43,7 +43,7 @@ class RunCheckCapital extends AbstractCronTask
                                 '累计充值下单金额总额' => $resp['data']['totalAmount'],
                                 '已使用金额总额' => $resp['data']['usedAmount'],
                                 '目前可用金额' => "<font color='#FF0000'>".$resp['data']['availableAmount']."</font>",
-                                '查看人'=>'@18511881968'//18501224247
+                                '查看人'=>"<font color='#000000'>".'@18511881968'."</font>",//18501224247
                             ],
                             ['18511881968']
             );
