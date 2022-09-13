8fc5d5fa9a7d49c60a1347f7606b1f664e18e8c9
liyunxian <908770505@qq.com>
2022-09-13 11:11:51 +0800

    add

--- App/HttpController/Business/Provide/ShenZhouYunHe/ShenZhouYunHeController.php
+++ App/HttpController/Business/Provide/ShenZhouYunHe/ShenZhouYunHeController.php
@@ -60,7 +60,7 @@ class ShenZhouYunHeController extends ProvideBase
             'pageNum' => $pageNum,
             'pageSize' => $pageSize
         ];
-        dingAlarm('invoices',['$postData'=>$postData]);
+        dingAlarm('invoices',['$postData'=>json_encode($postData)]);
         $this->csp->add($this->cspKey, function () use ($postData) {
             return (new ShenZhouYunHeService())->setCheckRespFlag(true)->invoices($postData);
         });
