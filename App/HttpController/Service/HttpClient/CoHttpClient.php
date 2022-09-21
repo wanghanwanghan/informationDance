37e5cd614fa77c04c544224eb85eb07846184848
liyunxian <908770505@qq.com>
2022-09-21 10:06:18 +0800

    add

--- App/HttpController/Service/HttpClient/CoHttpClient.php
+++ App/HttpController/Service/HttpClient/CoHttpClient.php
@@ -66,7 +66,11 @@ class CoHttpClient extends ServiceBase
             //整理结果
             $data = $data->getBody();
 //            dingAlarm('http返回',['$url'=>$url,'$data'=>json_encode($data),'$postData'=>json_encode($postData)]);
-            CommonService::getInstance()->log4PHP([$url, $postData, jsonDecode($data), $headers], 'info', 'http_return_data');
+            if(empty($data) || $data['code'] != 200){
+                CommonService::getInstance()->log4PHP([$url, $postData, jsonDecode($data), $headers], 'info', 'error_http_return_data');
+            }else{
+                CommonService::getInstance()->log4PHP([$url, $postData, jsonDecode($data), $headers], 'info', 'http_return_data');
+            }
         } catch (\Exception $e) {
             $this->writeErr($e, 'CoHttpClient');
             return ['coHttpErr' => 'error'];
