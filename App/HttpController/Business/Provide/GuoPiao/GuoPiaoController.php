49fea64a9bd3eedf001916a6079e996914fa6cce
王瀚 <5211470+wanghanwanghan@user.noreply.gitee.com>
2022-10-11 11:34:04 +0800

    2022-10-11_11:34:04

--- App/HttpController/Business/Provide/GuoPiao/GuoPiaoController.php
+++ App/HttpController/Business/Provide/GuoPiao/GuoPiaoController.php
@@ -171,8 +171,6 @@ class GuoPiaoController extends ProvideBase
 
         $res = CspService::getInstance()->exec($this->csp, $this->cspTimeout);
 
-        CommonService::getInstance()->log4PHP($res);
-
         return $this->checkResponse($res);
     }
 
@@ -294,26 +292,13 @@ class GuoPiaoController extends ProvideBase
     {
         $code = $this->getRequestData('code');
 
-        $res = (new GuoPiaoService())->getVatReturn($code);
+        $this->csp->add($this->cspKey, function () use ($code) {
+            return (new GuoPiaoService())
+                ->setCheckRespFlag(true)
+                ->getVatReturn($code);
+        });
 
-        //正常
-        if ($res['code'] - 0 === 0 && !empty($res['data'])) {
-            $data = jsonDecode($res['data']);
-            $model = [];
-            foreach ($data as $row) {
-                $year_month = substr(str_replace(['-'], '', $row['beginDate']), 0, 6) . '';
-                if (!isset($model[$year_month])) {
-                    $model[$year_month] = [];
-                }
-                $row['sequence'] = $row['sequence'] - 0;
-                $model[$year_month][] = $row;
-            }
-            //排序
-            foreach ($model as $year => $val) {
-                $model[$year] = control::sortArrByKey($val, 'sequence', 'asc', true);
-            }
-            $res['data'] = jsonEncode($model);
-        }
+        $res = CspService::getInstance()->exec($this->csp, $this->cspTimeout);
 
         return $this->checkResponse($res);
     }
