61c9c85cc7ea41b7d5c8231438159441e33a5d47
tian <1270331569@qq.com>
2022-10-13 17:37:29 +0800

    kaiTingNoticeDetail

--- App/HttpController/ProvideRouter.php
+++ App/HttpController/ProvideRouter.php
@@ -328,6 +328,8 @@ class ProvideRouter
             $routeCollector->addRoute(['GET', 'POST'], '/waiHuiJuLicenseDetail', $prefix . 'waiHuiJuLicenseDetail');
             $routeCollector->addRoute(['GET', 'POST'], '/faYuanNoticeList', $prefix . 'faYuanNoticeList');
             $routeCollector->addRoute(['GET', 'POST'], '/faYuanNoticeDetail', $prefix . 'faYuanNoticeDetail');
+            $routeCollector->addRoute(['GET', 'POST'], '/kaiTingNoticeList', $prefix . 'kaiTingNoticeList');
+            $routeCollector->addRoute(['GET', 'POST'], '/kaiTingNoticeDetail', $prefix . 'kaiTingNoticeDetail');
 
             $routeCollector->addRoute(['GET', 'POST'], '/getKtggDetail', $prefix . 'getKtggDetail');//开庭公告
             $routeCollector->addRoute(['GET', 'POST'], '/getFyggDetail', $prefix . 'getFyggDetail');//法院公告
