fceadf199f1982bfffa1b88763a1766c30ba9d14
tian <1270331569@qq.com>
2022-10-13 16:19:43 +0800

    证监处罚公示列表

--- App/HttpController/Business/Provide/FaHai/FaHaiController.php
+++ App/HttpController/Business/Provide/FaHai/FaHaiController.php
@@ -647,6 +647,33 @@ class FaHaiController extends ProvideBase
         return $this->checkResponse($res);
     }
 
+    // 证监处罚公示列表
+    function zhengJianHuiPunishNoticeList()
+    {
+        $pageno = $this->request()->getRequestParam('page') ?? '1';
+        $range = $this->request()->getRequestParam('pageSize') ?? '20';
+        $entName = $this->request()->getRequestParam('entName') ?? '';
+        $doc_type = 'pbcparty_csrc_chufa';
+        $postData = [
+            'doc_type' => $doc_type,
+            'keyword' => $entName,
+            'pageno' => $pageno,
+            'range' => $range,
+        ];
+        $this->csp->add($this->cspKey, function () use ($postData) {
+
+            return  (new FaYanYuanService())
+                //->setCheckRespFlag(false)
+                ->setCheckRespFlag(true)
+                ->getList( CreateConf::getInstance()->getConf('fayanyuan.listBaseUrl') . 'pbc', $postData);
+        });
+        $res = CspService::getInstance()->exec($this->csp, $this->cspTimeout);
+        CommonService::writeTestLog(json_encode([
+            'zhengJianHuiPunishNoticeList_post'=>$postData,
+            'zhengJianHuiPunishNoticeList_$res'=>$res,
+        ]));
+        return $this->checkResponse($res);
+    }
 
 }
 
--- App/HttpController/ProvideRouter.php
+++ App/HttpController/ProvideRouter.php
@@ -315,6 +315,7 @@ class ProvideRouter
             $routeCollector->addRoute(['GET', 'POST'], '/xingZhengPunishDetails', $prefix . 'xingZhengPunishDetails');
             $routeCollector->addRoute(['GET', 'POST'], '/yinJianHuiPunishNoticeList', $prefix . 'yinJianHuiPunishNoticeList');
             $routeCollector->addRoute(['GET', 'POST'], '/yinJianHuiPunishNoticeDetail', $prefix . 'yinJianHuiPunishNoticeDetail');
+            $routeCollector->addRoute(['GET', 'POST'], '/zhengJianHuiPunishNoticeList', $prefix . 'zhengJianHuiPunishNoticeList');
 
             $routeCollector->addRoute(['GET', 'POST'], '/getKtggDetail', $prefix . 'getKtggDetail');//开庭公告
             $routeCollector->addRoute(['GET', 'POST'], '/getFyggDetail', $prefix . 'getFyggDetail');//法院公告
