34c8a28ef65f7555837669a3b94513c24ab9d729
tian <1270331569@qq.com>
2022-10-27 15:49:41 +0800

    测试

--- App/HttpController/Business/AdminV2/Mrxd/Tools/ToolsController.php
+++ App/HttpController/Business/AdminV2/Mrxd/Tools/ToolsController.php
@@ -14,8 +14,10 @@ use App\HttpController\Models\AdminV2\DataModelExample;
 use App\HttpController\Models\AdminV2\DeliverHistory;
 use App\HttpController\Models\AdminV2\DownloadSoukeHistory;
 use App\HttpController\Models\AdminV2\FinanceLog;
+use App\HttpController\Models\AdminV2\QueueLists;
 use App\HttpController\Models\AdminV2\ToolsUploadQueue;
 use App\HttpController\Models\Api\CompanyCarInsuranceStatusInfo;
+use App\HttpController\Models\MRXD\ToolsFileLists;
 use App\HttpController\Models\Provide\RequestApiInfo;
 use App\HttpController\Models\RDS3\Company;
 use App\HttpController\Service\AdminRole\AdminPrivilegedUser;
@@ -85,6 +87,23 @@ class ToolsController extends ControllerBase
         $page =$requestData['page']?:1;
         $pageSize =$requestData['pageSize']?:20;
         $total = 1;
+        if($requestData['real']){
+            $res = ToolsFileLists::findByConditionWithCountInfo(
+                [],$page
+            );
+            foreach ($res['data'] as &$dataItem ){
+                $adminInfo = \App\HttpController\Models\AdminV2\AdminNewUser::findById($dataItem['admin_id']);
+                $dataItem['admin_id_cname'] = $adminInfo->user_name;
+                $dataItem['state_cname'] = ToolsFileLists::stateMaps()[$dataItem['state']];
+            }
+            $total = $res['total'];
+            return $this->writeJson(200, [
+                'page' => $page,
+                'pageSize' =>$pageSize,
+                'total' => $total,
+                'totalPage' =>  ceil( $total/ $pageSize ),
+            ],  $res['data'],'');
+        }
         return $this->writeJson(200, [
             'page' => $page,
             'pageSize' =>$pageSize,
@@ -107,15 +126,73 @@ class ToolsController extends ControllerBase
 
     public function uploadeBuQuanZiDuanFiles(){
         $requestData =  $this->getRequestData();
-        $page =$requestData['page']?:1;
-        $pageSize =$requestData['pageSize']?:20;
-        $total = 1;
-        return $this->writeJson(200, [
-            'page' => $page,
-            'pageSize' =>$pageSize,
-            'total' => $total,
-            'totalPage' =>  ceil( $total/ $pageSize ),
-        ],
+        $succeedFiels = [];
+        if($requestData['real']){
+            $files = $this->request()->getUploadedFiles();
+            foreach ($files as $key => $oneFile) {
+                try {
+                    $fileName = $oneFile->getClientFilename();
+                    $fileInfo = pathinfo($fileName);
+                    if($fileInfo['extension']!='xlsx'){
+                        return $this->writeJson(203, [], [],'暂时只支持xlsx文件！');
+                    }
+                    $fileName = date('Y_m_d_H_i',time()).$fileName;
+                    $path = OTHER_FILE_PATH . $fileName;
+                    if(file_exists($path)){
+                        return $this->writeJson(203, [], [],'文件已存在！');
+                    }
+
+                    $res = $oneFile->moveTo($path);
+                    if(!file_exists($path)){
+                        return $this->writeJson(203, [], [],'文件移动失败！');
+                    }
+
+                    $UploadRecordRes =  ToolsFileLists::addRecordV2(
+                        [
+                            'admin_id' => $this->loginUserinfo['id'],
+                            'file_name' => $fileName,
+                            'new_file_name' => '',
+                            'remark' => $requestData['remark']?:'',
+                            'type' => $requestData['type']?:'',
+                            'state' => $requestData['state']?:'',
+                            'touch_time' => $requestData['touch_time']?:'',
+                        ]
+                    );
+                    if(!$UploadRecordRes){
+                        return $this->writeJson(203, [], [],'文件上传失败');
+                    }
+
+//                    $res = QueueLists::addRecord(
+//                        [
+//                            'name' => '',
+//                            'desc' => '',
+//                            'func_info_json' => json_encode(
+//                                [
+//                                    'class' => '\App\HttpController\Models\MRXD\ToolsFileLists',
+//                                    'static_func'=> 'buQuanZiDuan',
+//                                ]
+//                            ),
+//                            'params_json' => json_encode([
+//
+//                            ]),
+//                            'type' => QueueLists::$typle_finance,
+//                            'remark' => '',
+//                            'begin_date' => NULL,
+//                            'msg' => '',
+//                            'status' => QueueLists::$status_init,
+//                        ]
+//                    );
+
+                    $succeedFiels[] = $fileName;
+                } catch (\Throwable $e) {
+                    return $this->writeJson(202, [], [],'导入失败'.$e->getMessage());
+                }
+            }
+
+            return $this->writeJson(200, [], [],'成功 入库文件:'.join(',',$succeedFiels));
+        }
+
+        return $this->writeJson(200, [  ],
             [
 
             ],'成功');
--- App/HttpController/Models/MRXD/ToolsFileLists.php
+++ App/HttpController/Models/MRXD/ToolsFileLists.php
@@ -5,6 +5,7 @@ namespace App\HttpController\Models\MRXD;
 use App\HttpController\Models\AdminNew\ConfigInfo;
 use App\HttpController\Models\Api\FinancesSearch;
 use App\HttpController\Models\ModelBase;
+use App\HttpController\Service\ChuangLan\ChuangLanService;
 use App\HttpController\Service\Common\CommonService;
 use App\HttpController\Service\CreateConf;
 use App\HttpController\Service\LongXin\LongXinService;
@@ -18,11 +19,22 @@ class ToolsFileLists extends ModelBase
 
     protected $tableName = 'tools_file_lists';
 
+    static $state_init = 0 ;
+    static $state_init_cname =  '处理中(0)' ;
 
+    static $state_succeed =  10  ;
+    static $state_succeedt_cname =  '处理成功' ;
 
-    static $level_vip = 1 ;
-    static $level_vip_cname =  'vip客户' ;
+    static  $type_bu_quan_zi_duan =  5 ;
+    static  $type_bu_quan_zi_duan_cname =  '补全字段' ;
 
+    static  function  stateMaps(){
+
+        return [
+            self::$state_init => self::$state_init_cname,
+            self::$state_succeed => self::$state_succeedt_cname,
+        ] ;
+    }
 
     static  function  addRecordV2($info){
 
@@ -140,5 +152,90 @@ class ToolsFileLists extends ModelBase
         return $data;
     }
 
+
+    static function buQuanZiDuan(){
+       $filesDatas = self::findBySql("
+            WHERE touch_time < 1
+            AND type = 5 
+            AND state = 0 
+            LIMIT 3 
+       ");
+       foreach ($filesDatas as $filesData){
+           self::setTouchTime($filesData['id'],date('Y-m-d H:i:s'));
+           $yieldDatas = self::getXlsxYieldData($filesData['file_name'],OTHER_FILE_PATH);
+           //写到csv里
+           $fileName = pathinfo($filesData['file_name'])['filename'];
+           $f = fopen(OTHER_FILE_PATH.$fileName.".csv", "w");
+           fwrite($f,chr(0xEF).chr(0xBB).chr(0xBF));
+
+           foreach ($yieldDatas as $dataItem) {
+               fputcsv($f, $dataItem);
+           }
+           self::setData($filesData['id'],[
+               'new_file_name' => $fileName.".csv"
+           ]);
+       }
+    }
+
+    static  function getXlsxYieldData($xlsx_name,$path){
+        $excel_read = new \Vtiful\Kernel\Excel(['path' => $path]);
+        $excel_read->openFile($xlsx_name)->openSheet();
+
+        $datas = [];
+        $i = 1;
+        while (true) {
+
+            $one = $excel_read->nextRow([
+                \Vtiful\Kernel\Excel::TYPE_STRING,
+                \Vtiful\Kernel\Excel::TYPE_STRING,
+                \Vtiful\Kernel\Excel::TYPE_STRING,
+            ]);
+
+            if (empty($one)) {
+                break;
+            }
+
+
+            $i ++;
+            yield $datas[] = [
+                self::strtr_func($one[0]),
+                self::strtr_func($one[1]),
+                self::strtr_func($one[2]),
+                self::strtr_func($one[3]),
+                self::strtr_func($one[4]),
+                self::strtr_func($one[5]),
+                self::strtr_func($one[6]),
+                self::strtr_func($one[7]),
+                self::strtr_func($one[8]),
+            ];
+        }
+    }
+
+    static function strtr_func($str): string
+    {
+        $str = trim($str);
+
+        if (empty($str)) {
+            return '';
+        }
+
+        $arr = [
+            '０' => '0', '１' => '1', '２' => '2', '３' => '3', '４' => '4', '５' => '5', '６' => '6', '７' => '7', '８' => '8', '９' => '9',
+            'Ａ' => 'A', 'Ｂ' => 'B', 'Ｃ' => 'C', 'Ｄ' => 'D', 'Ｅ' => 'E', 'Ｆ' => 'F', 'Ｇ' => 'G', 'Ｈ' => 'H', 'Ｉ' => 'I', 'Ｊ' => 'J',
+            'Ｋ' => 'K', 'Ｌ' => 'L', 'Ｍ' => 'M', 'Ｎ' => 'N', 'Ｏ' => 'O', 'Ｐ' => 'P', 'Ｑ' => 'Q', 'Ｒ' => 'R', 'Ｓ' => 'S', 'Ｔ' => 'T',
+            'Ｕ' => 'U', 'Ｖ' => 'V', 'Ｗ' => 'W', 'Ｘ' => 'X', 'Ｙ' => 'Y', 'Ｚ' => 'Z', 'ａ' => 'a', 'ｂ' => 'b', 'ｃ' => 'c', 'ｄ' => 'd',
+            'ｅ' => 'e', 'ｆ' => 'f', 'ｇ' => 'g', 'ｈ' => 'h', 'ｉ' => 'i', 'ｊ' => 'j', 'ｋ' => 'k', 'ｌ' => 'l', 'ｍ' => 'm', 'ｎ' => 'n',
+            'ｏ' => 'o', 'ｐ' => 'p', 'ｑ' => 'q', 'ｒ' => 'r', 'ｓ' => 's', 'ｔ' => 't', 'ｕ' => 'u', 'ｖ' => 'v', 'ｗ' => 'w', 'ｘ' => 'x',
+            'ｙ' => 'y', 'ｚ' => 'z',
+            '（' => '(', '）' => ')', '〔' => '(', '〕' => ')', '【' => '[', '】' => ']', '〖' => '[', '〗' => ']',
+            '｛' => '{', '｝' => '}', '《' => '<', '》' => '>', '％' => '%', '＋' => '+', '—' => '-', '－' => '-',
+            '～' => '~', '：' => ':', '。' => '.', '，' => ',', '、' => ',', '；' => ';', '？' => '?', '！' => '!', '…' => '-',
+            '‖' => '|', '”' => '"', '’' => '`', '‘' => '`', '｜' => '|', '〃' => '"', '　' => ' ', '×' => '*', '￣' => '~', '．' => '.', '＊' => '*',
+            '＆' => '&', '＜' => '<', '＞' => '>', '＄' => '$', '＠' => '@', '＾' => '^', '＿' => '_', '＂' => '"', '￥' => '$', '＝' => '=',
+            '＼' => '\\', '／' => '/', '“' => '"', PHP_EOL => ''
+        ];
+
+        return str_replace([',', ' '], '', strtr($str, $arr));
+    }
 
 }
