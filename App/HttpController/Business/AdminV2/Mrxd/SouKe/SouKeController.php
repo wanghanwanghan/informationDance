e4b04612c45ec37d010d69ed31b6b41eefa12fba
tian <1270331569@qq.com>
2022-10-28 17:14:43 +0800

    测试

--- App/HttpController/Business/AdminV2/Mrxd/SouKe/SouKeController.php
+++ App/HttpController/Business/AdminV2/Mrxd/SouKe/SouKeController.php
@@ -1472,7 +1472,7 @@ class SouKeController extends ControllerBase
             ])
         );
 
-        $sqlWhere = " WHERE user_id =  ".$this->loginUserinfo['id'];
+        $sqlWhere = " WHERE userid =  ".$this->loginUserinfo['id'];
 
         // searchText
         if(!empty($requestData['searchText'])){
