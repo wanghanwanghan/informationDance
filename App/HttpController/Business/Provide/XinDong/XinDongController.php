14c9994c626cb8cf260374e8009a16c904002ee0
liyunxian <908770505@qq.com>
2022-11-03 17:28:30 +0800

    add

--- App/HttpController/Business/Provide/XinDong/XinDongController.php
+++ App/HttpController/Business/Provide/XinDong/XinDongController.php
@@ -2340,36 +2340,14 @@ class XinDongController extends ProvideBase
     //企业基本信息
     function getEntMarketInfo(): bool
     {
-        $UNISCID = $this->getRequestData('UNISCID', '');
+        $code = $this->getRequestData('code', '');
         if (empty($UNISCID)) {
             return $this->writeJson(201, null, null, '参数缺失(统一社会信用代码)');
         }
-        $this->csp->add($this->cspKey, function () use ($UNISCID) {
-            $info = CompanyBasic::create()->where(['UNISCID'=>$UNISCID])->get();
-            $list = AggreListedH::create()->where(['companyid'=>$info->getAttr('companyid')])->all();
-            $data = [];
-            foreach ($list as $val){
-                $v = [
-                    'SEC_CODE' => $val->getAttr('SEC_CODE'),
-                    'SEC_SNAME' => $val->getAttr('SEC_SNAME'),
-                    'SEC_STYPE' => $val->getAttr('SEC_STYPE'),
-                    'MKT_TYPE' => $val->getAttr('MKT_TYPE'),
-                    'LIST_STATUS' => $val->getAttr('LIST_STATUS'),
-                    'LIST_SECTOR' => $val->getAttr('LIST_SECTOR'),
-                    'LIST_DATE' => $val->getAttr('LIST_DATE'),
-                    'LIST_ENDDATE' => $val->getAttr('LIST_ENDDATE'),
-                    'ISIN' => $val->getAttr('ISIN'),
-                    'nic_csrc' => $val->getAttr('nic_csrc'),
-                    'nic_cf' => $val->getAttr('nic_cf'),
-                    'nic_sws' => $val->getAttr('nic_sws'),
-                    'nic_gisc' => $val->getAttr('nic_gisc'),
-                    'EMPNUM' => $val->getAttr('EMPNUM'),
-                    'TSTAFFNUM' => $val->getAttr('TSTAFFNUM'),
-                ];
-                $data[] = $v;
-            }
-
-            return $data;
+        $this->csp->add($this->cspKey, function () use ($code) {
+            return (new XinDongService())
+                ->setCheckRespFlag(true)
+                ->getEntMarketInfo($code);
         });
 
         $res = CspService::getInstance()->exec($this->csp, $this->cspTimeout);
