c16ea104403985731d6b5ecd4a55117f342e1f05
tian <1270331569@qq.com>
2022-11-10 16:39:43 +0800

    测试

--- App/HttpController/Business/AdminV2/Mrxd/Tools/ToolsController.php
+++ App/HttpController/Business/AdminV2/Mrxd/Tools/ToolsController.php
@@ -1007,6 +1007,15 @@ class ToolsController extends ControllerBase
 
         }
 
+        //根据信用代码查询资产负债（入参格式:信用代码）
+        if($requestData['type'] == 110 ){
+            $response =  (new GuoPiaoService())
+                ->setCheckRespFlag(true)
+                ->getFinanceIncomeStatement(
+                    $key
+                );
+        }
+
         return $this->writeJson(200, [], [
             [
                 'params'=> json_encode([
@@ -1045,6 +1054,7 @@ class ToolsController extends ControllerBase
             95 => '根据信用代码导出企业税务基本信息（入参格式:信用代码）',
             100 => '根据信用代码查询企业利润（入参格式:信用代码）',
             105 => '根据信用代码导出企业利润（入参格式:信用代码）',
+            110 => '根据信用代码查询资产负债（入参格式:信用代码）',
         ],'成功');
     }
 
