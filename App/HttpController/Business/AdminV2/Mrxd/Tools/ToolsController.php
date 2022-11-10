c244eab03bf802e99aafc24312b34940d24c1f00
tian <1270331569@qq.com>
2022-11-10 16:48:30 +0800

    测试

--- App/HttpController/Business/AdminV2/Mrxd/Tools/ToolsController.php
+++ App/HttpController/Business/AdminV2/Mrxd/Tools/ToolsController.php
@@ -1026,20 +1026,21 @@ class ToolsController extends ControllerBase
             fwrite($f,chr(0xEF).chr(0xBB).chr(0xBF));
 
             $allFields = [
-                "本月累计金额",//
-                "申报日期",//
-                "所属时期止",//
-                "征收项目",//
+                "declarationDate",//申报日期
+                "endDate",//所属时期止
+                "levyProjectName",//征收项目
+                "projectType",
+                "endingBalance",//期末数
+                "type",//资产负债类型,1:资产,2:负债及所有者权益
                 "reportType",
-                "所属时期起",//
-                "顺序",//
-                "栏次",//
-                "本年累计金额",//
-                "项目代码",//
-                "授权批次号",//
-                "项目名称",//
-                "纳税识别号",//
-                "SQJE"
+                "beginDate",//所属时期起
+                "sequence",//顺序
+                "columnSequence",//栏次
+                "initialBalance",//年初数
+                "projectNameCode",//项目代码
+                "taxNo",//授权批次号
+                "projectName",//项目名称
+                "taxpayerId"//纳税识别号
             ];
             foreach ($allFields as $field=>$cname){
 
