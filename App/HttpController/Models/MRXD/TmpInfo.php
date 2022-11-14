f50b4c9d74ff49187343e962bed9ed7d924496c6
tian <1270331569@qq.com>
2022-11-14 16:56:31 +0800

    测试

--- App/HttpController/Models/MRXD/TmpInfo.php
+++ App/HttpController/Models/MRXD/TmpInfo.php
@@ -29,10 +29,10 @@ class TmpInfo extends ModelBase
 
 
     static  function  addRecordV2($info){
-        $res = self::findByName($info['name']);
-        if($res){
-            return  $res->id;
-        }
+//        $res = self::findByName($info['name']);
+//        if($res){
+//            return  $res->id;
+//        }
         return TmpInfo::addRecord(
             $info
         );
@@ -41,12 +41,33 @@ class TmpInfo extends ModelBase
     public static function addRecord($requestData){
         try {
            $res =  TmpInfo::create()->data([
-                'name' => $requestData['name']?:'',
-                'details' => $requestData['details']?:'',
-                'value' => $requestData['value']?:'',
-                'remark' => $requestData['remark']?:'',
-               'created_at' => time(),
-               'updated_at' => time(),
+                'biao_ti' => $requestData['biao_ti']?:'',
+                'xiang_mu_ming_cheng' => $requestData['xiang_mu_ming_cheng']?:'',
+                'xiang_mu_bian_hao' => $requestData['xiang_mu_bian_hao']?:'',
+                'xiang_mu_jian_jie' => $requestData['xiang_mu_jian_jie']?:'',
+                'cai_gou_fang_shi' => $requestData['cai_gou_fang_shi']?:'',
+                'gong_gao_lei_xing2' => $requestData['gong_gao_lei_xing2']?:'',
+                'gong_gao_ri_qi' => $requestData['gong_gao_ri_qi']?:'',
+                'xing_zheng_qv_yu_sheng' => $requestData['xing_zheng_qv_yu_sheng']?:'',
+                'xing_zheng_qv_yu_shi' => $requestData['xing_zheng_qv_yu_shi']?:'',
+                'xing_zheng_qv_yu_xian' => $requestData['xing_zheng_qv_yu_xian']?:'',
+                'cai_gou_dan_wei_ming_cheng' => $requestData['cai_gou_dan_wei_ming_cheng']?:'',
+                'cai_gou_dan_wei_di_zhi' => $requestData['cai_gou_dan_wei_di_zhi']?:'',
+                'cai_gou_dan_wei_lian_xi_ren' => $requestData['cai_gou_dan_wei_lian_xi_ren']?:'',
+                'cai_gou_dan_wei_lian_xi__dian_hua' => $requestData['cai_gou_dan_wei_lian_xi__dian_hua']?:'',
+                'ming_ci' => $requestData['ming_ci']?:'',
+                'zhong_biao_gong_ying_shang' => $requestData['zhong_biao_gong_ying_shang']?:'',
+                'zhong_biao_jin_e' => $requestData['zhong_biao_jin_e']?:'',
+                'dai_li_ji_gou_ming_cheng' => $requestData['dai_li_ji_gou_ming_cheng']?:'',
+                'dai_li_ji_gou_di_zhi' => $requestData['dai_li_ji_gou_di_zhi']?:'',
+                'dai_li_ji_gou_lian_xi_ren' => $requestData['dai_li_ji_gou_lian_xi_ren']?:'',
+                'dai_li_ji_gou_lian_xi_dian_hua' => $requestData['dai_li_ji_gou_lian_xi_dian_hua']?:'',
+                'ping_gu_zhuan_jia' => $requestData['ping_gu_zhuan_jia']?:'',
+                'DLSM_UUID' => $requestData['DLSM_UUID']?:'',
+                'url' => $requestData['url']?:'',
+                'corexml' => $requestData['corexml']?:'',
+                'updated_at' => $requestData['updated_at']?:'',
+                'source' => $requestData['source']?:'',
            ])->save();
 
         } catch (\Throwable $e) {
