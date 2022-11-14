3316b9c263b22e1cc63a5608678029d79a7e8c2c
tian <1270331569@qq.com>
2022-11-14 10:31:57 +0800

    测试

---
+++ App/HttpController/Models/MRXD/TmpInfo.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\HttpController\Models\MRXD;
+
+use App\HttpController\Models\AdminNew\ConfigInfo;
+use App\HttpController\Models\AdminV2\AdminUserSoukeConfig;
+use App\HttpController\Models\AdminV2\MobileCheckInfo;
+use App\HttpController\Models\Api\FinancesSearch;
+use App\HttpController\Models\BusinessBase\CompanyClue;
+use App\HttpController\Models\BusinessBase\WechatInfo;
+use App\HttpController\Models\ModelBase;
+use App\HttpController\Models\RDS3\HdSaic\CodeCa16;
+use App\HttpController\Models\RDS3\HdSaic\CodeEx02;
+use App\HttpController\Models\RDS3\HdSaic\CompanyBasic;
+use App\HttpController\Service\ChuangLan\ChuangLanService;
+use App\HttpController\Service\Common\CommonService;
+use App\HttpController\Service\CreateConf;
+use App\HttpController\Service\LongXin\LongXinService;
+use App\HttpController\Service\Sms\AliSms;
+use App\HttpController\Service\XinDong\XinDongService;
+use EasySwoole\RedisPool\Redis;
+
+// use App\HttpController\Models\AdminRole;
+
+class TmpInfo extends ModelBase
+{
+
+    protected $tableName = 'tmp_info';
+
+
+    static  function  addRecordV2($info){
+        $res = self::findByName($info['name']);
+        if($res){
+            return  $res->id;
+        }
+        return TmpInfo::addRecord(
+            $info
+        );
+    }
+
+    public static function addRecord($requestData){
+        try {
+           $res =  TmpInfo::create()->data([
+                'name' => $requestData['name']?:'',
+                'details' => $requestData['details']?:'',
+                'value' => $requestData['value']?:'',
+                'remark' => $requestData['remark']?:'',
+               'created_at' => time(),
+               'updated_at' => time(),
+           ])->save();
+
+        } catch (\Throwable $e) {
+            return CommonService::getInstance()->log4PHP(
+                json_encode([
+                    __CLASS__.__FUNCTION__ .__LINE__,
+                    'failed',
+                    '$requestData' => $requestData,
+                    'msg' => $e->getMessage(),
+                ])
+            );
+        }
+        return $res;
+    }
+
+
+    public static function findAllByCondition($whereArr){
+        $res =  TmpInfo::create()
+            ->where($whereArr)
+            ->all();
+        return $res;
+    }
+
+    public static function setTouchTime($id,$touchTime){
+        $info = TmpInfo::findById($id);
+
+        return $info->update([
+            'touch_time' => $touchTime,
+        ]);
+    }
+
+    public static function updateById(
+        $id,$data
+    ){
+        $info = self::findById($id);
+        return $info->update($data);
+    }
+
+    public static function findByConditionWithCountInfo($whereArr,$page){
+        $model = TmpInfo::create()
+                ->where($whereArr)
+                ->page($page)
+                ->order('id', 'DESC')
+                ->withTotalCount();
+
+        $res = $model->all();
+
+        $total = $model->lastQueryResult()->getTotalCount();
+        return [
+            'data' => $res,
+            'total' =>$total,
+        ];
+    }
+
+    public static function findByConditionV2($whereArr,$page){
+        $model = TmpInfo::create();
+        foreach ($whereArr as $whereItem){
+            $model->where($whereItem['field'], $whereItem['value'], $whereItem['operate']);
+        }
+        $model->page($page)
+            ->order('id', 'DESC')
+            ->withTotalCount();
+
+        $res = $model->all();
+
+        $total = $model->lastQueryResult()->getTotalCount();
+        return [
+            'data' => $res,
+            'total' =>$total,
+        ];
+    }
+
+
+    public static function findById($id){
+        $res =  TmpInfo::create()
+            ->where('id',$id)            
+            ->get();  
+        return $res;
+    }
+
+    public static function findByName($name){
+        $res =  TmpInfo::create()
+            ->where('name',$name)
+            ->get();
+        return $res;
+    }
+
+    public static function setData($id,$field,$value){
+        $info = TmpInfo::findById($id);
+        return $info->update([
+            "$field" => $value,
+        ]);
+    }
+
+    public static function findBySql($where){
+        $Sql = " select *  
+                            from  
+                        `tmp_info` 
+                            $where
+      " ;
+        $data = sqlRaw($Sql, CreateConf::getInstance()->getConf('env.mysqlDatabase'));
+        return $data;
+    }
+
+
+}
