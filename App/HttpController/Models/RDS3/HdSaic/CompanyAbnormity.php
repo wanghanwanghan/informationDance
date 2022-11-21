36f14de2a97695dad0c2f7ac4b73b684c2e4243b
liyunxian <908770505@qq.com>
2022-11-21 15:35:52 +0800

    add

---
+++ App/HttpController/Models/RDS3/HdSaic/CompanyAbnormity.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\HttpController\Models\RDS3\HdSaic;
+
+use App\HttpController\Models\ModelBase;
+use App\HttpController\Service\CreateConf;
+
+class CompanyAbnormity extends ModelBase
+{
+    protected $tableName = 'company_abnormity';
+
+
+    function __construct(array $data = [])
+    {
+        parent::__construct($data);
+
+        $this->connectionName = CreateConf::getInstance()->getConf('env.mysqlDatabaseRDS_3_hd_saic');
+    }
+}
--- App/HttpController/ProvideRouter.php
+++ App/HttpController/ProvideRouter.php
@@ -295,6 +295,14 @@ class ProvideRouter
             $routeCollector->addRoute(['GET', 'POST'], '/getEntMarketInfo', $prefix . 'getEntMarketInfo');//企业上市信息
             $routeCollector->addRoute(['GET', 'POST'], '/getEntLiquidation', $prefix . 'getEntLiquidation');//清算
             $routeCollector->addRoute(['GET', 'POST'], '/fuzzyMatchEntName', $prefix . 'fuzzyMatchEntName');//清算
+
+            //获取本库中的数据接口
+            $routeCollector->addRoute(['GET', 'POST'], '/getCncaRzGltx_h', $prefix . 'getCncaRzGltx_h');//认监委-ISO管理体系认证
+            $routeCollector->addRoute(['GET', 'POST'], '/getCaseAll_h', $prefix . 'getCaseAll_h');//工商-行政处罚
+            $routeCollector->addRoute(['GET', 'POST'], '/getCaseCheck_h', $prefix . 'getCaseCheck_h');//工商-抽查检查信息
+            $routeCollector->addRoute(['GET', 'POST'], '/getCaseYzwfsx_h', $prefix . 'getCaseYzwfsx_h');//工商-严重违法失信
+            $routeCollector->addRoute(['GET', 'POST'], '/getCompanyAbnormity_h', $prefix . 'getCompanyAbnormity_h');// 工商-经营异常
+            $routeCollector->addRoute(['GET', 'POST'], '/getCompanyBasic_h', $prefix . 'getCompanyBasic_h');//获取企业基本信息
         });
 
         return true;
