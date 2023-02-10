4ff16a992ac34a5c083a82ba94166a80c83501fe
tian <1270331569@qq.com>
2023-02-10 10:35:56 +0800

    删除多余的log

--- App/HttpController/Models/MRXD/OnlineGoodsUserInviteRelation.php
+++ App/HttpController/Models/MRXD/OnlineGoodsUserInviteRelation.php
@@ -263,13 +263,13 @@ class OnlineGoodsUserInviteRelation extends ModelBase
                 break;
             }
         }
-        CommonService::getInstance()->log4PHP(
-            json_encode([
-                __CLASS__.__FUNCTION__ .__LINE__,
-                'getVipInviterInfo_$userId'=>$userId,
-                '$topInvitor'=>$topInvitor,
-            ])
-        );
+//        CommonService::getInstance()->log4PHP(
+//            json_encode([
+//                __CLASS__.__FUNCTION__ .__LINE__,
+//                'getVipInviterInfo_$userId'=>$userId,
+//                '$topInvitor'=>$topInvitor,
+//            ])
+//        );
         if($topInvitor <= 0 ){
             return  [];
         }
