14ca62ddae7d408b7c6c2d1ebebd6c50dbae723d
tian <1270331569@qq.com>
2023-02-20 09:42:52 +0800

    招投标定时任务

--- App/Crontab/CrontabList/RunDealZhaoTouBiao.php
+++ App/Crontab/CrontabList/RunDealZhaoTouBiao.php
@@ -109,9 +109,19 @@ class RunDealZhaoTouBiao extends AbstractCronTask
     public static function getZhaoTouBiaoData(
         $dateStart,$dateEnd,$source
     ){
+        CommonService::getInstance()->log4PHP(
+            json_encode([
+                __CLASS__.__FUNCTION__ .__LINE__,
+                '发送招投标邮件（1）--获取招投标数据'=>[
+                    '开始时间'=>$dateStart,
+                    '结束时间'=>$dateEnd,
+                    '来源'=>$source,
+                ]
+            ],JSON_UNESCAPED_UNICODE)
+        );
+
 
         $returnDatas  = [];
-        //所有的数据
         $where = " WHERE updated_at >= '$dateStart' AND updated_at <= '$dateEnd' AND source = '$source'  ";
         $datas = \App\HttpController\Models\RDS3\ZhaoTouBiao\ZhaoTouBiaoAll::findBySql(
             $where
@@ -119,7 +129,10 @@ class RunDealZhaoTouBiao extends AbstractCronTask
         CommonService::getInstance()->log4PHP(
             json_encode([
                 __CLASS__.__FUNCTION__ .__LINE__,
-                '查询招投标数据'=>[
+                '发送招投标邮件（1）--获取招投标数据'=>[
+                    '开始时间'=>$dateStart,
+                    '结束时间'=>$dateEnd,
+                    '来源'=>$source,
                     '条件'=>$where,
                     '返回结果数量'=>count($datas),
                 ]
@@ -173,10 +186,15 @@ class RunDealZhaoTouBiao extends AbstractCronTask
       * */
     function run(int $taskId, int $workerIndex): bool
     {
-        //设置为正在执行中
-        ConfigInfo::setIsRunning(__CLASS__);
-
         $day = date('Y-m-d');
+        CommonService::getInstance()->log4PHP(
+            json_encode([
+                __CLASS__.__FUNCTION__ .__LINE__,
+                '发送招投标邮件-脚本开始执行'=>[
+                    '日期'=>$day,
+                ]
+            ],JSON_UNESCAPED_UNICODE)
+        );
 
         //第一次的招投标
         self::sendEmail($day);
@@ -221,7 +239,6 @@ class RunDealZhaoTouBiao extends AbstractCronTask
         $dateEnd = $day.' 23:59:59';
 
         $res = self::exportDataV4($dateStart,$dateEnd);
-
         if(
             $res['p2Nums'] == 0 &&
             $res['p1Nums'] == 0
@@ -265,6 +282,7 @@ class RunDealZhaoTouBiao extends AbstractCronTask
             '',
             [TEMP_FILE_PATH . $res['filename']]
         );
+
 
         CommonService::getInstance()->log4PHP(
             json_encode([
@@ -315,12 +333,13 @@ class RunDealZhaoTouBiao extends AbstractCronTask
         CommonService::getInstance()->log4PHP(
             json_encode([
                 __CLASS__.__FUNCTION__ .__LINE__,
-                '发送招投标邮件（2）-压缩csv文件' => [
-                    //'csv文件集合'=> $res['filesArr'],
+                '发送招投标邮件（2）--压缩成ZIP'=>[
+                    '日期'=>$day,
+                    '收件人'=>$emailsLists,
                     '压缩结果'=> $zipRes,
                     '新的文件名'=> $filename,
                 ]
-            ])
+            ],JSON_UNESCAPED_UNICODE)
         );
 
         $sendResArr = [];
@@ -336,11 +355,20 @@ class RunDealZhaoTouBiao extends AbstractCronTask
 
         return true ;
     }
+
     //TODO  改成按行的 防止内存溢出
     static function  exportDataV4($dateStart,$dateEnd){
-        $startMemory = memory_get_usage();
-        //p1
+        CommonService::getInstance()->log4PHP(
+            json_encode([
+                __CLASS__.__FUNCTION__ .__LINE__,
+                '发送招投标邮件（1）--生成表格文件'=>[
+                    '开始日期'=>$dateStart,
+                    '结束日期'=>$dateEnd,
+                ]
+            ],JSON_UNESCAPED_UNICODE)
+        );
 
+        $startMemory = memory_get_usage();
         $financeDatas = self::getZhaoTouBiaoData(
             $dateStart,$dateEnd,'p1'
         );
@@ -397,19 +425,18 @@ class RunDealZhaoTouBiao extends AbstractCronTask
             'corexml' , //
         ];
         $file = $fileObject
-            //->defaultFormat($colorStyle)
             ->header(
                 $headerTitle
-            )
-           // ->defaultFormat($alignStyle)
-        ;
+            );
         $p1Nums = 0;
         foreach ($financeDatas as $dataItem){
             if($p1Nums%20==0){
                 CommonService::getInstance()->log4PHP(
                     json_encode([
                         __CLASS__.__FUNCTION__ .__LINE__,
-                        '生成招投标邮件sheet1'=>[
+                        '发送招投标邮件（1）--生成表格文件-生成招投标邮件sheet1'=>[
+                            '开始日期'=>$dateStart,
+                            '结束日期'=>$dateEnd,
                             '文件名'=>$filename,
                             '已生成'=>$p1Nums,
                         ]
@@ -436,9 +463,11 @@ class RunDealZhaoTouBiao extends AbstractCronTask
                 CommonService::getInstance()->log4PHP(
                     json_encode([
                         __CLASS__.__FUNCTION__ .__LINE__,
-                        '生成招投标邮件sheet2'=>[
+                        '发送招投标邮件（1）--生成表格文件-生成招投标邮件sheet2'=>[
+                            '开始日期'=>$dateStart,
+                            '结束日期'=>$dateEnd,
                             '文件名'=>$filename,
-                            '已生成'=>$p2Nums,
+                            '已生成'=>$p1Nums,
                         ]
                     ],JSON_UNESCAPED_UNICODE)
                 );
@@ -461,12 +490,10 @@ class RunDealZhaoTouBiao extends AbstractCronTask
         CommonService::getInstance()->log4PHP(
             json_encode([
                 __CLASS__.__FUNCTION__ .__LINE__,
-                '生成招投标邮件完成'=>[
+                '发送招投标邮件（1）--生成表格文件-生成招投标邮件完成'=>[
+                    '开始日期'=>$dateStart,
+                    '结束日期'=>$dateEnd,
                     '文件名'=>$filename,
-                    '内存使用'=>round((memory_get_usage()-$startMemory)/1024/1024,3).'M',
-                    'dateStart' => $dateStart  ,
-                    'dateEnd' => $dateEnd ,
-                    'filename'=>$filename,
                     'p2Nums' => $p2Nums,
                     'p1Nums' => $p1Nums,
                 ]
@@ -481,186 +508,18 @@ class RunDealZhaoTouBiao extends AbstractCronTask
             'p1Nums' => $p1Nums,
         ];
     }
-    static function  exportDataV5($day){
-        $startMemory = memory_get_usage();
-
-        $the_date = $day;
-        $the_day_of_week = date("w",strtotime($the_date)); //sunday is 0
-
-        $first_day_of_week = date("Y-m-d",strtotime( $the_date )-60*60*24*($the_day_of_week)+60*60*24*1 );
-        $last_day_of_week = date("Y-m-d",strtotime($first_day_of_week)+60*60*24*4 );
-
-        $dateStart = $first_day_of_week.' 00:00:00';
-        $dateEnd = $last_day_of_week.' 23:59:59';
-
-        $filename = 'zhao_tou_biao_new_'.date('YmdHis').'.xlsx';
-
-        //===============================
-        $config=  [
-            'path' => TEMP_FILE_PATH // xlsx文件保存路径
-        ];
-
-        $excel = new \Vtiful\Kernel\Excel($config);
-        $fileObject = $excel->fileName($filename, 'p1');
-        $fileHandle = $fileObject->getHandle();
-
-        $format = new Format($fileHandle);
-        $colorStyle = $format
-            ->fontColor(Format::COLOR_ORANGE)
-            ->border(Format::BORDER_DASH_DOT)
-            ->align(Format::FORMAT_ALIGN_CENTER, Format::FORMAT_ALIGN_VERTICAL_CENTER)
-            ->toResource();
-
-        $format = new Format($fileHandle);
-
-        $alignStyle = $format
-            ->align(Format::FORMAT_ALIGN_CENTER, Format::FORMAT_ALIGN_VERTICAL_CENTER)
-            ->toResource();
-
-        $headerTitle= [
-            '来源' , //
-            '标题' , //
-            '项目名称' , //
-            '项目编号' , //
-            '项目简介' , //
-            '采购方式' , //
-            '公告类型2' , //
-            '公告日期' , //
-            '行政区域_省' , //
-            '行政区域_市' , //
-            '行政区域_县' , //
-            '采购单位名称' , //
-            '采购单位地址' , //
-            '采购单位联系人' , //
-            '采购单位联系电话' , //
-            '名次' , //
-            '中标供应商' , //
-            '中标金额' , //
-            '代理机构名称' , //
-            '代理机构地址' , //
-            '代理机构联系人' , //
-            '代理机构联系电话' , //
-            '评标专家' , //
-            'DLSM_UUID' , //
-            'url' , //
-            'corexml' , //
-
-        ];
-
-        $file = $fileObject
-            //->defaultFormat($colorStyle)
-            ->header(
-                $headerTitle
-            )
-           // ->defaultFormat($alignStyle)
-        ;
-
-        $datas =  \App\HttpController\Models\RDS3\ZhaoTouBiao\ZhaoTouBiaoAll::findBySqlV2(
-            " SELECT * FROM zhao_tou_biao_key03 WHERE updated_at >= '$dateStart' AND  updated_at <= '$dateEnd'  "
-        );
-        $p1Nums = 1;
-        foreach ($datas as $dataItem){
-
-            $fileObject->insertText($p1Nums, 0, 'zhao_tou_biao_key03');
-            $fileObject->insertText($p1Nums, 1,  mb_substr($dataItem['标题'], 0, 500));
-            $fileObject->insertText($p1Nums, 2, $dataItem['项目名称']);
-            $fileObject->insertText($p1Nums, 3, $dataItem['项目编号']);
-            $fileObject->insertText($p1Nums, 4, $dataItem['项目简介']);
-            $fileObject->insertText($p1Nums, 5, $dataItem['采购方式']);
-            $fileObject->insertText($p1Nums, 6, $dataItem['公告类型2']);
-            $fileObject->insertText($p1Nums, 7, $dataItem['公告日期']);
-            $fileObject->insertText($p1Nums, 8, $dataItem['行政区域_省']);
-            $fileObject->insertText($p1Nums, 9, $dataItem['行政区域_市']);
-            $fileObject->insertText($p1Nums, 10, $dataItem['行政区域_县']);
-            $fileObject->insertText($p1Nums, 11, $dataItem['采购单位名称']);
-            $fileObject->insertText($p1Nums, 12, $dataItem['采购单位地址']);
-            $fileObject->insertText($p1Nums, 13, $dataItem['采购单位联系人']);
-            $fileObject->insertText($p1Nums, 14, $dataItem['采购单位联系电话']);
-            $fileObject->insertText($p1Nums, 15, $dataItem['名次']);
-            $fileObject->insertText($p1Nums, 16, $dataItem['中标供应商']);
-            $fileObject->insertText($p1Nums, 17, $dataItem['中标金额']);
-            $fileObject->insertText($p1Nums, 18, $dataItem['代理机构名称']);
-            $fileObject->insertText($p1Nums, 19, $dataItem['代理机构地址']);
-            $fileObject->insertText($p1Nums, 20, $dataItem['代理机构联系人']);
-            $fileObject->insertText($p1Nums, 21, $dataItem['代理机构联系电话']);
-            $fileObject->insertText($p1Nums, 22, $dataItem['评标专家']);
-            $fileObject->insertText($p1Nums, 23, $dataItem['DLSM_UUID']);
-            $fileObject->insertText($p1Nums, 24, $dataItem['url']);
-            $fileObject->insertText($p1Nums, 25, str_split ( $dataItem['corexml'], 32766 )[0]);
-            $p1Nums ++ ;
-        }
-
-
-
-        //==============================================
-        //p2
-
-        $file->addSheet('p2')
-            //->defaultFormat($colorStyle)
-            ->header($headerTitle)
-            //->defaultFormat($alignStyle)
-           ;
-        $p2Nums = 1;
-        foreach ($datas as $dataItem){
-            $fileObject->insertText($p2Nums, 0, 'zhao_tou_biao_key03');
-            $fileObject->insertText($p2Nums, 1, $dataItem['标题']);
-            $fileObject->insertText($p2Nums, 2, $dataItem['项目名称']);
-            $fileObject->insertText($p2Nums, 3, $dataItem['项目编号']);
-            $fileObject->insertText($p2Nums, 4, $dataItem['项目简介']);
-            $fileObject->insertText($p2Nums, 5, $dataItem['采购方式']);
-            $fileObject->insertText($p2Nums, 6, $dataItem['公告类型2']);
-            $fileObject->insertText($p2Nums, 7, $dataItem['公告日期']);
-            $fileObject->insertText($p2Nums, 8, $dataItem['行政区域_省']);
-            $fileObject->insertText($p2Nums, 9, $dataItem['行政区域_市']);
-            $fileObject->insertText($p2Nums, 10, $dataItem['行政区域_县']);
-            $fileObject->insertText($p2Nums, 11, $dataItem['采购单位名称']);
-            $fileObject->insertText($p2Nums, 12, $dataItem['采购单位地址']);
-            $fileObject->insertText($p2Nums, 13, $dataItem['采购单位联系人']);
-            $fileObject->insertText($p2Nums, 14, $dataItem['采购单位联系电话']);
-            $fileObject->insertText($p2Nums, 15, $dataItem['名次']);
-            $fileObject->insertText($p2Nums, 16, $dataItem['中标供应商']);
-            $fileObject->insertText($p2Nums, 17, $dataItem['中标金额']);
-            $fileObject->insertText($p2Nums, 18, $dataItem['代理机构名称']);
-            $fileObject->insertText($p2Nums, 19, $dataItem['代理机构地址']);
-            $fileObject->insertText($p2Nums, 20, $dataItem['代理机构联系人']);
-            $fileObject->insertText($p2Nums, 21, $dataItem['代理机构联系电话']);
-            $fileObject->insertText($p2Nums, 22, $dataItem['评标专家']);
-            $fileObject->insertText($p2Nums, 23, $dataItem['DLSM_UUID']);
-            $fileObject->insertText($p2Nums, 24, $dataItem['url']);
-            $fileObject->insertText($p2Nums, 25, str_split ( $dataItem['corexml'], 32766 )[0]);
-            $p2Nums ++ ;
-        }
-        //==============================================
-        CommonService::getInstance()->log4PHP(
-            json_encode([
-                __CLASS__.__FUNCTION__ .__LINE__,
-                'generate data done . memory use' => round((memory_get_usage()-$startMemory)/1024/1024,3).'M'
-            ])
-        );
-
-        $format = new Format($fileHandle);
-        //单元格有\n解析成换行
-        $wrapStyle = $format
-            ->align(Format::FORMAT_ALIGN_CENTER, Format::FORMAT_ALIGN_VERTICAL_CENTER)
-            ->wrap()
-            ->toResource();
-
-        $fileObject->output();
-        //===============================
-
-        return  [
-            'dateStart' => $dateStart  ,
-            'dateEnd' => $dateEnd ,
-            'filename'=>$filename,
-            'filename_url'=>'http://api.test.meirixindong.com/Static/Temp/'.$filename,
-            'p2Nums' => $p2Nums,
-            'p1Nums' => $p1Nums,
-        ];
-    }
-
 
 
     static function  exportDataV8($day){
+        CommonService::getInstance()->log4PHP(
+            json_encode([
+                __CLASS__.__FUNCTION__ .__LINE__,
+                '发送招投标邮件（2）--生成表格数据'=>[
+                    '日期'=>$day,
+                ]
+            ],JSON_UNESCAPED_UNICODE)
+        );
+
         $startMemory = memory_get_usage();
 
         $the_date = $day;
@@ -677,7 +536,7 @@ class RunDealZhaoTouBiao extends AbstractCronTask
         CommonService::getInstance()->log4PHP(
             json_encode([
                 __CLASS__.__FUNCTION__ .__LINE__,
-                '查询并写入招投标邮件数据'=>[
+                '发送招投标邮件（2）--生成表格数据'=>[
                     '日期'=>$day,
                     '开始日期'=>$dateStart,
                     '结束日期'=>$dateEnd,
@@ -712,7 +571,6 @@ class RunDealZhaoTouBiao extends AbstractCronTask
             'DLSM_UUID' , //
             'url' , //
             'corexml' , //
-
         ];
 
 
@@ -720,7 +578,7 @@ class RunDealZhaoTouBiao extends AbstractCronTask
             'zhao_tou_biao_key01',
             'zhao_tou_biao_key02',
             'zhao_tou_biao_key03',
-            //'zhao_tou_biao_key04',
+            'zhao_tou_biao_key04',
             'zhao_tou_biao_key05',
             'zhao_tou_biao_key06',
             'zhao_tou_biao_key07',
@@ -731,7 +589,7 @@ class RunDealZhaoTouBiao extends AbstractCronTask
             'zhao_tou_biao_key12',
             'zhao_tou_biao_key13',
         ];
-
+        
         $totalNums = 0;
         $filesArr = [];
         foreach ($tables as $table){
@@ -749,8 +607,10 @@ class RunDealZhaoTouBiao extends AbstractCronTask
             CommonService::getInstance()->log4PHP(
                 json_encode([
                     __CLASS__.__FUNCTION__ .__LINE__,
-                    '查询并写入招投标邮件数据'=>[
+                    '发送招投标邮件（2）--生成表格数据-查询并写入招投标邮件数据'=>[
                         '日期'=>$day,
+                        '开始日期'=>$dateStart,
+                        '结束日期'=>$dateEnd,
                         '表'=>$table,
                         '语句'=>" SELECT * FROM $table WHERE `公告日期` >= '$dateStart' AND  `公告日期` <= '$dateEnd'  ",
                         '返回记录数量'=>count($datas),
@@ -803,10 +663,10 @@ class RunDealZhaoTouBiao extends AbstractCronTask
         CommonService::getInstance()->log4PHP(
             json_encode([
                 __CLASS__.__FUNCTION__ .__LINE__,
-                '查询并写入招投标邮件数据-完成'=>[
+                '发送招投标邮件（2）--生成表格数据-查询并写入招投标邮件数据-完成'=>[
                     '日期'=>$day,
-                    //'生成的文件名'=>$filesArr,
-                    '内存使用' => round((memory_get_usage()-$startMemory)/1024/1024,3).'M',
+                    '开始日期'=>$dateStart,
+                    '结束日期'=>$dateEnd,
                 ]
             ],JSON_UNESCAPED_UNICODE)
         );
