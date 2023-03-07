21700173210979e08c1214a9fc5c2c565cec11eb
liyunxian <908770505@qq.com>
2023-03-07 14:07:51 +0800

    add

--- App/Crontab/CrontabList/RunCheckCapital.php
+++ App/Crontab/CrontabList/RunCheckCapital.php
@@ -21,7 +21,7 @@ class RunCheckCapital extends AbstractCronTask
     static function getRule(): string
     {
         //每分钟执行一次
-        return '40 13 * * *';
+        return '10 14 * * *';
     }
 
     static function getTaskName(): string
