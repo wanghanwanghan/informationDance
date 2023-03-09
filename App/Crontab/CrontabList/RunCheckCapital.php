1f415f228ef4d3ac3e2dea0b3d6cad6f624462b3
liyunxian <908770505@qq.com>
2023-03-09 10:46:23 +0800

    add

--- App/Crontab/CrontabList/RunCheckCapital.php
+++ App/Crontab/CrontabList/RunCheckCapital.php
@@ -37,7 +37,7 @@ class RunCheckCapital extends AbstractCronTask
         $resp      = (new CoHttpClient())
             ->useCache($DianZiQianService->curl_use_cache)
             ->send($DianZiQianService->url . $path, $param,[], ['enableSSL' => true], 'GET');
-        if($resp['description'] == 'success' && $resp['data']['availableAmount']<10000){
+        if($resp['description'] == 'success' && $resp['data']['availableAmount']<500){
             feishuTishi('本公司在电子牵的账户信息',
                         [
                             '累计充值下单金额总额' => $resp['data']['totalAmount'],
