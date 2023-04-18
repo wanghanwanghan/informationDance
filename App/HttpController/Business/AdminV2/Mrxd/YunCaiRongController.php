6910e1841177a6a67a891643b7ac4a4d3e31f970
tian <1270331569@qq.com>
2023-04-18 17:31:42 +0800

    测试

--- App/HttpController/Business/AdminV2/Mrxd/YunCaiRongController.php
+++ App/HttpController/Business/AdminV2/Mrxd/YunCaiRongController.php
@@ -83,6 +83,43 @@ class YunCaiRongController extends ControllerBase
         );
     }
 
+    function getProductNameHot2(): bool
+    {
+        $requestData =  $this->getRequestData();
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://qlycpro.techopen.org.cn/api/drugs/getProductNameHot');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // Skip SSL Verification
+        curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+
+        $headers = array();
+        $headers[] = 'Host: qlycpro.techopen.org.cn';
+        $headers[] = 'Access-Token: eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI5MmFlMWY2Ny1jMDFiLTRjNTgtYjZiNS00YzlkOWJiNTRkYTIiLCJzZXNzaW9uSWQiOiI5MmFlMWY2Ny1jMDFiLTRjNTgtYjZiNS00YzlkOWJiNTRkYTIiLCJleHAiOjQ4Mzc0NzQxNDksImlhdCI6MTY4MTgwMDU0OX0.GFW6ZWUX_P6Ar4PaEsCa3kWw1HxwDP22uA6OpfQjvqWDC0QQ2gv84vynf0Ls5_PaUE8OZwfG_IChnoTGZpqfng';
+        $headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat';
+        $headers[] = 'Content-Type: application/json;charset=UTF-8';
+        $headers[] = 'User_token: eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI5MmFlMWY2Ny1jMDFiLTRjNTgtYjZiNS00YzlkOWJiNTRkYTIiLCJzZXNzaW9uSWQiOiI5MmFlMWY2Ny1jMDFiLTRjNTgtYjZiNS00YzlkOWJiNTRkYTIiLCJleHAiOjQ4Mzc0NzQxNDksImlhdCI6MTY4MTgwMDU0OX0.GFW6ZWUX_P6Ar4PaEsCa3kWw1HxwDP22uA6OpfQjvqWDC0QQ2gv84vynf0Ls5_PaUE8OZwfG_IChnoTGZpqfng';
+        $headers[] = 'Referer: https://servicewechat.com/wxed1127fa8a0fc503/74/page-frame.html';
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $result = curl_exec($ch);
+        $resultArr =  json_decode($result,true);
+        curl_close($ch);
+
+
+        return $this->writeJson(
+            200,
+            [
+
+            ] ,
+            $resultArr['data']
+            ,
+            '成功',
+            true,
+            []
+        );
+    }
 
     function jiaoYiGongGaoList(): bool
     {
